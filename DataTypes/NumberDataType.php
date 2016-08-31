9270622685106535c1b8fa159567dd9cb037981c
Andrej Kabachnik <andrej_ka@gmx.de>
2016-08-31 13:25:53 +0200

    FIX Enhanced NumberDataType validation

--- DataTypes/NumberDataType.php
+++ DataTypes/NumberDataType.php
@@ -12,6 +12,11 @@ class NumberDataType extends AbstractDataType {
 			// Hexadecimal numbers in '0x....'-Notation
 			return $string;
 		} else {
+			$matches = array();
+			preg_match_all('!-?\d+[,\.]?\d*+!', str_replace(' ', '', $string), $matches);
+			if (is_numeric($matches[0][0])){
+				return $matches[0][0];
+			}			
 			throw new DataTypeValidationError('Cannot convert "' . $string . '" to a number!');
 			return '';
 		}
