bea19c4fbbd3ba68230f88ce63b06c6fc2f24965
Andrej Kabachnik <andrej_ka@gmx.de>
2016-12-02 09:31:56 +0100

    FIX create_from_anything() not working with instantiated links

--- Factories/WidgetLinkFactory.php
+++ Factories/WidgetLinkFactory.php
@@ -22,6 +22,9 @@ abstract class WidgetLinkFactory extends AbstractUxonFactory {
 	 * @return WidgetLinkInterface
 	 */
 	public static function create_from_anything(Workbench &$exface, $string_or_object){
+		if ($string_or_object instanceof WidgetLinkInterface){
+			return $string_or_object;
+		}
 		$ref = static::create_empty($exface);
 		$ref->parse_link($string_or_object);
 		return $ref;
