f55386f9a3de0d2920a3875e38d132d7bacfa4c9
Andrej Kabachnik <andrej_ka@gmx.de>
2016-12-20 15:31:18 +0100

    FIX handling empty and boolean values in numeric data types

--- DataTypes/NumberDataType.php
+++ DataTypes/NumberDataType.php
@@ -5,12 +5,18 @@ use exface\Core\Exceptions\DataTypeValidationError;
 class NumberDataType extends AbstractDataType {
 	
 	public static function parse($string){
-		if (is_numeric($string)){
+		if (is_numeric($string) || $string === ''){
 			// Decimal numbers
 			return $string;
 		} elseif (strpos($string, '0x') === 0) {
 			// Hexadecimal numbers in '0x....'-Notation
 			return $string;
+		} elseif (strcasecmp($string, 'true') === 0) {
+			return 1;
+		} elseif (strcasecmp($string, 'false') === 0) {
+			return 0;
+		} elseif (strcasecmp($string, 'null') === 0) {
+			return null;
 		} else {
 			$matches = array();
 			preg_match_all('!-?\d+[,\.]?\d*+!', str_replace(' ', '', $string), $matches);
