cdb6c679a68c4615077a027a12b285dae0cef46c
Andrej Kabachnik <andrej_ka@gmx.de>
2017-01-27 12:50:49 +0100

    FIX autosuggest now works even if not explicitly called by a widget

--- Actions/Autosuggest.php
+++ Actions/Autosuggest.php
@@ -1,5 +1,7 @@
 <?php
 namespace exface\Core\Actions;
+use exface\Core\Factories\WidgetFactory;
+
 /**
  * The autosuggest action is similar to the general ReadData, but it does not affect the current window filter context because the user
  * does not really perform an explicit serch here - it's merely the system helping the user to speed up input. The context, the user is
@@ -15,8 +17,28 @@ class Autosuggest extends ReadData {
 	protected function init(){
 		$this->set_update_filter_context(false);
 	}	
-
-	// IDEA override the perform() method to include recently used object in the autosuggest results. But where can we get those object from?
-	// Another window context? The filter context?
+	
+	protected function perform(){
+		// IDEA Include recently used objects in the autosuggest results. But where can we get those object from?
+		// Another window context? The filter context?
+		
+		// This IF makes sure, the autosuggest works even if the calling widget is not specified. 
+		// TODO This is a potential security issue as an attacker could get access to some data (UIDs and LABELs) 
+		// even without his access to a specific page being checked. He would only need to have access to any one page 
+		// (the login page?). Once we have some kind of reading access control for meta objects, this code should be
+		// rewritten.
+		// IDEA Once there is some kind of default table widget for meta object, we could use it here instead of
+		// simply outputting the UID and LABEL
+		if (!$this->get_called_by_widget() && $this->get_called_on_ui_page()){
+			/* @var $reading_widget \exface\Core\Widgets\DataTable */
+			$reading_widget = WidgetFactory::create($this->get_called_on_ui_page(), 'DataTable');
+			$reading_widget->set_meta_object($this->get_meta_object());
+			$reading_widget->add_column($reading_widget->create_column_from_attribute($this->get_meta_object()->get_label_attribute()));
+			$this->set_called_by_widget($reading_widget);
+			$this->set_input_data_sheet($reading_widget->prepare_data_sheet_to_read($this->get_input_data_sheet()));
+		}
+		
+		return parent::perform();
+	}
 }
 ?>
