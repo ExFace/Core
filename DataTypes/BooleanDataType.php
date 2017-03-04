93d92462c23c6d9deadeb0633c13c03911361ba2
Andrej Kabachnik <andrej_ka@gmx.de>
2017-03-04 21:37:27 +0100

    FIX Boolean validator now accepts NULL values

--- DataTypes/BooleanDataType.php
+++ DataTypes/BooleanDataType.php
@@ -3,6 +3,7 @@ namespace exface\Core\DataTypes;
 class BooleanDataType extends AbstractDataType {
 	
 	public static function parse($string){
+		if (is_null($string)) return null;
 		return filter_var($string, FILTER_VALIDATE_BOOLEAN);
 	}
 }
