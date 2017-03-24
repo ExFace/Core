60a0da26d8ef1f45d9f990202e4ac39d1caabe97
Andrej Kabachnik <andrej_ka@gmx.de>
2017-03-24 15:40:13 +0100

    NEW exception TemplateOutputError

---
+++ Exceptions/Templates/TemplateOutputError.php
@@ -0,0 +1,13 @@
+<?php namespace exface\Core\Exceptions\Templates;
+
+use exface\Core\Exceptions\RuntimeException;
+
+/**
+ * Exception thrown if the template fails render the action output.
+ *
+ * @author Andrej Kabachnik
+ *
+ */
+class TemplateOutputError extends RuntimeException {
+	
+}
