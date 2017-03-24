b68a35efb8ec1c53410d33ccdb9d17f423539871
Andrej Kabachnik <andrej_ka@gmx.de>
2017-03-24 15:39:57 +0100

    FIX Progressbar formula documentation

--- Formulas/Progressbar.php
+++ Formulas/Progressbar.php
@@ -1,18 +1,38 @@
 <?php namespace exface\Core\Formulas;
 
+use exface\Core\CommonLogic\Model\Formula;
+use exface\Core\DataTypes\NumberDataType;
+
 /**
- * Creates an HTML-Progressbar taking the text, value and a max value as parameters.
+ * Creates an HTML-Progressbar taking a value, an optional text and some customization-options (a min-max-range and a color map) as parameters.
+ * 
+ * The value is used to determine the progress, the text will be displayed on-top of the progress bar. If the text is empty, the value will
+ * be displayed. The value must start with a number! Non-numeric characters following the number will be ignored. Thus, alphanumeric values
+ * like "80%" may be used (resulting in value=80 and text="80%").
+ * 
+ * The color map will paint the progress bar in different colors depending on the value. The following example (assuming min=0 and max=99)
+ * would make values between 0 and 30 green, then upto 79 yellow, value 80 would be red and all the following values gill be grey. Colors
+ * can be defined in any format compatible with the CSS color attribute. 
+ * [
+ * 	30: "green",
+ * 	79: "yellow",
+ * 	80: "red",
+ * 	99: "grey"
+ * ]
+ * 
  * @author Andrej Kabachnik
  *
  */
-class Progressbar extends \exface\Core\CommonLogic\Model\Formula {
+class Progressbar extends Formula {
 	
-	function run($caption, $value, $max=100){
-		if (!$value) $value = $caption;
+	function run($value, $text='', $min=0, $max=100, $color_map=array()){
 		if (!$value) return '';
+		if (!$text) $text = $value;
+		$value = NumberDataType::parse($value);
+		// TODO make the color map work (currently just a demo for value = 90)
 		$return = '<div style="width:100%;border:1px solid #ccc;position:relative;">' .
-    			'<div style="width:' . ($value ? $value / $max * 100 : 0) . '%;background:#' . ($value == 90 ? 'ddd' : 'BFD297') . ';">&nbsp;</div>' .
-				'<div style="position:absolute; left:0; top:0; z-index:100; padding:0 3px; width:100%">' . $caption . '</div>' .
+    			'<div style="width:' . ($value ? $value / ($max-$min) * 100 : $min) . '%;background:#' . ($value >= 90 ? 'ddd' : 'BFD297') . ';">&nbsp;</div>' .
+				'<div style="position:absolute; left:0; top:0; z-index:100; padding:0 3px; width:100%">' . $text . '</div>' .
     			'</div>';
         return $return;
 	}
