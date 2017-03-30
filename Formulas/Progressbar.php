576275c1130d241502aaf87180b5db82f022ce26
Thomas Walter <thomas.walter@salt-solutions.de>
2017-03-30 11:18:16 +0200

    implement color map handling in ProgressBar formular

--- Formulas/Progressbar.php
+++ Formulas/Progressbar.php
@@ -5,36 +5,80 @@ use exface\Core\DataTypes\NumberDataType;
 
 /**
  * Creates an HTML-Progressbar taking a value, an optional text and some customization-options (a min-max-range and a color map) as parameters.
- * 
+ *
  * The value is used to determine the progress, the text will be displayed on-top of the progress bar. If the text is empty, the value will
  * be displayed. The value must start with a number! Non-numeric characters following the number will be ignored. Thus, alphanumeric values
  * like "80%" may be used (resulting in value=80 and text="80%").
- * 
+ *
  * The color map will paint the progress bar in different colors depending on the value. The following example (assuming min=0 and max=99)
  * would make values between 0 and 30 green, then upto 79 yellow, value 80 would be red and all the following values gill be grey. Colors
- * can be defined in any format compatible with the CSS color attribute. 
+ * can be defined in any format compatible with the CSS color attribute.
  * [
- * 	30: "green",
- * 	79: "yellow",
- * 	80: "red",
- * 	99: "grey"
+ *    30: "#0a0",
+ *    79: "yellow",
+ *    80: "red",
+ *    99: "grey"
  * ]
- * 
+ *
  * @author Andrej Kabachnik
  *
  */
-class Progressbar extends Formula {
-	
-	function run($value, $text='', $min=0, $max=100, $color_map=array()){
-		if (!$value) return '';
-		if (!$text) $text = $value;
-		$value = NumberDataType::parse($value);
-		// TODO make the color map work (currently just a demo for value = 90)
-		$return = '<div style="width:100%;border:1px solid #ccc;position:relative;">' .
-    			'<div style="width:' . ($value ? $value / ($max-$min) * 100 : $min) . '%;background:#' . ($value >= 90 ? 'ddd' : 'BFD297') . ';">&nbsp;</div>' .
-				'<div style="position:absolute; left:0; top:0; z-index:100; padding:0 3px; width:100%">' . $text . '</div>' .
-    			'</div>';
+class Progressbar extends Formula
+{
+    /**
+     * @param int $value
+     * @param string $text
+     * @param int $min
+     * @param int $max
+     * @param array $colorMap
+     *
+     * @return string
+     */
+    function run($value, $text = '', $min = 0, $max = 100, $colorMap = array(30 => "#0a0", 79 => "yellow", 80 => "red", 99 => "grey"))
+    {
+        if (!$value) return '';
+        if (!$text) $text = $value;
+        $value = NumberDataType::parse($value);
+
+        $return = '<div style="width:100%; border:1px solid #ccc; position:relative; overflow: hidden">' .
+            '<div style="width:' . ($value ? $value / ($max - $min) * 100 : $min) . '%;background:' . $this->getBackgroundColor($value, $colorMap) . ';">&nbsp;</div>' .
+            '<div style="position:absolute; left:0; top:0; z-index:100; padding:0 3px; width:100%">' . $text . '</div>' .
+            '</div>';
         return $return;
-	}
+    }
+
+    /**
+     * Returns color corresponding to $value.
+     *
+     * @param int $value
+     * @param array $colorMap
+     *
+     * @return string
+     */
+    protected function getBackgroundColor($value, $colorMap)
+    {
+        return $colorMap[$this->getBackgroundColorKey($value, $colorMap)];
+    }
+
+    /**
+     * Find the matching colorMap key.
+     *
+     * @param int $value
+     * @param array $colorMap
+     *
+     * @return int
+     */
+    protected function getBackgroundColorKey($value, $colorMap)
+    {
+        $colorKeys = array_keys($colorMap);
+        asort($colorKeys);
+        foreach ($colorKeys as $colorKey) {
+            if ($value <= $colorKey)
+                return $colorKey;
+        }
+
+        return max($colorKeys);
+    }
 }
+
 ?>
