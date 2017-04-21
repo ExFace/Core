2a972e0b36b1bb5c3d9ac505a5a3edff8517bfab
Thomas Walter <thomas.walter@salt-solutions.de>
2017-04-21 16:32:21 +0200

    exception logging

---
+++ Log/Log.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace exface\Core\Log;
+
+
+use exface\Core\CommonLogic\Filemanager;
+use exface\Core\Interfaces\LoggerInterface;
+use exface\Core\Log\Handlers\DebugMessageFileHandler;
+use exface\Core\Log\Handlers\LogfileHandler;
+
+class Log
+{
+    /**
+     * Creates a LoggerInterface implementation if none is there yet and returns it after adding default
+     * LogHandlerInterface instances to it.
+     *
+     * @return LoggerInterface
+     */
+    public static function getDefaultLogger()
+    {
+        $logger = new Logger();
+
+        $coreLogPath = static::getCoreLogPath();
+	    $detailsLogPath = static::getDetailsLogPath();
+
+        $logger->pushHandler(new LogfileHandler("exface", $coreLogPath));
+        // TODO tvw enable when log details are used
+//        $logger->pushHandler(new DebugMessageFileHandler($detailsLogPath));
+
+        return $logger;
+    }
+
+	private static function getCoreLogPath() {
+		global $exface;
+		$workbench = $exface->ui()->get_workbench();
+
+		$basePath = Filemanager::path_normalize($workbench->filemanager()->get_path_to_base_folder());
+
+		$obj = $workbench->model()->get_object('exface.Core.LOG');
+		$relativePath = $obj->get_data_address();
+
+		return $basePath . '/' . $relativePath;
+	}
+
+	private static function getDetailsLogPath() {
+		global $exface;
+		$workbench = $exface->ui()->get_workbench();
+
+		$basePath = Filemanager::path_normalize($workbench->filemanager()->get_path_to_base_folder());
+
+		$obj = $workbench->model()->get_object('exface.Core.LOGDETAILS');
+		$relativePath = $obj->get_data_address();
+
+		return $basePath . '/' . $relativePath;
+	}
+}
---
+++ Log/Logger.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace exface\Core\Log;
+
+
+use exface\Core\Exceptions\LogicException;
+use exface\Core\Interfaces\iCanGenerateDebugWidgets;
+use exface\Core\Interfaces\LoggerInterface;
+use exface\Core\Interfaces\LogHandlerInterface;
+use exface\Core\Log\Handlers\LogfileHandler;
+
+class Logger implements LoggerInterface
+{
+    /** @var LogHandlerInterface[] $handlers */
+    private $handlers = array();
+
+    /**
+     * System is unusable.
+     *
+     * @param string $message
+     * @param array $context
+     * @param iCanGenerateDebugWidgets $sender
+     *
+     * @return void
+     */
+    public function emergency($message, array $context = array(), iCanGenerateDebugWidgets $sender = null)
+    {
+        $this->log(LogHandlerInterface::EMERGENCY, $message, $context, $sender);
+    }
+
+    /**
+     * Action must be taken immediately.
+     *
+     * Example: Entire website down, database unavailable, etc. This should
+     * trigger the SMS alerts and wake you up.
+     *
+     * @param string $message
+     * @param array $context
+     * @param iCanGenerateDebugWidgets $sender
+     *
+     * @return void
+     */
+    public function alert($message, array $context = array(), iCanGenerateDebugWidgets $sender = null)
+    {
+        $this->log(LogHandlerInterface::ALERT, $message, $context, $sender);
+    }
+
+    /**
+     * Critical conditions.
+     *
+     * Example: Application component unavailable, unexpected exception.
+     *
+     * @param string $message
+     * @param array $context
+     * @param iCanGenerateDebugWidgets $sender
+     *
+     * @return void
+     */
+    public function critical($message, array $context = array(), iCanGenerateDebugWidgets $sender = null)
+    {
+        $this->log(LogHandlerInterface::CRITICAL, $message, $context, $sender);
+    }
+
+    /**
+     * Runtime errors that do not require immediate action but should typically
+     * be logged and monitored.
+     *
+     * @param string $message
+     * @param array $context
+     * @param iCanGenerateDebugWidgets $sender
+     *
+     * @return void
+     */
+    public function error($message, array $context = array(), iCanGenerateDebugWidgets $sender = null)
+    {
+        $this->log(LogHandlerInterface::ERROR, $message, $context, $sender);
+    }
+
+    /**
+     * Exceptional occurrences that are not errors.
+     *
+     * Example: Use of deprecated APIs, poor use of an API, undesirable things
+     * that are not necessarily wrong.
+     *
+     * @param string $message
+     * @param array $context
+     * @param iCanGenerateDebugWidgets $sender
+     *
+     * @return void
+     */
+    public function warning($message, array $context = array(), iCanGenerateDebugWidgets $sender = null)
+    {
+        $this->log(LogHandlerInterface::WARNING, $message, $context, $sender);
+    }
+
+    /**
+     * Normal but significant events.
+     *
+     * @param string $message
+     * @param array $context
+     * @param iCanGenerateDebugWidgets $sender
+     *
+     * @return void
+     */
+    public function notice($message, array $context = array(), iCanGenerateDebugWidgets $sender = null)
+    {
+        $this->log(LogHandlerInterface::NOTICE, $message, $context, $sender);
+    }
+
+    /**
+     * Interesting events.
+     *
+     * Example: User logs in, SQL logs.
+     *
+     * @param string $message
+     * @param array $context
+     * @param iCanGenerateDebugWidgets $sender
+     *
+     * @return void
+     */
+    public function info($message, array $context = array(), iCanGenerateDebugWidgets $sender = null)
+    {
+        $this->log(LogHandlerInterface::INFO, $message, $context, $sender);
+    }
+
+    /**
+     * Detailed debug information.
+     *
+     * @param string $message
+     * @param array $context
+     * @param iCanGenerateDebugWidgets $sender
+     *
+     * @return void
+     */
+    public function debug($message, array $context = array(), iCanGenerateDebugWidgets $sender = null)
+    {
+        $this->log(LogHandlerInterface::DEBUG, $message, $context, $sender);
+    }
+
+    /**
+     * Logs with an arbitrary level.
+     *
+     * @param mixed $level
+     * @param string $message
+     * @param array $context
+     * @param iCanGenerateDebugWidgets $sender
+     *
+     * @return void
+     */
+    public function log($level, $message, array $context = array(), iCanGenerateDebugWidgets $sender = null)
+    {
+        if (!$this->handlers)
+            $this->pushHandler(new LogfileHandler("exface", "/home/tvw/public_html/alexa-ui/exface/exface/logs/core.log"), $level);
+
+        foreach ($this->handlers as $handler)
+            $handler->handle($level, $message, $context, $sender);
+    }
+
+    /**
+     * Pushes a handler on to the stack.
+     *
+     * @param LogHandlerInterface $handler
+     * @return LoggerInterface
+     */
+    public function pushHandler(LogHandlerInterface $handler)
+    {
+        $this->handlers[] = $handler;
+
+        return $this;
+    }
+
+    /**
+     * Pops a handler from the top of the stack and returns it
+     *
+     * @return LogHandlerInterface
+     */
+    public function popHandler()
+    {
+        if (!$this->handlers) {
+            throw new LogicException('Can not pop handler from an empty handler stack.');
+        }
+
+        return array_shift($this->handlers);
+    }
+
+    /**
+     * Returns a numeric array with all log handlers currently registered
+     *
+     * @return LogHandlerInterface[]
+     */
+    public function getHandlers()
+    {
+        return $this->handlers;
+    }
+}
