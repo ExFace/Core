d58582b451c481dd355e48ed5e115fbaf51e6751
Thomas Walter <thomas.walter@salt-solutions.de>
2017-05-08 11:11:29 +0200

    LOG.MINIMUM_LEVEL_TO_LOG configuration value used in log handlers

--- CommonLogic/Log/Handlers/DebugMessageFileHandler.php
+++ CommonLogic/Log/Handlers/DebugMessageFileHandler.php
@@ -8,36 +8,40 @@ use exface\Core\Interfaces\iCanGenerateDebugWidgets;
 use exface\Core\Interfaces\LogHandlerInterface;
 use exface\Core\Widgets\DebugMessage;
 use FemtoPixel\Monolog\Handler\CsvHandler;
+use Monolog\Logger;
 
 class DebugMessageFileHandler implements LogHandlerInterface {
 	private $dir;
+	private $minLogLevel;
 
 	/**
 	 * DebugMessageFileHandler constructor.
 	 *
 	 * @param $dir
+	 * @param $minLogLevel
 	 */
-	function __construct( $dir ) {
+	function __construct($dir, $minLogLevel = Logger::DEBUG) {
 		$this->dir = $dir;
+		$this->minLogLevel = $minLogLevel;
 	}
 
-	public function handle( $level, $message, array $context = array(), iCanGenerateDebugWidgets $sender = null ) {
+	public function handle($level, $message, array $context = array(), iCanGenerateDebugWidgets $sender = null) {
 		$fileName = $context["id"];
-		if ( ! $fileName ) {
+		if ( ! $fileName) {
 			return;
 		}
 
-		$logger = new \Monolog\Logger( "Stacktrace" );
-		$logger->pushHandler( new CsvHandler( $this->dir . "/" . $fileName, $level ) );
+		$logger = new \Monolog\Logger("Stacktrace");
+		$logger->pushHandler(new CsvHandler($this->dir . "/" . $fileName, $this->minLogLevel));
 
-		$debugWidget = $sender->create_debug_widget( $this->createDebugMessage() );
-		$logger->error( $debugWidget->export_uxon_object()->to_json(), $this->prepareContext( $context ) );
+		$debugWidget = $sender->create_debug_widget($this->createDebugMessage());
+		$logger->log($level, $debugWidget->export_uxon_object()->to_json(), $this->prepareContext($context));
 	}
 
-	protected function prepareContext( $context ) {
+	protected function prepareContext($context) {
 		// do not log the exception in this handler
-		if ( isset( $context["exception"] ) ) {
-			unset( $context["exception"] );
+		if (isset($context["exception"])) {
+			unset($context["exception"]);
 		}
 
 		return $context;
@@ -46,10 +50,10 @@ class DebugMessageFileHandler implements LogHandlerInterface {
 	protected function createDebugMessage() {
 		global $exface;
 		$ui   = $exface->ui();
-		$page = UiPageFactory::create_empty( $ui );
+		$page = UiPageFactory::create_empty($ui);
 
-		$debugMessage = new DebugMessage( $page );
-		$debugMessage->set_meta_object( $page->get_workbench()->model()->get_object( 'exface.Core.ERROR' ) );
+		$debugMessage = new DebugMessage($page);
+		$debugMessage->set_meta_object($page->get_workbench()->model()->get_object('exface.Core.ERROR'));
 
 		return $debugMessage;
 	}
