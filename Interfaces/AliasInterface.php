6b78659cc59aa063bb17028648f81a51cd11d711
Andrej Kabachnik <andrej_ka@gmx.de>
2017-06-08 08:56:45 +0200

    FIX comments

--- Interfaces/AliasInterface.php
+++ Interfaces/AliasInterface.php
@@ -3,11 +3,26 @@ namespace exface\Core\Interfaces;
 
 interface AliasInterface
 {
-
+    
+    /**
+     * Returns the alias of this instance (e.g. "my_object" for "my.app.my_object")
+     * 
+     * @return string
+     */
     public function getAlias();
-
+    
+    /**
+     * Returns the alias of this instance with the respective namespace (e.g. "my.app.my_object" for "my.app.my_object")
+     *
+     * @return string
+     */
     public function getAliasWithNamespace();
-
+    
+    /**
+     * Returns the namespace of this instance (e.g. "my.app" for "my.app.my_object")
+     *
+     * @return string
+     */
     public function getNamespace();
 }
 ?>
