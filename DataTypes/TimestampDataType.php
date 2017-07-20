37654843b5106d565c9f0cadad056dfce40bd183
Andrej Kabachnik <andrej_ka@gmx.de>
2017-07-20 16:43:10 +0200

    FIX time got lost when saving timestamps to data source

--- DataTypes/TimestampDataType.php
+++ DataTypes/TimestampDataType.php
@@ -4,7 +4,7 @@ namespace exface\Core\DataTypes;
 class TimestampDataType extends DateDataType
 {    
     public static function formatDate(\DateTime $date){
-        return $date->format('Y-m-d');
+        return $date->format('Y-m-d H:i:s');
     }
 }
 ?>
