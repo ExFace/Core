0739a2a4e43e5b4c621a37174de70f8c3f2c800e
Andrej Kabachnik <andrej_ka@gmx.de>
2017-10-05 19:18:32 +0200

    NEW option ignore_related_objects_in_input_data for CreateData action

--- Actions/CreateData.php
+++ Actions/CreateData.php
@@ -4,13 +4,24 @@ namespace exface\Core\Actions;
 use exface\Core\Interfaces\Actions\iCreateData;
 use exface\Core\Exceptions\Actions\ActionUndoFailedError;
 use exface\Core\Interfaces\DataSources\DataTransactionInterface;
+use exface\Core\DataTypes\BooleanDataType;
 
 class CreateData extends SaveData implements iCreateData
 {
+    private $ingnore_related_objects_in_input_data = false;
 
     protected function perform()
     {
         $data_sheet = $this->getInputDataSheet();
+        
+        if ($this->getIgnoreRelatedObjectsInInputData()){
+            foreach ($data_sheet->getColumns() as $col){
+                if ($col->getExpressionObj()->isMetaAttribute() && ! $col->getAttribute()->getRelationPath()->isEmpty()){
+                    $data_sheet->getColumns()->remove($col);
+                }
+            }
+        }
+        
         $this->setAffectedRows($data_sheet->dataCreate(true, $this->getTransaction()));
         $this->setUndoDataSheet($data_sheet);
         $this->setResultDataSheet($data_sheet);
@@ -27,6 +38,29 @@ class CreateData extends SaveData implements iCreateData
         }
         $data_sheet->dataDelete($transaction ? $transaction : $this->getTransaction());
         return $data_sheet;
+    }
+    
+    public function getIgnoreRelatedObjectsInInputData()
+    {
+        return $this->ingnore_related_objects_in_input_data;
+    }
+    
+    /**
+     * Strips off all columns with relations from the input data sheet before creating - if set to TRUE.
+     * 
+     * This is usefull if you have related columns in your data for some reason,
+     * but do not want to update them when creating new data rows.
+     * 
+     * @uxon-property ignore_related_objects_in_input_data
+     * @uxon-type boolean
+     * 
+     * @param boolean $true_or_false
+     * @return \exface\Core\Actions\CreateData
+     */
+    public function setIgnoreRelatedObjectsInInputData($true_or_false)
+    {
+        $this->ingnore_related_objects_in_input_data = BooleanDataType::parse($true_or_false);
+        return $this;
     }
 }
 ?>
