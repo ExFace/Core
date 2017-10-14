ad2b0b83b5ea3e7438e680b0a2a6617bc724c3c9
Andrej Kabachnik <andrej_ka@gmx.de>
2017-10-14 21:48:42 +0200

    NEW translation key group LOCALIZATION for time and number formats

--- Formulas/Now.php
+++ Formulas/Now.php
@@ -8,7 +8,7 @@ class Now extends \exface\Core\CommonLogic\Model\Formula
     {
         $exface = $this->getWorkbench();
         if (! $format)
-            $format = $exface->getCoreApp()->getTranslator()->translate('GLOBAL.DEFAULT_DATETIME_FORMAT');
+            $format = $exface->getCoreApp()->getTranslator()->translate('LOCALIZATION.DATE.DATETIME_FORMAT');
         $date = new \DateTime();
         return $date->format($format);
     }
