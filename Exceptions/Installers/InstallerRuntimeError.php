2be6d7a0fe7f118987e4d13a3f30454cc1428596
Andrej Kabachnik <andrej_ka@gmx.de>
2017-11-10 10:20:48 +0100

    NEW exception class InstallerRuntimeError

---
+++ Exceptions/Installers/InstallerRuntimeError.php
@@ -0,0 +1,47 @@
+<?php
+namespace exface\Core\Exceptions\Installers;
+
+use exface\Core\Exceptions\RuntimeException;
+use exface\Core\Interfaces\Exceptions\InstallerExceptionInterface;
+use exface\Core\Interfaces\InstallerInterface;
+
+class InstallerRuntimeError extends RuntimeException implements InstallerExceptionInterface {
+    
+    private $installer = null;
+    
+    /**
+     * 
+     * @param InstallerInterface $installer
+     * @param string $message
+     * @param string $code
+     * @param \Throwable $previous
+     */
+    public function __construct(InstallerInterface $installer, $message, $alias = null, $previous = null)
+    {
+        parent::__construct($message, null, $previous);
+        $this->setAlias($alias);
+        $this->setInstaller($installer);
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\Interfaces\Exceptions\InstallerExceptionInterface::getInstaller()
+     */
+    public function getInstaller()
+    {
+        return $this->installer;
+    }
+
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\Interfaces\Exceptions\InstallerExceptionInterface::setInstaller()
+     */
+    public function setInstaller($installer)
+    {
+        $this->installer = $installer;
+        return $this;
+    }
+
+}
