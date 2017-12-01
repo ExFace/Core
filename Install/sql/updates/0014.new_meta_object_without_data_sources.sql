4cb32871894faf38315eb94534de97676abbe5da
Andrej Kabachnik <andrej_ka@gmx.de>
2017-12-01 10:54:36 +0100

    FIX update problems with objects without data sources

--- Install/sql/updates/0014.new_meta_object_without_data_sources.sql
+++ Install/sql/updates/0014.new_meta_object_without_data_sources.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `exf_object` 
 	CHANGE `data_address` `data_address` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT 'Where the object is located in the data source (e.g. table in SQL)', 
 	CHANGE `data_source_oid` `data_source_oid` BINARY(16) NULL;
+	
+UPDATE `exf_attribute` set attribute_required_flag = 0 WHERE oid = 0x31303000000000000000000000000000;
