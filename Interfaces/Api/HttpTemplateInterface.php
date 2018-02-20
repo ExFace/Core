dcce0fe2f0bf9bf694032d8cd2249154d0981047
Andrej Kabachnik <andrej_ka@gmx.de>
2018-02-20 13:05:52 +0100

    NEW interface HttpTemplateInterface

---
+++ Interfaces/Api/HttpTemplateInterface.php
@@ -0,0 +1,15 @@
+<?php
+namespace exface\Core\Interfaces\Api;
+
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
+interface HttpTemplateInterface extends TemplateInterface, RequestHandlerInterface
+{
+    
+}
+?>
