79a43c5084e2a5310a8f9ce86ce632541f38cf73
Andrej Kabachnik <andrej_ka@gmx.de>
2018-02-23 16:53:34 +0100

    FIX multiple filter-refs in master-detail sporadically not working

--- Templates/AbstractAjaxTemplate/Elements/JqueryDataConfiguratorTrait.php
+++ Templates/AbstractAjaxTemplate/Elements/JqueryDataConfiguratorTrait.php
@@ -20,10 +20,14 @@ trait JqueryDataConfiguratorTrait
         $widget = $this->getWidget();
         $data_element = null;
         foreach ($widget->getFilters() as $filter) {
+            // For each filter with auto-apply trigger a refresh once the value of the filter changes.
             if ($filter->getApplyOnChange()) {
                 $data_element = is_null($data_element) ? $this->getTemplate()->getElement($widget->getWidgetConfigured()) : $data_element;
                 $filter_element = $this->getTemplate()->getElement($filter);
-                $filter_element->addOnChangeScript($data_element->buildJsRefresh());
+                // Wrap the refresh in setTimeout() to make sure multiple filter can get their values before
+                // one of the actually triggers the refresh. This also solved a strange bug, where the refresh
+                // did not start with the first value change, but only with the second one an onwards.
+                $filter_element->addOnChangeScript('setTimeout(function(){' . $data_element->buildJsRefresh() . '}, 50)');
             }
         }
         
