ef150a74814765453ce10ba6a3ca36f1104de4a4
aka <andrej.kabachnik@salt-solutions.de>
2018-04-10 13:59:41 +0200

    FIX installing api.php && default .htaccess

--- CoreInstaller.php
+++ CoreInstaller.php
@@ -22,7 +22,11 @@ class CoreInstaller extends AbstractAppInstaller
     public function install($source_absolute_path)
     {
         $model_source_installer = $this->getWorkbench()->model()->getModelLoader()->getInstaller();
-        return $model_source_installer->install($this->getWorkbench()->filemanager()->getPathToVendorFolder() . DIRECTORY_SEPARATOR . $model_source_installer->getSelectorInstalling()->getFolderRelativePath());
+        $result = $model_source_installer->install($this->getWorkbench()->filemanager()->getPathToVendorFolder() . DIRECTORY_SEPARATOR . $model_source_installer->getSelectorInstalling()->getFolderRelativePath());
+        $result .= $this->copyDefaultHtaccess($source_absolute_path);
+        $result .= $this->createApiPhp($source_absolute_path);
+        $result .= $this->removeLegacyFiles($source_absolute_path);
+        return $result;
     }
 
     /**
@@ -36,6 +40,8 @@ class CoreInstaller extends AbstractAppInstaller
         $model_source_installer = $this->getWorkbench()->model()->getModelLoader()->getInstaller();
         $result = $model_source_installer->update($this->getWorkbench()->filemanager()->getPathToVendorFolder() . DIRECTORY_SEPARATOR . $model_source_installer->getSelectorInstalling()->getFolderRelativePath());
         $result .= $this->copyDefaultHtaccess($source_absolute_path);
+        $result .= $this->createApiPhp($source_absolute_path);
+        $result .= $this->removeLegacyFiles($source_absolute_path);
         return $result;
     }
 
@@ -61,7 +67,7 @@ class CoreInstaller extends AbstractAppInstaller
         return 'Backup not implemented for' . $this->getSelectorInstalling()->getAliasWithNamespace() . '!';
     }
     
-    protected function copyDefaultHtaccess($source_absolute_path)
+    protected function copyDefaultHtaccess(string $source_absolute_path) : string
     {
         $result = '';
         // Copy default .htaccess to the root of the installation
@@ -69,13 +75,54 @@ class CoreInstaller extends AbstractAppInstaller
         if (! file_exists($file)) {
             try {
                 $this->getWorkbench()->filemanager()->copy($this->getInstallFolderAbsolutePath($source_absolute_path) . DIRECTORY_SEPARATOR . '.htaccess', $file);
-                $result .= "\nGenerated default .htaccess file in plattform root";
+                $result .= "\nGenerated default .htaccess file in plattform root.";
             } catch (\Exception $e) {
-                $result .= "\nFailed to copy default .htaccss file: " . $e->getMessage() . ' in ' . $e->getFile() . ' at ' . $e->getLine();
+                $result .= "\nFailed to copy default .htaccss file: " . $e->getMessage() . ' in ' . $e->getFile() . ' at ' . $e->getLine() . '.';
             }
         }
         
         return $result;
+    }
+    
+    protected function createApiPhp(string $source_absolute_path) : string
+    {
+        $result = '';
+        // Copy default .htaccess to the root of the installation
+        $file = Filemanager::pathJoin([$this->getWorkbench()->getInstallationPath(), 'api.php']);
+        if (! file_exists($file)) {
+            $content = <<<PHP
+<?php 
+error_reporting(E_ALL & ~E_NOTICE);
+require_once('vendor/exface/Core/index.php');
+?>
+PHP;
+            try {
+                file_put_contents($file, $content);
+                $result .= "\nGenerated default api.php file in plattform root.";
+            } catch (\Exception $e) {
+                $result .= "\nFailed to copy default api.php file: " . $e->getMessage() . ' in ' . $e->getFile() . ' at ' . $e->getLine() . '.';
+            }
+        }
+        
+        return $result;
+    }
+    
+    protected function removeLegacyFiles(string $source_absolute_path) : string
+    {
+        $files = [
+            'exface.php'
+        ];
+        foreach ($files as $file) {
+            $filepath = Filemanager::pathJoin([$this->getWorkbench()->getInstallationPath(), $file]);
+            if (file_exists($filepath)) {
+                try {
+                    unlink($filepath);
+                } catch (\Throwable $e) {
+                    return "\n" . 'Could not remove legacy file "' . $filepath . '" - please delete the file manually!';
+                }
+            }
+        }
+        return '';
     }
 }
 ?>
