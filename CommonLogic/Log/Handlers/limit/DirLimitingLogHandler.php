66b7bf312bed5b34e0ce5a7868a470ab67f32f9e
Andrej Kabachnik <andrej_ka@gmx.de>
2018-04-17 12:18:43 +0200

    FIX filemtime errors when cleaning log details

--- CommonLogic/Log/Handlers/limit/DirLimitingLogHandler.php
+++ CommonLogic/Log/Handlers/limit/DirLimitingLogHandler.php
@@ -68,15 +68,15 @@ class DirLimitingLogHandler extends LimitingWrapper
         
         $limitTime = max(0, time() - ($this->maxDays * 24 * 60 * 60));
         $logFiles = glob(LogHelper::getPattern($this->logPath, $this->filenameFormat, '/*', $this->staticFileNamePart));
-        foreach ($logFiles as $logFile) {
-            $mtime = filemtime($logFile);
-            if ($mtime > $limitTime)
-                continue;
-            
+        foreach ($logFiles as $logFile) {            
             if (is_writable($logFile)) {
                 // suppress errors here as unlink() might fail if two processes
                 // are cleaning up/rotating at the same time
                 set_error_handler(function ($errno, $errstr, $errfile, $errline) {});
+                $mtime = filemtime($logFile);
+                if ($mtime > $limitTime) {
+                    continue;
+                }
                 unlink($logFile);
                 restore_error_handler();
             }
