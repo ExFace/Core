abbc133d6926c387126c33d9af6606643e371fab
aka <andrej.kabachnik@salt-solutions.de>
2018-09-24 16:56:06 +0200

    FIX prefill logic in widget DialogHeader

--- Widgets/DialogHeader.php
+++ Widgets/DialogHeader.php
@@ -2,6 +2,9 @@
 namespace exface\Core\Widgets;
 
 use exface\Core\DataTypes\BooleanDataType;
+use exface\Core\Interfaces\DataSheets\DataSheetInterface;
+use exface\Core\Interfaces\Model\MetaAttributeInterface;
+use exface\Core\Exceptions\Widgets\WidgetConfigurationError;
 
 /**
  * A dialog header is a special widget used to display a summary of a dialog - typically the object currently loaded.
@@ -46,14 +49,16 @@ class DialogHeader extends Form
      * 
      * @return string
      */
-    public function getTitleAttributeAlias()
+    public function getTitleAttributeAlias() : string
     {
-        if (is_null($this->title_attribute_alias)) {
+        if ($this->title_attribute_alias === null) {
             $object = $this->getMetaObject();
             if ($object->hasLabelAttribute()) {
                 $this->title_attribute_alias = $object->getLabelAttributeAlias();
             } elseif ($object->hasUidAttribute()) {
                 $this->title_attribute_alias = $object->getUidAttributeAlias();
+            } else {
+                throw new WidgetConfigurationError($this, 'Cannot determine attribute for a dynamic title of widget ' . $this->getWidgetType() . '!');
             }
         }
         return $this->title_attribute_alias;
@@ -63,7 +68,7 @@ class DialogHeader extends Form
      * 
      * @return \exface\Core\Interfaces\Model\MetaAttributeInterface
      */
-    public function getTitleAttribute()
+    public function getTitleAttribute() : MetaAttributeInterface
     {
         return $this->getMetaObject()->getAttribute($this->getTitleAttributeAlias());
     }
@@ -79,5 +84,63 @@ class DialogHeader extends Form
         return $this;
     }
     
+    public function isTitleBoundToAttribute() : bool
+    {
+        if ($this->title_attribute_alias !== null) {
+            return true;
+        }
+        
+        try {
+            $this->getTitleAttributeAlias();
+        } catch (WidgetConfigurationError $e) {
+            return false;
+        }
+        
+        return true;
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\Widgets\Container::prepareDataSheetToPrefill()
+     */
+    public function prepareDataSheetToPrefill(DataSheetInterface $dataSheet = null) : DataSheetInterface
+    {
+        $sheet = parent::prepareDataSheetToPrefill($dataSheet);
+        if (! $this->isTitleBoundToAttribute()) {
+            $sheet->getColumns()->addFromAttribute($this->getTitleAttribute());
+        }
+        return $sheet;
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\Widgets\Container::prepareDataSheetToRead()
+     */
+    public function prepareDataSheetToRead(DataSheetInterface $dataSheet = null) : DataSheetInterface
+    {
+        $sheet = parent::prepareDataSheetToRead($dataSheet);
+        if (! $this->isTitleBoundToAttribute()) {
+            $sheet->getColumns()->addFromAttribute($this->getTitleAttribute());
+        }
+        return $sheet;
+    }
+    
+    protected function doPrefill(DataSheetInterface $dataSheet)
+    {
+        if ($this->isTitleBoundToAttribute()) {
+            if ($col = $dataSheet->getColumns()->getByAttribute($this->getTitleAttribute())) {
+                $cnt = count(array_unique($col->getValues()));
+                if ($cnt > 1) {
+                    $this->setCaption($cnt . 'x ' . $this->getMetaObject()->getName());
+                } else {
+                    $this->setCaption($col->getCellValue(0));
+                }
+            }
+        }
+        
+        return parent::doPrefill($dataSheet);
+    }
 }
 ?>
