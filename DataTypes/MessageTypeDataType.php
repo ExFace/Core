25bb83a747fc3a5f538b4ab0fd00e4051e04f0e1
Andrej Kabachnik <andrej_ka@gmx.de>
2018-11-19 14:04:07 +0100

    REFACTOR improved message model editor + MessageTypeDataType

---
+++ DataTypes/MessageTypeDataType.php
@@ -0,0 +1,48 @@
+<?php
+namespace exface\Core\DataTypes;
+
+use exface\Core\CommonLogic\DataTypes\EnumStaticDataTypeTrait;
+use exface\Core\Interfaces\DataTypes\EnumDataTypeInterface;
+
+/**
+ * Enumeration for message types: error, info, warning, hint, success, etc.
+ * 
+ * @method MessageTypeDataType ERROR(\exface\Core\CommonLogic\Workbench $workbench)
+ * @method MessageTypeDataType WARNING(\exface\Core\CommonLogic\Workbench $workbench)
+ * @method MessageTypeDataType INFO(\exface\Core\CommonLogic\Workbench $workbench)
+ * @method MessageTypeDataType SUCCESS(\exface\Core\CommonLogic\Workbench $workbench)
+ * @method MessageTypeDataType HINT(\exface\Core\CommonLogic\Workbench $workbench)
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
+class MessageTypeDataType extends StringDataType implements EnumDataTypeInterface
+{
+    use EnumStaticDataTypeTrait;
+    
+    const ERROR = "ERROR";
+    const WARNING = "WARNING";
+    const INFO = "INFO";
+    const SUCCESS = "SUCCESS";
+    const HINT = "HINT";
+    
+    private $labels = [];
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\Interfaces\DataTypes\EnumDataTypeInterface::getLabels()
+     */
+    public function getLabels()
+    {
+        if (empty($this->labels)) {
+            $translator = $this->getWorkbench()->getCoreApp()->getTranslator();
+            
+            foreach (static::getValuesStatic() as $val) {
+                $this->labels[$val] = $translator->translate('MESSAGE.TYPES.' . $val);
+            }
+        }
+        
+        return $this->labels;
+    }
+}
