7774bf417c471ca45041253d00c287b93d829b60
Andrej Kabachnik <andrej_ka@gmx.de>
2018-11-29 10:22:41 +0100

    FIX SQL update 25

--- Install/sql/updates/0025.fix_empty_values_in_model.sql
+++ Install/sql/updates/0025.fix_empty_values_in_model.sql
@@ -5,13 +5,12 @@ ALTER TABLE `exf_message`
 	CHANGE COLUMN `title` `title` VARCHAR(250) NOT NULL AFTER `code`;
 
 UPDATE exf_attribute set default_editor_uxon = null where default_editor_uxon = '{}';
-UPDATE exf_object set default_editor_uxon = null where default_editor_uxon = '';
 UPDATE exf_attribute set default_display_uxon = null where default_display_uxon = '{}';
 UPDATE exf_attribute set custom_data_type_uxon = null where custom_data_type_uxon = '{}';
-UPDATE exf_attribute set data_properties = null where data_properties = '{}';
+UPDATE exf_attribute set data_properties = null where data_properties = '{}' OR data_properties = '';
 
-UPDATE exf_object set default_editor_uxon = null where default_editor_uxon = '{}';
-UPDATE exf_object set data_address_properties = null where data_address_properties = '{}';
+UPDATE exf_object set default_editor_uxon = null where default_editor_uxon = '{}' OR default_editor_uxon = '';
+UPDATE exf_object set data_address_properties = null where data_address_properties = '{}' OR data_address_properties = '';
 
 UPDATE exf_data_type set config_uxon = null where config_uxon = '{}';
 UPDATE exf_data_type set default_editor_uxon = null where default_editor_uxon = '{}';
