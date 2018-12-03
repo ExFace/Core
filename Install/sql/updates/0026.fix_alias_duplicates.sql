a0b8445022ca9ae119c91075becceaef72bad692
Andrej Kabachnik <andrej_ka@gmx.de>
2018-12-03 15:00:06 +0100

    FIX SQL update #26: added another known duplicate alias

--- Install/sql/updates/0026.fix_alias_duplicates.sql
+++ Install/sql/updates/0026.fix_alias_duplicates.sql
@@ -7,6 +7,7 @@ DELETE FROM exf_attribute WHERE oid = 0x11e8ebd57f86110587f10205857feb81;
 UPDATE exf_attribute SET attribute_alias = 'LABEL' WHERE oid = 0x36350000000000000000000000000000;
 UPDATE exf_attribute SET attribute_alias = 'LABEL' WHERE oid = 0x11e84eeb4835bd3db1870205857feb80;
 UPDATE exf_attribute SET attribute_alias = 'LABEL' WHERE oid = 0x11e7ce85ac25ec20a3da0205857feb80;
+UPDATE exf_attribute SET attribute_alias = 'BUSINESS_UNIT' WHERE oid = 0x37380000000000000000000000000000;
 UPDATE exf_data_type SET data_type_alias = 'GenericNumberEnum' WHERE oid = 0x11e7c39621725c1e8001e4b318306b9a;
 
 /* Now add the indexes */
