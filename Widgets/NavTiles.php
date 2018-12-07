9180253744038e947787ac5f11ebb958f6953b59
Andrej Kabachnik <andrej_ka@gmx.de>
2018-12-07 14:24:28 +0100

    FIX various issues in NavTiles widget

--- Widgets/NavTiles.php
+++ Widgets/NavTiles.php
@@ -9,14 +9,30 @@ use exface\Core\Exceptions\Widgets\WidgetConfigurationError;
 use exface\Core\Factories\WidgetFactory;
 use exface\Core\CommonLogic\UxonObject;
 use exface\Core\Interfaces\Widgets\iContainOtherWidgets;
-use exface\Core\Factories\ConditionFactory;
-use exface\Core\Factories\ConditionGroupFactory;
 
 /**
- * NavTiles show a navigational tile menu starting from a given parent page.
+ * NavTiles show a hierarchical navigational tile menu starting from a given parent page.
  * 
- * This a simple shortcut to create tile menus - much quicker, than building
- * them manually via separate tiles.
+ * NavTiles produce a flat menu consisting of grouped tiles (= multiple Tiles widgets).
+ * Each tile navigates to a child page of the menu root when clicked.
+ * 
+ * NavTiles are a great choice for folder pages, that do not have any meaningful widgets.
+ * Just place the following short UXON code in the folder page:
+ * 
+ * ```
+ * {
+ *  "widget_type": "NavTiles",
+ *  "object_alias": "exface.Core.PAGE"
+ * }
+ * 
+ * ```
+ * 
+ * Using the optional `depth` property, you can control, how many levels of the menu will 
+ * be listed. By default, two levels are listed, which is a good choice for menus with
+ * not too many pages on each level.
+ * 
+ * The visual representation, as allways, depends on the template. A typical visualization
+ * is something like the Windows 8 start menu: tiles are organized in groups.
  * 
  * @method Tiles[] getWidgets()
  * @method Tiles getWidgetFirst()
@@ -36,6 +52,12 @@ class NavTiles extends WidgetGrid
     private $parentTileIds = [];
     
     /**
+     * Specifies the alias of the root page of the menu (i.e. tiles for it's children will be generated).
+     * 
+     * If not set explicitly, the current page will be used as root.
+     * 
+     * @uxon-property root_page_alias
+     * @uxon-type string
      * 
      * @param string $pageSelector
      * @return NavTiles
@@ -46,12 +68,21 @@ class NavTiles extends WidgetGrid
         return $this;
     }
     
+    /**
+     * 
+     * @param UiPageSelectorInterface $selector
+     * @return NavTiles
+     */
     public function setRootPageSelector(UiPageSelectorInterface $selector) : NavTiles
     {
         $this->rootPageSelector = $selector;
         return $this;
     }
     
+    /**
+     * 
+     * @return UiPageSelectorInterface|NULL
+     */
     public function getRootPageSelector() : ?UiPageSelectorInterface
     {
         if ($this->rootPageSelector === null) {
@@ -77,6 +108,14 @@ class NavTiles extends WidgetGrid
         return parent::getWidgets();
     }
     
+    /**
+     * 
+     * @param DataSheetInterface $pageSheet
+     * @param string $caption
+     * @param int $depth
+     * @param Tile $upperLevelTile
+     * @return Tiles
+     */
     protected function createTileGroup(DataSheetInterface $pageSheet, string $caption, int $depth, Tile $upperLevelTile = null) : Tiles
     {
         $tiles = WidgetFactory::create($this->getPage(), 'Tiles', $this);
@@ -87,13 +126,13 @@ class NavTiles extends WidgetGrid
             $tile = $this->createTileFromPageDataRow($row, $tiles);
             $tiles->addWidget($tile);
             if ($upperLevelTile !== null) {
-                $this->parentTileIds[$tile->getId()] = $upperLevelTile->getId();
+                $this->parentTileIds[$tile->getId()] = $upperLevelTile;
             }
             if ($depth > 1) {
                 $parentPageSelector = new UiPageSelector($this->getWorkbench(), $row['CMS_ID']);
                 $childrenSheet = $this->getMenuDataSheet($parentPageSelector);
                 if ($childrenSheet->isEmpty() === false) {
-                    $this->createTileGroup($childrenSheet, $row['NAME'], ($depth-1), $tile);
+                    $this->createTileGroup($childrenSheet, $caption . ' > ' . $row['NAME'], ($depth-1), $tile);
                 }
             }
         }
@@ -101,17 +140,12 @@ class NavTiles extends WidgetGrid
         return $tiles;
     }
     
-    protected function createTilesForParent(UiPageSelectorInterface $parentPageSelector, iContainOtherWidgets $container) : Tiles
-    {
-        $pageSheet = $this->getMenuDataSheet($parentPageSelector);
-        
-        foreach ($pageSheet->getRows() as $row) {
-            $tile = $this->createTileFromPageDataRow($row, $container);
-            $container->addWidget($tile);
-        }
-        return $container;
-    }
-    
+    /**
+     * 
+     * @param array $row
+     * @param iContainOtherWidgets $container
+     * @return Tile
+     */
     protected function createTileFromPageDataRow(array $row, iContainOtherWidgets $container) : Tile
     {
         /* @var $tile \exface\Core\Widgets\Tile */
@@ -131,7 +165,6 @@ class NavTiles extends WidgetGrid
     /**
      * 
      * @param UiPageSelectorInterface $parentPageSelector
-     * @param int $depth
      * @throws WidgetConfigurationError
      * @return DataSheetInterface
      */
@@ -172,6 +205,13 @@ class NavTiles extends WidgetGrid
     }
     
     /**
+     * Controls the depth of the menu (2 by default).
+     * 
+     * If set to 1, the tile menu will include tiles for the direct children of the root page
+     * and their children (i.e. 2 menu levels).
+     * 
+     * @uxon-property depth
+     * @uxon-type integer
      * 
      * @param int $value
      * @return NavTiles
@@ -183,6 +223,7 @@ class NavTiles extends WidgetGrid
     }
     
     /**
+     * Returns all tiles in a flat array
      * 
      * @param callable $filter
      * @return Tile[]
@@ -200,6 +241,15 @@ class NavTiles extends WidgetGrid
         return $tiles;
     }
     
+    /**
+     * Returns the tile, representing the parent page of the page of the given tile.
+     * 
+     * This method can be used to let tiles inherit properties (e.g. colors or icons)
+     * from the respective parent menu levels.
+     * 
+     * @param Tile $tile
+     * @return Tile|NULL
+     */
     public function getUpperLevelTile(Tile $tile) : ?Tile
     {
         return $this->parentTileIds[$tile->getId()];
