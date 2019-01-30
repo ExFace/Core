9ca9de6a8b57d753b48fac1769183f213d839a9e
aka <andrej.kabachnik@salt-solutions.de>
2019-01-30 12:13:47 +0100

    FIX improved error messages in InstallerRuntimeError

--- Exceptions/Installers/InstallerRuntimeError.php
+++ Exceptions/Installers/InstallerRuntimeError.php
@@ -4,6 +4,7 @@ namespace exface\Core\Exceptions\Installers;
 use exface\Core\Exceptions\RuntimeException;
 use exface\Core\Interfaces\Exceptions\InstallerExceptionInterface;
 use exface\Core\Interfaces\InstallerInterface;
+use exface\Core\Interfaces\AppInstallerInterface;
 
 class InstallerRuntimeError extends RuntimeException implements InstallerExceptionInterface {
     
@@ -18,6 +19,13 @@ class InstallerRuntimeError extends RuntimeException implements InstallerExcepti
      */
     public function __construct(InstallerInterface $installer, $message, $alias = null, $previous = null)
     {
+        if ($installer instanceof AppInstallerInterface) {
+            try {
+                $message = 'Error installing app ' . $installer->getApp()->getAliasWithNamespace() . '.' . $message;
+            } catch (\Throwable $e) {
+                // ignore errors! Just leave the message as it is then.
+            }
+        }
         parent::__construct($message, null, $previous);
         $this->setAlias($alias);
         $this->setInstaller($installer);
