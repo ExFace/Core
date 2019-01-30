438f3378e98f439c126461e13edc53853e8b514d
aka <andrej.kabachnik@salt-solutions.de>
2019-01-30 18:16:41 +0100

    REFACTOR added JqueryInputTrait

---
+++ Templates/AbstractAjaxTemplate/Elements/JqueryInputTrait.php
@@ -0,0 +1,47 @@
+<?php
+namespace exface\Core\Templates\AbstractAjaxTemplate\Elements;
+
+trait JqueryInputTrait
+{
+    /**
+     * Returns <input type="xxx"> with the appropriate name, value, id, etc.
+     * 
+     * The parameter $propertyValues can be used to override default HTML properties
+     * or add new ones: ['data-myprop' => 'myVal'] would result in data-myprop="myVal"
+     * within the <input>.
+     * 
+     * @param string $type
+     * @param string[] $propertyValues
+     * 
+     * @return string
+     */
+    protected function buildHtmlInput(string $type, array $propertyValues = []) : string
+    {
+        $defaults = [
+            'name' => $this->getWidget()->getAttributeAlias(),
+            'value' => $this->getValueWithDefaults(),
+            'id' => $this->getId()
+        ];
+        
+        $propArray = array_merge($defaults, $propertyValues);
+        $props = '';
+        foreach ($propArray as $p => $v) {
+            $props .= $p . '="' . $v . '" ';
+        }
+        
+        return '<input type="' . $type . '" ' . $props . ' />';
+    }
+    
+    /**
+     *
+     * {@inheritdoc}
+     * @see \exface\Core\Templates\AbstractAjaxTemplate\Elements\AbstractJqueryElement::buildJsValidator()
+     */
+    public function buildJsValidator()
+    {
+        if ($this->getWidget()->isRequired()) {
+            return '(' . $this->buildJsValueGetter() . ' === "" ? false : true)';
+        }
+        return 'true';
+    }
+}
