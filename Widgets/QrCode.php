19ca258df1c6a112dbe29b5400f1b37c871ea98d
aka <andrej.kabachnik@salt-solutions.de>
2019-02-11 12:45:53 +0100

    NEW widget QrCode

---
+++ Widgets/QrCode.php
@@ -0,0 +1,12 @@
+<?php
+namespace exface\Core\Widgets;
+
+/**
+ * This widget shows a QR code generated from it's value.
+ *
+ * @author Andrej Kabachnik
+ *        
+ */
+class QrCode extends Display
+{
+}
