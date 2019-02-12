eea7cdf63b83a85d15a669bcc17e303042dfd1df
aka <andrej.kabachnik@salt-solutions.de>
2019-02-12 11:21:56 +0100

    FIX auto-create Tile widgets in Tiles container

--- Widgets/Tiles.php
+++ Widgets/Tiles.php
@@ -2,6 +2,7 @@
 namespace exface\Core\Widgets;
 
 use exface\Core\CommonLogic\UxonObject;
+use exface\Core\Factories\WidgetFactory;
 
 /**
  * A special container for Tile widget - it makes it easier to create tile menus quickly.
@@ -29,6 +30,7 @@ class Tiles extends WidgetGrid
      * 
      * @uxon-property tiles
      * @uxon-type \exface\Core\Widgets\Tile[]
+     * @uxon-template [{"": ""}]
      * 
      * @param UxonObject $uxon
      * @return Tiles
@@ -36,5 +38,21 @@ class Tiles extends WidgetGrid
     public function setTiles(UxonObject $uxon) : Tiles
     {
         return $this->setWidgets($uxon);
+    }
+    
+    public function setWidgets($widget_or_uxon_array)
+    {
+        $widgets = array();
+        foreach ($widget_or_uxon_array as $w) {
+            if ($w instanceof UxonObject) {
+                // If we have a UXON or instantiated widget object, use the widget directly
+                $widgets[] = WidgetFactory::createFromUxon($this->getPage(), $w, $this, 'Tile');
+            } else {
+                // If it is something else, just add it to the result and let the parent object deal with it
+                $widgets[] = $w;
+            }
+        }
+        
+        return parent::setWidgets($widgets);
     }
 }
