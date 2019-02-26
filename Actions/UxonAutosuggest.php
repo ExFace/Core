63dc7633251bf6bcfbdd99ff30244fd5c4388006
Andrej Kabachnik <andrej_ka@gmx.de>
2019-02-26 12:44:18 +0100

    DOC added documentation for action UxonAutosuggest

--- Actions/UxonAutosuggest.php
+++ Actions/UxonAutosuggest.php
@@ -16,12 +16,50 @@ use exface\Core\DataTypes\StringDataType;
 use exface\Core\Exceptions\Model\MetaObjectNotFoundError;
 use exface\Core\Interfaces\Model\MetaObjectInterface;
 
+/**
+ * Returns autosuggest values for provided UXON objects.
+ * 
+ * Accepts the following parameters:
+ * 
+ * - `uxon` - the entire UXON being edited
+ * - `text` - the text, that needs to get suggested (i.e. what was typed so far)
+ * - `path` - the path to the currently edited node from the root of the UXON (a JSON array of node names)
+ * - `input` - the type of input: `field` or `value`
+ * - `schema` - the UXON schema to be used (if not set, the generic UxonSchema will be used).
+ * - `prototype` - class or file path (relative to the vendor folder) to be used as prototype
+ * - `object` - a selector for the root object of the UXON
+ * 
+ * The result is a JSON with the following structure
+ * 
+ * {
+ *  "values": [
+ *      "suggestion 1",
+ *      "suggestion 2",
+ *      "..."
+ *  ],
+ *  "templates": {
+ *      "field1": {"": ""},
+ *      "field2": ["": ""]
+ *  }
+ * }
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
 class UxonAutosuggest extends AbstractAction
 {
     const SCHEMA_WIDGET = 'widget';
     const SCHEMA_ACTION = 'action';
     const SCHEMA_BEHAVIOR = 'behavior';
     const SCHEMA_DATATYPE = 'datatype';
+    
+    const PARAM_TEXT = 'text';
+    const PARAM_PATH = 'path';
+    const PARAM_TYPE = 'input';
+    const PARAM_UXON = 'uxon';
+    const PARAM_SCHEMA = 'schema';
+    const PARAM_OBJECT = 'object';
+    const PARAM_PROTOTYPE = 'prototype';
     
     const TYPE_FIELD = 'field';
     const TYPE_VALUE = 'value';
@@ -30,13 +68,13 @@ class UxonAutosuggest extends AbstractAction
     {
         $options = [];
         
-        $currentText = $task->getParameter('text');
-        $path = json_decode($task->getParameter('path'), true);
-        $type = $task->getParameter('input');
-        $uxon = UxonObject::fromJson($task->getParameter('uxon'));
-        $schema = $task->getParameter('schema');
+        $currentText = $task->getParameter(self::PARAM_TEXT);
+        $path = json_decode($task->getParameter(self::PARAM_PATH), true);
+        $type = $task->getParameter(self::PARAM_TYPE);
+        $uxon = UxonObject::fromJson($task->getParameter(self::PARAM_UXON));
+        $schema = $task->getParameter(self::PARAM_SCHEMA);
         
-        if ($rootObjectSelector = $task->getParameter('object')) {
+        if ($rootObjectSelector = $task->getParameter(self::PARAM_OBJECT)) {
             try {
                 $rootObject = $this->getWorkbench()->model()->getObject($rootObjectSelector);
             } catch (MetaObjectNotFoundError $e) {
@@ -44,7 +82,7 @@ class UxonAutosuggest extends AbstractAction
             }
         }
         
-        if ($rootPrototypeSelector = trim($task->getParameter('prototype'))) {
+        if ($rootPrototypeSelector = trim($task->getParameter(self::PARAM_PROTOTYPE))) {
             if (StringDataType::endsWith($rootPrototypeSelector, '.php', false)) {
                 $rootPrototypeClass = str_replace("/", "\\", substr($rootPrototypeSelector, 0, -4));
                 $rootPrototypeClass = "\\" . ltrim($rootPrototypeClass, "\\");
