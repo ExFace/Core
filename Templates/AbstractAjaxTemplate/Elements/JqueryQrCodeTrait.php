9f5a22cee4208f13b4c60f40e4f94ecf886325e9
Andrej Kabachnik <andrej_ka@gmx.de>
2019-03-07 16:23:47 +0100

    NEW JqueryQrCodeTrait

---
+++ Templates/AbstractAjaxTemplate/Elements/JqueryQrCodeTrait.php
@@ -0,0 +1,81 @@
+<?php
+namespace exface\Core\Templates\AbstractAjaxTemplate\Elements;
+
+use exface\Core\Templates\AbstractAjaxTemplate\Interfaces\JsValueDecoratingInterface;
+use exface\Core\Widgets\QrCode;
+
+/**
+ *
+ * @method QrCode getWidget()
+ *        
+ * @author Andrej Kabachnik
+ *        
+ */
+trait JqueryQrCodeTrait
+{
+    
+    /**
+     * 
+     * @see AbstractJqueryElement::buildHtml()
+     */
+    public function buildHtml()
+    {
+        return $this->buildHtmlQrCode();
+    }
+
+    /**
+     *
+     * @see AbstractJqueryElement::buildJs()
+     */
+    public function buildJs()
+    {
+        return '$("#' . $this->getId() . '").children("canvas").remove(); $("#' . $this->getId() . '").qrcode("' . $this->getWidget()->getValueWithDefaults() . '");';
+    }
+    
+    /**
+     * Returns the <img> HTML tag with the given source.
+     * 
+     * @param string $src
+     * @return string
+     */
+    protected function buildHtmlQrCode()
+    {
+        $style = '';
+        if (! $this->getWidget()->getWidth()->isUndefined()) {
+            $style .= 'width:' . $this->getWidth() . '; ';
+        }
+        if (! $this->getWidget()->getHeight()->isUndefined()) {
+            $style .= 'height: ' . $this->getHeight() . '; ';
+        }
+        
+        $output = '<div class="exf-qrcode-wrapper"><div class="' . $this->buildCssElementClass() . '" style="' . $style . '" id="' . $this->getId() . '" /></div>';
+        return $output;
+    }
+    
+    /**
+     * {@inheritdoc}
+     * @see JsValueDecoratingInterface::buildJsValueDecorator
+     */
+    public function buildJsValueDecorator($value_js)
+    {
+        return <<<JS
+'{$this->buildHtmlQrCode("'+" . $value_js . "+'")}'
+JS;
+    }
+        
+    public function buildHtmlHeadTags()
+    {
+        $includes = parent::buildHtmlHeadTags();
+        $template = $this->getTemplate();
+        $includes[] = '<script type="text/javascript" src="' . $template->buildUrlToSource('LIBS.QRCODE.JS') . '"></script>';
+        return $includes;
+    }
+    
+    
+    
+    public function buildCssElementClass()
+    {
+        return 'exf-qrcode';
+    }
+}
+?>
