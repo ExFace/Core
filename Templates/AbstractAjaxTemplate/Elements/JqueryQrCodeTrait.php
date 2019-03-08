048b899cf99d3e496bcdc5bfbdf5d27747bb4d2b
Andrej Kabachnik <andrej_ka@gmx.de>
2019-03-08 11:46:00 +0100

    DOC added documentation to JqueryQrCodeTrait

--- Templates/AbstractAjaxTemplate/Elements/JqueryQrCodeTrait.php
+++ Templates/AbstractAjaxTemplate/Elements/JqueryQrCodeTrait.php
@@ -5,7 +5,16 @@ use exface\Core\Templates\AbstractAjaxTemplate\Interfaces\JsValueDecoratingInter
 use exface\Core\Widgets\QrCode;
 
 /**
- *
+ * This trait helps render QRCodes.
+ * 
+ * The trait uses the Javascript library jquery.qrcode (http://blog.jetienne.com/blog/2011/04/07/jquery-qrcode/).
+ * 
+ * How to use: 
+ * 
+ * 1. Add the following dependency to the composer.json of the template: "bower-asset/jquery-qrcode" : "^1.0"
+ * 2. Add the config option "LIBS.QRCODE.JS": "bower-asset/jquery-qrcode/jquery.qrcode.min.js" to the template
+ * 3. Use the trait in your element and call buildHtmlQrCode() and buildJsQrCodeRenderer() where needed.
+ * 
  * @method QrCode getWidget()
  *        
  * @author Andrej Kabachnik
