2f24e0c078607690b3529edbebb100e0dbb3f6d3
aka <andrej.kabachnik@salt-solutions.de>
2019-05-23 13:00:12 +0200

    FIX default styling in HtmlBrowserTrait

--- Facades/AbstractAjaxFacade/Elements/HtmlBrowserTrait.php
+++ Facades/AbstractAjaxFacade/Elements/HtmlBrowserTrait.php
@@ -22,4 +22,47 @@ trait HtmlBrowserTrait
 <iframe src="{$url}" style="{$this->buildCssElementStyle()}" seamless></iframe>
 HTML;
     }
+        
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\Core\Facades\AbstractAjaxFacade\Elements\AbstractJqueryElement::buildCssElementStyle()
+     */
+    public function buildCssElementStyle()
+    {
+        return 'width: 100%; height: 100%; border: 0;';
+    }
+    
+    /**
+     * Wraps the given content in a sap.m.Page with back-button, that works with the iFrame.
+     *
+     * @param string $contentJs
+     * @param string $footerConstructor
+     * @param string $headerContentJs
+     *
+     * @return string
+     */
+    protected function buildJsPageWrapper(string $contentJs) : string
+    {
+        $caption = $this->getCaption();
+        if ($caption === '' && $this->getWidget()->hasParent() === false) {
+            $caption = $this->getWidget()->getPage()->getName();
+        }
+        
+        return <<<JS
+        
+        new sap.m.Page({
+            title: "{$caption}",
+            showNavButton: true,
+            navButtonPress: function(){window.history.go(-1);},
+            content: [
+                {$contentJs}
+            ],
+            headerContent: [
+                
+            ]
+        })
+        
+JS;
+    }
 }
