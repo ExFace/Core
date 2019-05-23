d8adc578b172db10d054e14d5ef1593ebc0aac8f
aka <andrej.kabachnik@salt-solutions.de>
2019-05-23 13:21:02 +0200

    REFACTOR moved iHaveContextualHelp back from Panel to Form

--- Widgets/Form.php
+++ Widgets/Form.php
@@ -2,11 +2,18 @@
 namespace exface\Core\Widgets;
 
 use exface\Core\Interfaces\Widgets\iHaveButtons;
+use exface\Core\Interfaces\Widgets\iHaveContextualHelp;
 use exface\Core\Widgets\Traits\iHaveButtonsAndToolbarsTrait;
+use exface\Core\Widgets\Traits\iHaveContextualHelpTrait;
 use exface\Core\Interfaces\Widgets\iHaveToolbars;
 use exface\Core\DataTypes\AggregatorFunctionsDataType;
 use exface\Core\Interfaces\Widgets\iShowMessageList;
 use exface\Core\Factories\WidgetFactory;
+use exface\Core\Interfaces\Widgets\iContainOtherWidgets;
+use exface\Core\Factories\DataSheetFactory;
+use exface\Core\Interfaces\Widgets\iShowSingleAttribute;
+use exface\Core\Interfaces\DataSheets\DataSheetInterface;
+use exface\Core\Interfaces\Model\MetaAttributeInterface;
 
 /**
  * A Form is a Panel with buttons.
@@ -21,9 +28,12 @@ use exface\Core\Factories\WidgetFactory;
  * @author Andrej Kabachnik
  *        
  */
-class Form extends Panel implements iHaveButtons, iHaveToolbars, iShowMessageList
+class Form extends Panel implements iHaveButtons, iHaveToolbars, iShowMessageList, iHaveContextualHelp
 {
     use iHaveButtonsAndToolbarsTrait;
+    use iHaveContextualHelpTrait {
+        getHideHelpButton as getHideHelpButtonViaTrait;
+    }
     
     private $messageList = null;
 
@@ -133,5 +143,100 @@ class Form extends Panel implements iHaveButtons, iHaveToolbars, iShowMessageLis
     public function setWidgets($widget_or_uxon_array)
     {
         return parent::setWidgets($widget_or_uxon_array);
+    }
+    
+    /**
+     *
+     * {@inheritdoc}
+     *
+     * @see \exface\Core\Interfaces\Widgets\iHaveContextualHelp::getHelpWidget()
+     */
+    public function getHelpWidget(iContainOtherWidgets $help_container) : iContainOtherWidgets
+    {
+        $table = $this->getHelpTable($help_container);
+        $help_container->addWidget($table);
+        
+        
+        $data_sheet = $this->getHelpData($this->getWidgets(), DataSheetFactory::createFromObject($table->getMetaObject()));
+        
+        if ($data_sheet->isEmpty() === true) {
+            $data_sheet->addRow([
+                'TITLE' => '',
+                'DESCRIPTION' => $this->getWorkbench()->getCoreApp()->getTranslator()->translate('ACTION.SHOWHELPDIALOG.NO_HELP'),
+                'GROUP' => ''
+            ]);
+        }
+        
+        // Mark the data sheet is fresh here - even if it is empty! - because
+        // otherwise further code might attempt to load data from the unreadable
+        // help meta object.
+        $data_sheet->setFresh(true);
+        
+        $table->prefill($data_sheet);
+        
+        return $help_container;
+    }
+    
+    /**
+     * Adds information about each widget in the array to the given sheet.
+     *
+     * @param array $widgets
+     * @param DataSheetInterface $dataSheet
+     * @param string $groupName
+     * @return DataSheetInterface
+     */
+    protected function getHelpData(array $widgets, DataSheetInterface $dataSheet, string $groupName = null) : DataSheetInterface
+    {
+        foreach ($widgets as $widget) {
+            if ($widget->isHidden()) {
+                continue;
+            }
+            
+            if ($widget instanceof iContainOtherWidgets) {
+                if ($widget->getCaption()) {
+                    $groupName = $widget->getCaption();
+                }
+                $dataSheet = $this->getHelpData($widget->getWidgets(), $dataSheet, $groupName);
+            } elseif ($widget->getCaption()) {
+                $row = [
+                    'TITLE' => $widget->getCaption(),
+                    'GROUP' => $groupName ?? ''
+                ];
+                if ($widget instanceof iShowSingleAttribute && $attr = $widget->getAttribute()) {
+                    $row = array_merge($row, $this->getHelpRowFromAttribute($attr));
+                }
+                $dataSheet->addRow($row);
+            }
+        }
+        return $dataSheet;
+    }
+    
+    /**
+     * Returns a row (assotiative array) for a data sheet with exface.Core.USER_HELP_ELEMENT filled with information about
+     * the given attribute.
+     * The inforation is derived from the attributes meta model.
+     *
+     * @param MetaAttributeInterface $attr
+     * @return string[]
+     */
+    protected function getHelpRowFromAttribute(MetaAttributeInterface $attr)
+    {
+        $row = array();
+        $row['DESCRIPTION'] = $attr->getShortDescription() ? rtrim(trim($attr->getShortDescription()), ".") . '.' : '';
+        
+        if (! $attr->getRelationPath()->isEmpty()) {
+            $row['DESCRIPTION'] .= $attr->getObject()->getShortDescription() ? ' ' . rtrim($attr->getObject()->getShortDescription(), ".") . '.' : '';
+        }
+        return $row;
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\Interfaces\Widgets\iHaveContextualHelp::getHideHelpButton()
+     */
+    public function getHideHelpButton($default = false) : ?bool
+    {
+        return $this->getHideHelpButtonViaTrait(null) ?? $this->hasParent() === true;
     }
 }
