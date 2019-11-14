8ba66b987493b244b0bce9bfb791496b21c00700
Andrej Kabachnik <andrej_ka@gmx.de>
2019-11-14 11:46:48 +0100

    DOC improved facade error documentation

--- Exceptions/Facades/FacadeLogicError.php
+++ Exceptions/Facades/FacadeLogicError.php
@@ -4,7 +4,7 @@ namespace exface\Core\Exceptions\Facades;
 use exface\Core\Exceptions\LogicException;
 
 /**
- * Exception thrown on errors in the facade logic (i.e. tempate element missing mandatory properties).
+ * Exception thrown on errors in the facade logic: e.g. tempate element missing mandatory properties, etc.
  *
  * @author Andrej Kabachnik
  *        
--- Exceptions/Facades/FacadeRuntimeError.php
+++ Exceptions/Facades/FacadeRuntimeError.php
@@ -4,7 +4,7 @@ namespace exface\Core\Exceptions\Facades;
 use exface\Core\Exceptions\LogicException;
 
 /**
- * Exception thrown on errors in the facade logic (i.e. tempate element missing mandatory properties).
+ * Exception thrown on rendering errors in the facade: e.g ambiguous element ids, etc..
  *
  * @author Andrej Kabachnik
  *        
