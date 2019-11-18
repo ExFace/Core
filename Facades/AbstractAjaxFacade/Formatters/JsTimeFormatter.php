b2a1d6607737f4bb153557c5b3c4224e0bac1462
rml <rml@NBDR326.salt-solutions.de>
2019-11-18 16:28:07 +0100

    REFACTOR now using moment.js library and exfTools.js

--- Facades/AbstractAjaxFacade/Formatters/JsTimeFormatter.php
+++ Facades/AbstractAjaxFacade/Formatters/JsTimeFormatter.php
@@ -24,7 +24,7 @@ class JsTimeFormatter extends AbstractJsDataTypeFormatter
      */
     protected function setDataType(DataTypeInterface $dataType)
     {
-        if (! $dataType instanceof DateDataType) {
+        if (! $dataType instanceof TimeDataType) {
             // TODO
         }
         return parent::setDataType($dataType);
@@ -37,140 +37,49 @@ class JsTimeFormatter extends AbstractJsDataTypeFormatter
      */
     public function buildJsFormatter($jsInput)
     {
-        return "(! {$jsInput} ? {$jsInput} : Date.parse({$jsInput}).toString(\"{$this->getFormat()}\"))";
+        //return "(! {$jsInput} ? {$jsInput} : Date.parse({$jsInput}).toString(\"{$this->getFormat()}\"))";
+        return "exfTools.time.format((! {$jsInput} ? {$jsInput} : ? exfTools.time.parse({$jsInput})), \"{$this->getFormat()}\")";
     }
     
-    /**
-     * Returns inline javascript code to format the JS Date object behind the given variable name.
+     /**
+     * Returns inline javascript code to format the JS Date object behind the given variable name
+     * into the internal used value.
      * 
-     * E.g. buildJsDateFormatter('Date()') would format the current date.
+     * E.g. buildJsFormatDateObjectToInternal('Date()') would format the current date.
      * 
      * @param string $jsDateObject
      * @return string
      */
-    public function buildJsDateFormatter($jsDateObject)
+    public function buildJsFormatDateObjectToInternal($jsDateObject)
     {
-        return "{$jsDateObject}.toString(\"{$this->getFormat()}\")";
+        return "exfTools.time.format({$jsDateObject}, \"{$this->buildJsDateFormatInternal()}\")";
     }
     
-    /**
+     /**
      * Returns inline javascript code to turn the JS Date object behind the given variable 
-     * name into a normalized date/time string.
+     * name into a formated string.
      *
-     * E.g. buildJsDateFormatter('Date()') would create a JS expression returning something like '2017-01-31'.
      *
      * @param string $jsDateObject
      * @return string
      */
-    public function buildJsDateStringifier($jsDateObject)
+    public function buildJsFormatDateObjectToString($jsDateObject)
     {
-        return "{$jsDateObject}.toString(\"{$this->buildJsDateFormatInternal()}\")";
+        return "exfTools.time.format({$jsDateObject}, \"{$this->getFormat()}\")";
     }
     
     /**
-     * Returns a javascript function to parse string input into a JS Date object.
-     * 
-     * The function's name is set by buildJsDateParserFunctionName() and it accepts
-     * a single argument - the string to parse.
-     * 
-     * function parseDate(date) {
-     *  ...
-     *  return Date(...);
-     * }
-     * 
+     * Returns inline javascript code to turn the given String to a Date Object
+     *
+     *
+     * @param string $jsString
      * @return string
      */
-    protected function buildJsDateParserFunction()
+    public function buildJsParseStringToDateObject($jsString)
     {
-        // TODO: Muss angepasst werden um auch eingegebene Zeiten zu verarbeiten. Momentan
-        // wird die Zeit ignoriert -> immer 00:00:00.
-        // Vorsicht wenn neben dem Datum auch die Zeit uebergeben werden soll. In welcher
-        // Zeitzone befindet sich der Client und der Server. In welcher Zeitzone erwartet
-        // der Server die uebergebene Zeit? new Date(...) und date.toString arbeiten immer
-        // mit der Zeitzone des Clients. Der Bootstrap Datepicker erwartet die
-        // uebergebenen Dates in der UTC-Zeitzone und gibt auch entsprechende Dates
-        // zurueck. Dates in UTC-Zeit koennen z.B. mit new Date(Date.UTC(yyyy, MM, dd))
-        // erstellt werden.
-        
-        
-        // IDEA: Der Code des Parsers wird noch hier erzeugt und steht nicht an einer
-        // einzelnen Stelle wie z.B. facade.js, da er aus einer Konfiguration erzeugt
-        // werden soll. Diese muesste die regulaeren Ausdruecke, sowie die Zuordnungen
-        // der matches zu dd, MM, yyyy enthalten. Die Schwierigkeit besteht darin auch
-        // Operationen wie 2000 + Number(match[3]) oder (new Date()).getFullYear()
-        // abzubilden (vlt. durch Angabe von Jahrhundert, Jahr getrennt, bzw. currentYear
-        // als Schluesselwort???). Diese Konfiguration koennte dann auch im DateDataType
-        // verwendet werden um entsprechenden PHP-Code zu erzeugen um das Datum zu
-        // parsen.
-        
-        // Auch moeglich: stattdessen Verwendung des DateJs-Parsers
-        // date wird entsprechend CultureInfo geparst, hierfuer muss das entsprechende locale
-        // DateJs eingebunden werden und ein kompatibler Formatter verwendet werden
-        // return Date.parse(date);
-        return <<<JS
-        
-        
-    function {$this->buildJsDateParserFunctionName()}(date) {
-        // date ist ein String und wird zu einem date-Objekt geparst
-        
-        // Variablen initialisieren
-        var match = null;
-        var dateParsed = false;
-        var dateValid = false;
-        
-        // dd.MM.yyyy, dd-MM-yyyy, dd/MM/yyyy, d.M.yyyy, d-M-yyyy, d/M/yyyy
-        if (!dateParsed && (match = /(\d{1,2}):?(\d{1,2})(:\d{1,2})?$/.exec(date)) != null) {
-            var hh = Number(match[1]);
-            var mm = Number(match[2]) - 1;
-            var ss = Number(match[3]);
-            dateParsed = true;
-            dateValid = date.validateHour(hh) && Date.validateMinute(mm);
-        }
-        
-        // Ausgabe des geparsten Wertes
-        if (dateParsed && dateValid) {
-            return new Date(null, null, null, hh, mm, ss);
-        }
-        
-        // (+/-)? ... (T/D/W/M/J/Y)?
-        if (!dateParsed && (match = /^([+\-]?\d{1,3})([HhMmSs]?)$/.exec(date)) != null) {
-            var output = Date.today();
-            switch (match[2].toUpperCase()) {
-                case "H":
-                case "":
-                    output.addHours(Number(match[1]));
-                    break;
-                case "M":
-                    output.addMinutes(Number(match[1]));
-                    break;
-                case "S":
-                    output.addSeconds(Number(match[1]));
-                    break;
-            }
-            dateParsed = true;
-            dateValid = true;
-        }
-        
-        // Ausgabe des geparsten Wertes
-        if (dateParsed && dateValid) {
-            return output;
-        } else {
-            return null;
-        }
+        return "exfTools.time.parse({$jsString})";
     }
-JS;
-    }
-    
-    /**
-     * Returns the name of the date parser function: "parseDate" by default.
-     * 
-     * @return string
-     */
-    public function buildJsDateParserFunctionName()
-    {
-        return 'parseDate';
-    }
-    
+        
     /**
      * 
      * {@inheritDoc}
@@ -181,8 +90,8 @@ JS;
         return <<<JS
 
     function() {
-        var dateObj = {$this->buildJsDateParserFunctionName()}({$jsInput});
-        return (dateObj ? {$this->buildJsDateFormatter('dateObj')} : '');
+        var dateObj = exfTools.time.parse({$jsInput});
+        return (dateObj ? {$this->buildJsFormatDateObjectToString('dateObj')} : '');
     }()
 
 JS;
@@ -206,10 +115,8 @@ JS;
     public function buildHtmlBodyIncludes()
     {
         return [
-            '<script type="text/javascript" src="exface/vendor/npm-asset/datejs/build/production/' . $this->buildDateJsLocaleFilename() . '"></script>',
-            '<script type="text/javascript">
-                ' . $this->buildJsDateParserFunction() . '
-            </script>'
+            '<script type="text/javascript" src="exface/moment/moment.min.js"></script>',
+            '<script type="text/javascript" src="exface/vendor/exface/Core/Facades/AbstractAjaxFacade/js/exfTools.js"></script>'
         ];
     }
     
@@ -248,7 +155,8 @@ JS;
      */
     protected function buildJsDateFormatInternal()
     {
-        return "HH:mm:ss";
+        $type = $this->getDataType();
+        return $type->getFormatToParseTo();
     }
     
     /**
@@ -262,13 +170,17 @@ JS;
     public function getFormat()
     {
         if (is_null($this->format)) {
-            $dataType = $this->getDataType();
-            $this->format = 'HH:mm';
-            if ($dataType->getShowSeconds() === true) {
-                $this->format .= ':ss';
-            }
-            if ($dataType->getAmPm() === true) {
-                $this->format .= ' a';
+            $type = $this->getDataType();
+            if ($type instanceof TimeDataType) {
+                $this->format = $type->getFormat();
+                if ($type->getShowSeconds() === true) {
+                    $this->format .= ':ss';
+                }
+                if ($type->getAmPm() === true) {
+                    $this->format .= ' a';
+                }
+            } else {
+                return $this->getWorkbench()->getCoreApp()->getTranslator()->translate('LOCALIZATION.DATE.TIME_FORMAT');
             }
         }
         return $this->format;
