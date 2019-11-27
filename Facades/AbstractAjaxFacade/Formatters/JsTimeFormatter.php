2f68f4bd0f7533216e9e4adde29f7af59c3efa19
rml <rml@NBDR326.salt-solutions.de>
2019-11-27 14:18:38 +0100

    NEW added comments with examples for functions

--- Facades/AbstractAjaxFacade/Formatters/JsTimeFormatter.php
+++ Facades/AbstractAjaxFacade/Formatters/JsTimeFormatter.php
@@ -31,8 +31,14 @@ class JsTimeFormatter extends JsDateFormatter
     }
     
     /**
-     * 
-     * {@inheritDoc}
+     * Formats an anything as a human-readable time.
+     *
+     * Accepts as input:
+     * - empty values,
+     * - parsable value (ISO string, human-readable string)
+     *
+     * e.g. "+2h" -> 14:00, "-10m" -> 11:50
+     *
      * @see \exface\Core\Facades\AbstractAjaxFacade\Interfaces\JsDataTypeFormatterInterface::buildJsFormatter()
      */
     public function buildJsFormatter($jsInput)
@@ -44,7 +50,7 @@ class JsTimeFormatter extends JsDateFormatter
      * Returns inline javascript code to format the JS Date object behind the given variable name
      * into the internal used value.
      * 
-     * E.g. buildJsFormatDateObjectToInternal('Date()') would format the current date.
+     * e.g. buildJsFormatDateObjectToInternal('new Date()') would format the current date to a format like '00:00:00'
      * 
      * @param string $jsDateObject
      * @return string
@@ -58,6 +64,7 @@ class JsTimeFormatter extends JsDateFormatter
      * Returns inline javascript code to turn the JS Date object behind the given variable 
      * name into a formated string.
      *
+     * e.g. buildJsFormatDateObjectToString('new Date()') -> format like '00:00'
      *
      * @param string $jsDateObject
      * @return string
@@ -70,6 +77,7 @@ class JsTimeFormatter extends JsDateFormatter
     /**
      * Returns inline javascript code to turn the given String to a Date Object
      *
+     * e.g. 02:00 -> new Date('1970-01-01 02:00')
      *
      * @param string $jsString
      * @return string
@@ -80,6 +88,8 @@ class JsTimeFormatter extends JsDateFormatter
     }
         
     /**
+     * Returns inline JS code to parse a time string to the internal string format: e.g.
+     * "+2h" -> 14:00:00.
      * 
      * {@inheritDoc}
      * @see \exface\Core\Facades\AbstractAjaxFacade\Interfaces\JsDataTypeFormatterInterface::buildJsFormatParser()
