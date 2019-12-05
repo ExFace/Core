b0b267d38afe277d470003a13fa08fb80320e77f
Andrej Kabachnik <andrej_ka@gmx.de>
2019-12-05 12:20:09 +0100

    FIX TimestampDataType turned time to 00:00:00

--- DataTypes/TimestampDataType.php
+++ DataTypes/TimestampDataType.php
@@ -3,16 +3,31 @@ namespace exface\Core\DataTypes;
 
 class TimestampDataType extends DateDataType
 {    
-    /*public static function formatDate(\DateTime $date) : string
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\Core\DataTypes\DateDataType::formatDateNormalized()
+     */
+    public static function formatDateNormalized(\DateTime $date) : string
     {
         return $date->format(DateTimeDataType::DATETIME_FORMAT_INTERNAL);
-    }*/
-    
-    public function getFormatToParseTo() : string
-    {
-        return DateTimeDataType::DATE_ICU_FORMAT_INTERNAL;
     }
     
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\DataTypes\DateDataType::getFormatToParseTo()
+     */
+    public function getFormatToParseTo() : string
+    {
+        return DateTimeDataType::DATETIME_ICU_FORMAT_INTERNAL;
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\DataTypes\DateDataType::getFormat()
+     */
     public function getFormat() : string
     {
         return $this->hasCustomFormat() ? parent::getFormat() : $this->getWorkbench()->getCoreApp()->getTranslator()->translate('LOCALIZATION.DATE.DATETIME_FORMAT');
