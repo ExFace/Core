2457e5e8ae753ddfbbc7f0021ed9042d3e715fd9
aka <andrej.kabachnik@salt-solutions.de>
2019-12-13 21:24:48 +0100

    REFACTOR add method JqueryAlignmentTrait::get/setDefaultAlignment()

--- Facades/AbstractAjaxFacade/Elements/JqueryAlignmentTrait.php
+++ Facades/AbstractAjaxFacade/Elements/JqueryAlignmentTrait.php
@@ -2,6 +2,8 @@
 namespace exface\Core\Facades\AbstractAjaxFacade\Elements;
 
 trait JqueryAlignmentTrait {
+    
+    private $defaultAlign = null;
 
     /**
      * Calculates the value of the CSS attribute text-align based on the align property of the widget.
@@ -15,7 +17,7 @@ trait JqueryAlignmentTrait {
     {        
         if ($widget_align === EXF_ALIGN_DEFAULT || $widget_align === EXF_ALIGN_OPPOSITE){
             if (is_null($default_alignment)){
-                $default_alignment = $this->getFacade()->getConfig()->getOption('WIDGET.ALL.DEFAULT_ALIGNMENT');
+                $default_alignment = $this->getDefaultAlignment();
             }
             
             if ($widget_align === EXF_ALIGN_DEFAULT){
@@ -29,5 +31,27 @@ trait JqueryAlignmentTrait {
         
         return $widget_align;
     }
+    
+    /**
+     * Changes the default alignment in the element (i.e. if no align-property specified for the widget)
+     * @param string $value
+     * @return self
+     */
+    public function setDefaultAlignment(string $value) : self
+    {
+        $this->defaultAlign = $value;
+        return $this;
+    }
+    
+    /**
+     * Returns the default CSS-align value for this element.
+     * 
+     * Override this method to change the defaults!
+     * 
+     * @return string
+     */
+    protected function getDefaultAlignment() : string
+    {
+        return $this->defaultAlign ?? $this->getFacade()->getConfig()->getOption('WIDGET.ALL.DEFAULT_ALIGNMENT');
+    }
 }
-?>
