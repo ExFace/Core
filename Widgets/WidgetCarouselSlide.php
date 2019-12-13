eb0c0e91a18e2c90293a8309b90bfee3c2934f8d
tmc <tmc@NBDR211.salt-solutions.de>
2019-12-13 12:07:41 +0100

    FEATURE added WidgetCarouselSlide Widget

---
+++ Widgets/WidgetCarouselSlide.php
@@ -0,0 +1,16 @@
+<?php
+namespace exface\Core\Widgets;
+
+/**
+ * A WidgetCarouselSlide is a special panel, used in WidgetCarousel containers. 
+ * 
+ * @method Carousel getParent()
+ *
+ * @author Andrej Kabachnik
+ *        
+ */
+class WidgetCarouselSlide extends Tab
+{
+   
+}
+?>
