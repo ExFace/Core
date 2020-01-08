59a9345e05b94b7f0f1674499172902699a1cdb4
Andrej Kabachnik <andrej_ka@gmx.de>
2020-01-08 19:41:01 +0100

    NEW multi-user credential sets

---
+++ Install/Sql/MySQL/Migrations/0.28/20200108_1730_01_NEW_multi_user_credentials.sql
@@ -0,0 +1,46 @@
+-- UP
+
+ALTER TABLE `exf_data_connection_credentials`
+	ADD COLUMN `name` VARCHAR(50) NOT NULL AFTER `data_connection_oid`,
+	ADD COLUMN `data_connector_config` TEXT NULL DEFAULT NULL AFTER `name`,
+	ADD COLUMN `private` TINYINT(1) NOT NULL DEFAULT 1 AFTER `data_connector_config`;
+	
+ALTER TABLE `exf_user_credentials`
+	ADD COLUMN `data_connection_credentials_oid` BINARY(16) NOT NULL AFTER `user_oid`;
+	
+UPDATE exf_data_connection_credentials dc 
+	SET dc.data_connector_config = (
+			SELECT 
+				uc.data_connector_config 
+			FROM exf_user_credentials uc 
+			WHERE dc.user_credentials_oid = uc.oid
+		),
+		dc.name = (
+			SELECT 
+				uc.name 
+			FROM exf_user_credentials uc 
+			WHERE dc.user_credentials_oid = uc.oid
+		);
+
+UPDATE exf_user_credentials uc 
+	SET uc.data_connection_credentials_oid = (
+			SELECT 
+				dc.oid 
+			FROM exf_data_connection_credentials dc
+			WHERE dc.user_credentials_oid = uc.oid 
+		);
+
+ALTER TABLE `exf_user_credentials`
+	DROP COLUMN `data_connector_config`,
+	DROP COLUMN `name`;
+
+-- DOWN
+
+ALTER TABLE `exf_data_connection_credentials`
+	DROP COLUMN `name`,
+	DROP COLUMN `data_connector_config`,
+	DROP COLUMN `private`;
+	
+ALTER TABLE `exf_user_credentials`
+	ADD COLUMN `data_connector_config` TEXT NULL DEFAULT NULL AFTER `user_oid`,
+	DROP COLUMN `data_connection_credentials_oid`;
--- ModelLoaders/SqlModelLoader.php
+++ ModelLoaders/SqlModelLoader.php
@@ -471,8 +471,8 @@ class SqlModelLoader implements ModelLoaderInterface
         
         // If there is a user logged in, fetch his specific connctor config (credentials)
         if ($user_name = $exface->getSecurity()->getAuthenticatedToken()->getUsername()) {
-            $join_user_credentials = ' LEFT JOIN (exf_data_connection_credentials dcc LEFT JOIN exf_user_credentials uc ON dcc.user_credentials_oid = uc.oid INNER JOIN exf_user u ON uc.user_oid = u.oid AND u.username = "' . $user_name . '") ON dcc.data_connection_oid = dc.oid';
-            $select_user_credentials = ', uc.data_connector_config AS user_connector_config';
+            $join_user_credentials = ' LEFT JOIN (exf_data_connection_credentials dcc LEFT JOIN exf_user_credentials uc ON dcc.oid = uc.data_connection_credentials_oid INNER JOIN exf_user u ON uc.user_oid = u.oid AND u.username = "' . $user_name . '") ON dcc.data_connection_oid = dc.oid';
+            $select_user_credentials = ', dcc.data_connector_config AS user_connector_config';
         }
         
         if ($selector->isUid() === true) {
