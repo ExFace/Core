6302cdc9c8ab3edf8a17507956fe5482aa42f613
rml <rml@NBDR326.salt-solutions.de>
2020-01-08 16:47:34 +0100

    FIX removed not supported parameter in createForLeafNode function

--- Factories/UiPageTreeFactory.php
+++ Factories/UiPageTreeFactory.php
@@ -7,6 +7,12 @@ use exface\Core\CommonLogic\Workbench;
 
 class UiPageTreeFactory extends AbstractStaticFactory
 {
+    /**
+     * 
+     * @param Workbench $exface
+     * @param int $depth
+     * @return UiPageTree
+     */
     public static function createFromRoot(Workbench $exface, int $depth = null) : UiPageTree
     {
         $tree = new UiPageTree($exface);
@@ -14,6 +20,13 @@ class UiPageTreeFactory extends AbstractStaticFactory
         return $tree;
     }
     
+    /**
+     * 
+     * @param Workbench $exface
+     * @param UiPageInterface $rootPage
+     * @param int $depth
+     * @return UiPageTree
+     */
     public static function createFromRootPage(Workbench $exface, UiPageInterface $rootPage, int $depth = null) : UiPageTree
     {
         $tree = new UiPageTree($exface);
@@ -22,17 +35,29 @@ class UiPageTreeFactory extends AbstractStaticFactory
         return $tree;
     }
     
-    public static function createForLeafNode(Workbench $exface, UiPageInterface $leafPage, UiPageInterface $rootPage = null, int $depthForOtherNodes = 1) : UiPageTree
+    /**
+     * 
+     * @param Workbench $exface
+     * @param UiPageInterface $leafPage
+     * @param UiPageInterface $rootPage
+     * @return UiPageTree
+     */
+    public static function createForLeafNode(Workbench $exface, UiPageInterface $leafPage, UiPageInterface $rootPage = null) : UiPageTree
     {
         $tree = new UiPageTree($exface);
         $tree->setExpandPathToPage($leafPage);
         if ($rootPage !== null) {
             $tree->setRootPages([$rootPage]);
         }
-        $tree->setExpandDepth($depthForOtherNodes);
         return $tree;
     }
     
+    /**
+     * 
+     * @param Workbench $exface
+     * @param UiPageInterface $page
+     * @return UiPageTree
+     */
     public static function createBreadcrumbsToPage(Workbench $exface, UiPageInterface $page) : UiPageTree
     {
         $tree = new UiPageTree($exface);
