e3b062fda6bc633369bd228e727e41e97ddc5365
rml <rml@NBDR326.salt-solutions.de>
2020-01-10 13:33:56 +0100

    FIX return value type fixed for setRootPageAlias function

--- Widgets/Breadcrumbs.php
+++ Widgets/Breadcrumbs.php
@@ -5,9 +5,29 @@ use exface\Core\Factories\UiPageTreeFactory;
 use exface\Core\Factories\SelectorFactory;
 
 /**
- * NavMenu shows a navigation menu similar to NavTiles but as a list of links
+ * Breadcrumbs show links to all ancestor pages of the current page starting from a given root page or,
+ * if no root page is given, from the default root page.
+ * 
+ * Breadcrumbs produce a tree menu consisting of nodes. Each node contains its parent node and (if exists) its child node
+ * that is either an ancestor of the current page, the parent of the current page or the current page itself.
+ * 
+ * Each menu entry navigates to its inherent page when clicked.
+ *  
+ * ```
+ * {
+ *  "widget_type": "Breadcrumbs",
+ *  "object_alias": "exface.Core.PAGE"
+ * }
+ * 
+ * ```
+ * 
+ * Using the optional `root_page_alias` property you can control which page should be the root page for the menu.
+ * 
+ * The visual representation, as always, depends on the facade.
+ * 
+ * @method Breadcrumbs getWidget() 
  *
- * @author Andrej Kabachnik
+ * @author Ralf Mulansky
  *        
  */
 class Breadcrumbs extends AbstractWidget
@@ -33,7 +53,7 @@ class Breadcrumbs extends AbstractWidget
      * @param string $pageSelector
      * @return NavMenu
      */
-    public function setRootPageAlias(string $pageSelectorString) : NavMenu
+    public function setRootPageAlias(string $pageSelectorString) : Breadcrumbs
     {
         $this->rootPage = $this->getWorkbench()->getCMS()->getPage(SelectorFactory::createPageSelector($this->getWorkbench(), $pageSelectorString));
         return $this;
