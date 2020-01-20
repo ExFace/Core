d757471557d917224d0b77d50f88b14487e85141
Andrej Kabachnik <andrej_ka@gmx.de>
2020-01-20 11:07:32 +0100

    FIX multi-user-cred SQL migration not working with orphaned user creds

--- Install/Sql/MySQL/Migrations/0.28/20200108_1730_01_NEW_multi_user_credentials.sql
+++ Install/Sql/MySQL/Migrations/0.28/20200108_1730_01_NEW_multi_user_credentials.sql
@@ -32,12 +32,15 @@ UPDATE exf_user_credentials uc
 			WHERE dc.user_credentials_oid = uc.oid 
         	LIMIT 1
 		);
+		
+DELETE FROM exf_user_credentials WHERE data_connection_credentials_oid IS NULL;
 
 ALTER TABLE `exf_user_credentials`
 	DROP COLUMN `data_connector_config`,
 	DROP COLUMN `name`,
 	CHANGE COLUMN `data_connection_credentials_oid` `data_connection_credentials_oid` BINARY(16) NOT NULL AFTER `user_oid`;
 	
+/* If there were unused user credentials - delete them! We can't keep them as we don't know what connection they are meant for. */
 ALTER TABLE `exf_data_connection_credentials`
 	DROP COLUMN `user_credentials_oid`;
 
