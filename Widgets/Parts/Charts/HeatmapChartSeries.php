155db56691f8a566f926cb104ba0345f6e91d8ef
rml <rml@NBDR326.salt-solutions.de>
2020-01-29 17:02:43 +0100

    NEW classes heatmap chart types and visualMap chart parts

---
+++ Widgets/Parts/Charts/HeatmapChartSeries.php
@@ -0,0 +1,15 @@
+<?php
+namespace exface\Core\Widgets\Parts\Charts;
+
+use exface\Core\Widgets\Parts\Charts\Traits\XYChartSeriesTrait;
+use exface\Core\Widgets\Parts\Charts\Traits\VisualMapChartSeriesTrait;
+
+/**
+ 
+ */
+class HeatmapChartSeries extends ChartSeries
+{
+    use XYChartSeriesTrait;
+    use VisualMapChartSeriesTrait;      
+    
+}
