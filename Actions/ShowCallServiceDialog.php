b97906d6ede076aed390c43629b9f9a9ba4acf9e
aka <andrej.kabachnik@salt-solutions.de>
2020-02-27 21:14:16 +0100

    NEW button properties reset_widget_ids and refresh_widget_ids

--- Actions/ShowCallServiceDialog.php
+++ Actions/ShowCallServiceDialog.php
@@ -144,7 +144,7 @@ class ShowCallServiceDialog extends ShowDialog
         ->setAlign(EXF_ALIGN_OPPOSITE);
         // Make the save button refresh the same widget as the Button showing the dialog would do
         if ($this->getWidgetDefinedIn() instanceof Button) {
-            $saveButton->setRefreshWidgetLink($this->getWidgetDefinedIn()->getRefreshWidgetLink());
+            $saveButton->setRefreshWidgetIds($this->getWidgetDefinedIn()->getRefreshWidgetIds());
             $this->getWidgetDefinedIn()->setRefreshWidgetLink(null);
         }
         $dialog->addButton($saveButton);
