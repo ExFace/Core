35df49b2b4b53bf3052c30bd5dfb1fea62cda28f
Andrej Kabachnik <andrej_ka@gmx.de>
2020-03-18 14:51:12 +0100

    NEW data type Binary

---
+++ DataTypes/BinaryDataType.php
@@ -0,0 +1,11 @@
+<?php
+namespace exface\Core\DataTypes;
+
+/**
+ * Data type for binary data (e.g. media file contents, etc.).
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
+class BinaryDataType extends StringDataType
+{}
