9f0c053e861fcb2a429fbeb01b1561001a6db134
Andrej Kabachnik <andrej_ka@gmx.de>
2020-03-20 14:08:43 +0100

    REFACTOR added encoding conversion to BinaryDataType

--- DataTypes/BinaryDataType.php
+++ DataTypes/BinaryDataType.php
@@ -3,6 +3,8 @@ namespace exface\Core\DataTypes;
 
 use exface\Core\Exceptions\DataTypes\DataTypeConfigurationError;
 use exface\Core\Exceptions\DataTypes\DataTypeValidationError;
+use exface\Core\Exceptions\DataTypes\DataTypeCastingError;
+use exface\Core\Exceptions\RuntimeException;
 
 /**
  * Data type for binary data (e.g. media file contents, etc.).
@@ -104,5 +106,154 @@ class BinaryDataType extends StringDataType
             return null;
         }
         return $this->getMaxSizeInBytes() / 1000000;
+    }
+    
+    /**
+     * Converts a given string from Base64 to a hexadecimal number like `xf1a23` - the `x` being the optional prefix
+     * 
+     * @param string $base64String
+     * @param bool $addPrefixX
+     * 
+     * @return string
+     */
+    public static function convertBase64ToHex(string $base64String, bool $addPrefixX = true) : string
+    {
+        return static::convertBinaryToHex(static::convertBase64ToBinary($base64String), $addPrefixX);
+    }
+    
+    /**
+     * Converts a given string from Base64 to a binary string.
+     *  
+     * @param string $base64String
+     * @throws DataTypeCastingError
+     * 
+     * @return string
+     */
+    public static function convertBase64ToBinary(string $base64String) : string
+    {
+        $binary = base64_decode($base64String, true);
+        if ($binary === false) {
+            throw new DataTypeCastingError('Cannot convert Base64 to binary: invalid Base64 string!');
+        }
+        return $binary;
+    }
+    
+    /**
+     * Converts a given binary string to Base64.
+     * 
+     * @param string $binaryString
+     * @return string
+     */
+    public static function convertBinaryToBase64(string $binaryString) : string
+    {
+        return base64_encode($binaryString);
+    }
+    
+    /**
+     * Converts a given binary string to a hexadecimal number like `xf1a23` - the `x` being the optional prefix
+     * 
+     * @param string $binaryString
+     * @param bool $addPrefixX
+     * 
+     * @return string
+     */
+    public static function convertBinaryToHex(string $binaryString, bool $addPrefixX = true) : string
+    {
+        return ($addPrefixX ? 'x' : '') . bin2hex($binaryString);
+    }
+    
+    /**
+     * Converts a given hexadecimal string to a Base64 encoded string
+     * 
+     * @param string $hexString
+     * @return string
+     */
+    public static function convertHexToBase64(string $hexString) : string
+    {
+        return static::convertBinaryToBase64(static::convertHexToBinary($hexString));
+    }
+    
+    /**
+     * Converts a given hexadecimal string to a binary string.
+     * 
+     * The hexadecimal number may be prefixed with an `x` - it will be automatically removed.
+     * 
+     * @param string $hexString
+     * @throws DataTypeCastingError
+     * 
+     * @return string
+     */
+    public static function convertHexToBinary(string $hexString) : string
+    {
+        $hexString = ltrim($hexString, "xX");
+        $binary = hex2bin($hexString, true);
+        if ($binary === false) {
+            throw new DataTypeCastingError('Cannot convert hexadecimal to binary: invalid hexadecimal number!');
+        }
+        return $binary;
+    }
+    
+    /**
+     * Converts the given data from the current encoding to Base64.
+     * 
+     * @param string|null $value
+     * @throws RuntimeException
+     * @return string
+     */
+    public function convertToBase64(string $value = null) : ?string
+    {
+        $value = $value ?? $this->getValue();
+        if ($value === null) {
+            return $value;
+        }
+        switch ($this->getEncoding()) {
+            case self::ENCODING_BASE64: return $value;
+            case self::ENCODING_HEX: return self::convertHexToBase64($value);
+            default:
+                throw new RuntimeException('Cannot convert binary data in ' . $this->getEncoding() . ' to Base64!');
+        }
+    }
+    
+    /**
+     * Converts the given data from the current encoding to hexadecimal.
+     * 
+     * @param string $value
+     * @param bool $addPrefixX
+     * @throws RuntimeException
+     * @return string|NULL
+     */
+    public function convertToHex(string $value = null, bool $addPrefixX = true) : ?string
+    {
+        $value = $value ?? $this->getValue();
+        if ($value === null) {
+            return $value;
+        }
+        switch ($this->getEncoding()) {
+            case self::ENCODING_BASE64: return self::convertBase64ToHex($value, $addPrefixX);
+            case self::ENCODING_HEX: ($addPrefixX ? 'x' : '') . ltrim($value, "xX");
+            default:
+                throw new RuntimeException('Cannot convert binary data in ' . $this->getEncoding() . ' to a hexadecimal number!');
+        }
+    }
+    
+    /**
+     * Converts the given data from the current encoding to a binary string.
+     * 
+     * @param string $value
+     * @throws RuntimeException
+     * @return string|NULL
+     */
+    public function convertToBinary(string $value = null) : ?string
+    {
+        $value = $value ?? $this->getValue();
+        if ($value === null) {
+            return $value;
+        }
+        switch ($this->getEncoding()) {
+            case self::ENCODING_BASE64: return self::convertBase64ToBinary($value);
+            case self::ENCODING_HEX: return self::convertHexToBinary($value);
+            default:
+                throw new RuntimeException('Cannot convert binary data in ' . $this->getEncoding() . ' to a binary string!');
+        }
     }
 }
