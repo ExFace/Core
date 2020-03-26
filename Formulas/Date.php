fd74b6e505d6689e2faf461f673eaceb5690b06f
Andrej Kabachnik <andrej_ka@gmx.de>
2020-03-26 10:06:46 +0100

    DOC added documentation to date/time related formulas

--- Formulas/Date.php
+++ Formulas/Date.php
@@ -4,6 +4,24 @@ namespace exface\Core\Formulas;
 use exface\Core\Factories\DataTypeFactory;
 use exface\Core\DataTypes\DateDataType;
 
+/**
+ * Parses (almost) any value into a date in the internal format or a given ICU format.
+ * 
+ * The first parameter is the value to parse, while the second (optional) parameter is
+ * the ICU date format.
+ * 
+ * Examples:
+ * 
+ * - `=DATE('25.03.2020')` = 2020-03-25
+ * - `=DATE('1585090800')` = 2020-03-25
+ * - `=DATE('1585090800', 'dd.MM.yyyy)` = 25.03.2020
+ * - `=DATE('2020-03-25', 'yyyyMMddHHmmss')` = 20200325000000
+ * 
+ * See http://userguide.icu-project.org/formatparse/datetime for a complete guide to
+ * the ICU date format syntax.
+ *
+ * @link http://userguide.icu-project.org/formatparse/datetime
+ */
 class Date extends \exface\Core\CommonLogic\Model\Formula
 {
 
--- Formulas/Timestamp.php
+++ Formulas/Timestamp.php
@@ -3,21 +3,45 @@ namespace exface\Core\Formulas;
 
 use exface\Core\DataTypes\TimestampDataType;
 use exface\Core\Factories\DataTypeFactory;
+use exface\Core\DataTypes\DateTimeDataType;
 
+/**
+ * Converts a date in almost any format to seconds since 01.01.1970 00:00:00
+ * 
+ * Examples: 
+ * 
+ * - `=TIMESTAMP('2020-03-25')` = 1585090800
+ * - `=TIMESTAMP()` = current timestamp
+ * - `=TIMESTAMP('2020-03-25', 1000)` = 1585090800000
+ * 
+ * The first parameter is the date to convert. If not specified, the current
+ * date/time will be used.
+ * 
+ * The second parameter is a multiplier - set it to `1000` to get milliseconds
+ * instead of seconds. 
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
 class Timestamp extends \exface\Core\CommonLogic\Model\Formula
 {
 
     /**
      *
      * @param string $date            
-     * @param number $multiplier            
+     * @param number $multiplier  
+     *           
      * @return number
      */
-    function run($date, $multiplier = 1000)
+    function run($date, $multiplier = 1)
     {
-        if (! $date)
-            return 0;
-        return strtotime($date) * $multiplier;
+        if ($date === null && $date === '') {
+            return strtotime() * $multiplier;
+        }
+        if ($date === 0) {
+            return $date;
+        }
+        return strtotime(DateTimeDataType::cast($date)) * $multiplier;
     }
     
     /**
