c9f0fe795b189af41b6e3f670235e2ce65485912
Andrej Kabachnik <andrej_ka@gmx.de>
2020-03-26 10:06:46 +0100

    DOC added documentation to date/time related formulas

--- Formulas/Now.php
+++ Formulas/Now.php
@@ -4,6 +4,23 @@ namespace exface\Core\Formulas;
 use exface\Core\DataTypes\DateTimeDataType;
 use exface\Core\Factories\DataTypeFactory;
 
+/**
+ * Returns the current date or date and time with the option to define a specific format.
+ *
+ * Accepts a single optional perameter - the ICU date/time format.
+ *
+ * Examples:
+ *
+ * - `=NOW()` = 2020-03-25 21:00:55
+ * - `=NOW('yyyy-MM-dd')` = 2020-03-25
+ * - `=NOW('dd.MM.yyyy')` = 25.03.2020
+ * - `=NOW('dd.MM.yyyy HH:mm:ss')` = 25.03.2020 21:00:55
+ *
+ * See http://userguide.icu-project.org/formatparse/datetime for a complete guide to
+ * the ICU date format syntax.
+ *
+ * @link http://userguide.icu-project.org/formatparse/datetime
+ */
 class Now extends \exface\Core\CommonLogic\Model\Formula
 {
 
