12f3cb4b82a32f7b9a450b985226acd431b31cc0
rml <rml@NBDR326.salt-solutions.de>
2020-04-09 16:41:34 +0200

    REFACTOR moved getter/setter methods to CreateUserFromTokenTrait

--- CommonLogic/Security/Authenticators/Traits/CreateUserFromTokenTrait.php
+++ CommonLogic/Security/Authenticators/Traits/CreateUserFromTokenTrait.php
@@ -11,11 +11,67 @@ use exface\Core\CommonLogic\Selectors\UserSelector;
 use exface\Core\CommonLogic\Selectors\UserRoleSelector;
 use exface\Core\DataTypes\ComparatorDataType;
 use exface\Core\Interfaces\Selectors\AliasSelectorInterface;
-use exface\Core\Exceptions\UnexpectedValueException;
 use exface\Core\DataTypes\StringDataType;
+use exface\Core\Interfaces\Security\AuthenticatorInterface;
+use exface\Core\CommonLogic\UxonObject;
+use exface\Core\Exceptions\Security\AuthenticationFailedError;
+use exface\Core\Interfaces\DataSheets\DataSheetInterface;
 
 trait CreateUserFromTokenTrait
-{
+{   
+    private $createNewUsers = false;
+    
+    private $newUsersRoles = null;
+    
+    /**
+     * Set if a new PowerUI user should be created if no user with that username already exists.
+     *
+     * @uxon-property create_new_users
+     * @uxon-type boolean
+     * @uxon-default false
+     *
+     * @param bool $trueOrFalse
+     * @return AuthenticatorInterface
+     */
+    public function setCreateNewUsers(bool $trueOrFalse) : AuthenticatorInterface
+    {
+        $this->createNewUsers = $trueOrFalse;
+        return $this;
+    }
+    
+    protected function getCreateNewUsers() : bool
+    {
+        return $this->createNewUsers;
+    }
+    
+    /**
+     * The role aliases for the roles newly created users should inherit.
+     *
+     * @uxon-property create_new_users_with_roles
+     * @uxon-type array
+     * @uxon-template [""]
+     *
+     * @param string[]|UxonObject $create_new_users_with_roles
+     * @return AuthenticatorInterface
+     */
+    public function setCreateNewUsersWithRoles($arrayOrUxon) : AuthenticatorInterface
+    {
+        if ($arrayOrUxon instanceof UxonObject) {
+            $this->newUsersRoles = $arrayOrUxon->toArray();
+        } elseif (is_array($arrayOrUxon)) {
+            $this->newUsersRoles = $arrayOrUxon;
+        }
+        return $this;
+    }
+    
+    /**
+     *
+     * @return array|NULL
+     */
+    protected function getNewUserRoles() : ?array
+    {
+        return $this->newUsersRoles;
+    }
 
     /**
      * Creates a user from the given token and saves it to the database. Returns the user.
@@ -24,9 +80,9 @@ trait CreateUserFromTokenTrait
      * @param WorkbenchInterface $exface
      * @return UserInterface
      */
-    protected function createUserFromToken(UsernamePasswordAuthToken $token, WorkbenchInterface $exface, string $surname = null, string $givenname = null): UserInterface
+    protected function createUserFromToken(WorkbenchInterface $exface, UsernamePasswordAuthToken $token, string $surname = null, string $givenname = null): UserInterface
     {
-        $userDataSheet = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'exface.Core.USER');
+        $userDataSheet = DataSheetFactory::createFromObjectIdOrAlias($exface, 'exface.Core.USER');
         $row = [];
         $row['USERNAME'] = $token->getUsername();
         $row['PASSWORD'] = $token->getPassword();
@@ -54,10 +110,8 @@ trait CreateUserFromTokenTrait
      */
     protected function addRolesToUser(WorkbenchInterface $exface, UserInterface $user, array $rolesArray) : UserInterface
     {
-        $roleDataSheet = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'exface.Core.USER_ROLE');
-        $orFilterGroup = ConditionGroupFactory::createEmpty($this->getWorkbench(), EXF_LOGICAL_OR, $roleDataSheet->getMetaObject());
-        #TODO build new RoleSelectors for each array entry, so array can contain, aliases, uids, or both
-        #TODO add filter for app via APP__ALIAS
+        $roleDataSheet = DataSheetFactory::createFromObjectIdOrAlias($exface, 'exface.Core.USER_ROLE');
+        $orFilterGroup = ConditionGroupFactory::createEmpty($exface, EXF_LOGICAL_OR, $roleDataSheet->getMetaObject());
         foreach ($rolesArray as $role) {
             $roleSelector = new UserRoleSelector($exface, $role);
             if ($roleSelector->isUid()) {
@@ -66,7 +120,7 @@ trait CreateUserFromTokenTrait
                 if ($roleSelector->hasNamespace() === false) {
                     $orFilterGroup->addConditionFromString('ALIAS', $roleSelector->toString(), ComparatorDataType::EQUALS);
                 } else {
-                    $aliasFilterGrp = ConditionGroupFactory::createEmpty($this->getWorkbench(), EXF_LOGICAL_AND, $roleDataSheet->getMetaObject());
+                    $aliasFilterGrp = ConditionGroupFactory::createEmpty($exface, EXF_LOGICAL_AND, $roleDataSheet->getMetaObject());
                     $aliasFilterGrp->addConditionFromString('APP__ALIAS', $roleSelector->getAppAlias(), ComparatorDataType::EQUALS);
                     //$roleAlias = substr($roleSelector->toString(), strlen($roleSelector->getAppAlias() . AliasSelectorInterface::ALIAS_NAMESPACE_DELIMITER));
                     $roleAlias = StringDataType::substringAfter($roleSelector->toString(), $roleSelector->getAppAlias() . AliasSelectorInterface::ALIAS_NAMESPACE_DELIMITER);                    
@@ -80,7 +134,7 @@ trait CreateUserFromTokenTrait
         if (empty($roleDataSheet->getRows())) {
             return $user;
         }
-        $userRoleDataSheet = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'exface.Core.USER_ROLE_USERS');
+        $userRoleDataSheet = DataSheetFactory::createFromObjectIdOrAlias($exface, 'exface.Core.USER_ROLE_USERS');
         foreach ($roleDataSheet->getRows() as $row) {
             $userRoleRow = [];
             $userRoleRow['USER'] = $user->getUid();
@@ -89,5 +143,55 @@ trait CreateUserFromTokenTrait
         }
         $userRoleDataSheet->dataCreate();
         return $user;
+    }
+    
+    /**
+     * Creates a new user, saves in the database and adds the roles.
+     * 
+     * @param WorkbenchInterface $exface
+     * @param UsernamePasswordAuthToken $token
+     * @param string $surname
+     * @param string $givenname
+     * @throws AuthenticationFailedError
+     * @return UserInterface
+     */
+    protected function createUserWithRoles(WorkbenchInterface $exface, UsernamePasswordAuthToken $token, string $surname = null, string $givenname = null) : UserInterface
+    {
+        $userDataSheet = $this->getUserData($exface, $token);        
+        if (empty($this->getUserData($exface, $token)->getRows())) {
+            try {
+                $user = $this->createUserFromToken($exface, $token, $surname, $givenname);
+            } catch (\Throwable $e) {
+                throw new AuthenticationFailedError($this, 'User could not be created!', null, $e);
+            }
+            if ($this->getNewUserRoles() !== null) {
+                try {
+                    $user = $this->addRolesToUser($exface, $user, $this->getNewUserRoles());
+                } catch (\Throwable $e) {
+                    $user->exportDataSheet()->dataDelete();
+                    throw new AuthenticationFailedError($this, 'User roles could not be applied!', null, $e);
+                }
+            }
+        } else {
+            $user = UserFactory::createFromUsernameOrUid($exface, $userDataSheet->getRows(0)[0][$userDataSheet->getMetaObject()->getUidAttributeAlias()]);
+        }
+        return $user;
+    }
+    
+    /**
+     * Returns data sheet with rows containing the data for users with same username as in the given token.
+     * 
+     * @param WorkbenchInterface $exface
+     * @param UsernamePasswordAuthToken $token
+     * @return DataSheetInterface
+     */
+    protected function getUserData(WorkbenchInterface $exface, UsernamePasswordAuthToken $token) : DataSheetInterface
+    {
+        $userDataSheet = DataSheetFactory::createFromObjectIdOrAlias($exface, 'exface.Core.USER');
+        $userFilterGroup = ConditionGroupFactory::createEmpty($exface, EXF_LOGICAL_AND, $userDataSheet->getMetaObject());
+        $userFilterGroup->addConditionFromString('USERNAME', $token->getUsername(), ComparatorDataType::EQUALS);
+        $userDataSheet->getFilters()->addNestedGroup($userFilterGroup);
+        $userDataSheet->dataRead();
+        return $userDataSheet;
     }
 }
