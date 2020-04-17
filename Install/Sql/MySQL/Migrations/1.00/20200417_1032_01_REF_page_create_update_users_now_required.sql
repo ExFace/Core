7d64bea09c1c015db310670ba3afe3c339233639
Andrej Kabachnik <andrej_ka@gmx.de>
2020-04-17 10:42:20 +0200

    FIX error in last SQL migration

--- Install/Sql/MySQL/Migrations/1.00/20200417_1032_01_REF_page_create_update_users_now_required.sql
+++ Install/Sql/MySQL/Migrations/1.00/20200417_1032_01_REF_page_create_update_users_now_required.sql
@@ -1,11 +1,11 @@
 -- UP
 
+UPDATE `exf_page` p SET p.modified_by_user_oid = 0x00000000000000000000000000000000 WHERE p.modified_by_user_oid IS NULL;
+UPDATE `exf_page` p SET p.created_by_user_oid = 0x00000000000000000000000000000000 WHERE p.created_by_user_oid IS NULL;
+
 ALTER TABLE `exf_page`
 	CHANGE COLUMN `created_by_user_oid` `created_by_user_oid` BINARY(16) NOT NULL AFTER `modified_on`,
 	CHANGE COLUMN `modified_by_user_oid` `modified_by_user_oid` BINARY(16) NOT NULL AFTER `created_by_user_oid`;
-	
-UPDATE `exf_page` p SET p.modified_by_user_oid = 0x00000000000000000000000000000000 WHERE p.modified_by_user_oid IS NULL;
-UPDATE `exf_page` p SET p.created_by_user_oid = 0x00000000000000000000000000000000 WHERE p.created_by_user_oid IS NULL;
 	
 -- DOWN
 
