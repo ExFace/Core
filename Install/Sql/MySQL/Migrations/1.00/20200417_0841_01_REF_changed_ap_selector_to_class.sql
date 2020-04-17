294e36490e351c122a4fa50c277a61974e00170c
Andrej Kabachnik <andrej_ka@gmx.de>
2020-04-17 10:38:00 +0200

    FIX more SQL errors in migrations

--- Install/Sql/MySQL/Migrations/1.00/20200417_0841_01_REF_changed_ap_selector_to_class.sql
+++ Install/Sql/MySQL/Migrations/1.00/20200417_0841_01_REF_changed_ap_selector_to_class.sql
@@ -13,12 +13,12 @@ ALTER TABLE `exf_auth_point`
 -- DOWN
 
 ALTER TABLE `exf_auth_point`
+	DROP INDEX `Class unique`;
+
+ALTER TABLE `exf_auth_point`
 	ADD COLUMN `alias` VARCHAR(50) NOT NULL AFTER `name`,
 	DROP COLUMN `class`;
 	
 UPDATE `exf_auth_point` SET `alias`='PAGE_ACCESS' WHERE  `oid`=0x11EA5EDE96E738F6B9920205857FEB80;
 UPDATE `exf_auth_point` SET `alias`='CONTEXT_ACCESS' WHERE  `oid`=0x11EA6C42DFAC007BA3480205857FEB80;
-
-ALTER TABLE `exf_auth_point`
-	DROP INDEX `Class unique`;
 	
