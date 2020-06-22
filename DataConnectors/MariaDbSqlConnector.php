602657223178bf0c1e213763278ebe17c3a8f34b
Andrej Kabachnik <andrej_ka@gmx.de>
2020-06-22 09:33:05 +0200

    NEW MariaDB SQL connector

---
+++ DataConnectors/MariaDbSqlConnector.php
@@ -0,0 +1,11 @@
+<?php
+namespace exface\Core\DataConnectors;
+
+/**
+ * Data source connector for MariaDB databases
+ *
+ * @author Andrej Kabachnik
+ */
+class MariaDbSqlConnector extends MySqlConnector
+{
+}
