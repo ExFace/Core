c1fdd6c96bd8c8147febbf1156d51887f69173be
aka <aka@NBDR223.salt-solutions.de>
2020-11-12 12:22:06 +0100

    FIX Auto-generated prototype docs were missing spaces in some places

--- QueryBuilders/PhpAnnotationsReader.php
+++ QueryBuilders/PhpAnnotationsReader.php
@@ -298,7 +298,7 @@ class PhpAnnotationsReader extends AbstractQueryBuilder
      */
     protected function prepareCommentText($string)
     {
-        $string = preg_replace('/([^\r\n])\R([^{}\s\r\n#=-])/', '$1$2', $string);
+        $string = preg_replace('/([^\r\n])\R([^{}\s\r\n#=-])/', '$1 $2', $string);
         return $string;
     }
 
