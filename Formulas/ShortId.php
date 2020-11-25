5b9f26d28a4399f84a2408cb20c534771cbbbe8b
aka <aka@NBDR223.salt-solutions.de>
2020-11-25 15:23:49 +0100

    REFACTOR renamed formula LogId() -> ShortId()

--- Formulas/ShortId.php
+++ Formulas/ShortId.php
@@ -5,12 +5,12 @@ namespace exface\Core\Formulas;
 /**
  * Creates a 7 characters long id depending on the time and based on 36 characters system.
  * 
- * E.g. '=LogId()' => 7B7KU9Q
+ * E.g. '=ShortId()' => 7B7KU9Q
  *
  * @author Ralf Mulansky
  *        
  */
-class LogId extends \exface\Core\CommonLogic\Model\Formula
+class ShortId extends \exface\Core\CommonLogic\Model\Formula
 {
 
     function run()
