e79478cb07e522de727e409ad28bb5693a96f83a
aka <aka@NBDR223.salt-solutions.de>
2020-12-07 17:55:26 +0100

    NEW widget InputNumberSpinner

---
+++ Widgets/InputNumberSpinner.php
@@ -0,0 +1,11 @@
+<?php
+namespace exface\Core\Widgets;
+
+/**
+ * An input for decimal numbers with +/- buttons or similar controls.
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
+class InputNumberSpinner extends InputNumber
+{}
