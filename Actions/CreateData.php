df8f532315c10a5d4a8dfaadb49c31106a3569b4
aka <aka@NBDR223.salt-solutions.de>
2021-03-18 12:51:37 +0100

    FIX CreateData action with ignore_related_objects_in_input_data

--- Actions/CreateData.php
+++ Actions/CreateData.php
@@ -9,7 +9,21 @@ use exface\Core\Interfaces\Tasks\TaskInterface;
 use exface\Core\Factories\ResultFactory;
 use exface\Core\Interfaces\Tasks\ResultInterface;
 use exface\Core\Interfaces\DataSheets\DataSheetInterface;
+use exface\Core\Exceptions\Actions\ActionRuntimeError;
 
+/**
+ * Creates data in the data source(s).
+ * 
+ * Executes create-operations in all data sources holding data from the input data sheet.
+ * This is very similar to saving data via `SaveData`, but it forces a create operation.
+ * 
+ * If you just want to save the main object of the data sheet, set `ignore_related_objects_in_input_data`
+ * to `true`. This will ignore any data columns of related objects in the create operation, but
+ * will keep them in the result data as-is.
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
 class CreateData extends SaveData implements iCreateData
 {
     private $ingnore_related_objects_in_input_data = false;
@@ -32,7 +46,12 @@ class CreateData extends SaveData implements iCreateData
                 }
             }
             $affected_rows += $clean_sheet->dataCreate(true, $transaction);
-            $data_sheet->merge($clean_sheet);
+            if ($data_sheet->hasUidColumn(false)) {
+                $data_sheet->getUidColumn()->setValues($clean_sheet->getUidColumn()->getValues(false));
+                $data_sheet->merge($clean_sheet);
+            } else {
+                throw new ActionRuntimeError($this, 'CreateData actions without UID columns in their input currently cannot use `ignore_related_objects_in_input_data`!');
+            }
         } else {
             $affected_rows += $data_sheet->dataCreate(true, $transaction);
         }
@@ -72,12 +91,13 @@ class CreateData extends SaveData implements iCreateData
     }
     
     /**
-     * Strips off all columns with relations from the input data sheet before creating data.
+     * Removes all columns with relations from the input data sheet before creating data and adds them back afterwards.
      * 
      * This is usefull if you have related columns in your data for some reason,
-     * but do not want to update them when creating new data rows.
+     * but do not want to update them when creating new data rows (e.g. if you need
+     * the data in subsequent actions in an `ActionChain`).
      * 
-     * Note, that related columns will only be ignored in the create operation.
+     * **Note**, that related columns will only be ignored in the create operation.
      * The result data sheet will still contain them!
      * 
      * @uxon-property ignore_related_objects_in_input_data
