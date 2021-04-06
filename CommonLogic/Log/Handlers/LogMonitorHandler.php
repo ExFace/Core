2155b41057a2f3d4bfef8e419a2f345e59841e65
rml <rml@NBDR326.salt-solutions.de>
2021-04-06 13:22:23 +0200

    NEW LogMonitorHandler class added

---
+++ CommonLogic/Log/Handlers/LogMonitorHandler.php
@@ -0,0 +1,45 @@
+<?php
+namespace exface\Core\CommonLogic\Log\Handlers;
+
+use exface\Core\Interfaces\WorkbenchInterface;
+use exface\Core\Interfaces\iCanGenerateDebugWidgets;
+use exface\Core\Interfaces\Log\LogHandlerInterface;
+use exface\Core\Interfaces\Log\LoggerInterface;
+use exface\Core\CommonLogic\Workbench;
+
+class LogMonitorHandler implements LogHandlerInterface
+{
+    private $workbench;
+    
+    private $level;
+    
+    function __construct(WorkbenchInterface $workbench, $level = LoggerInterface::DEBUG)
+    {
+        $this->workbench = $workbench;
+        $this->level = $level;
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\CommonLogic\Log\Handlers\monolog\AbstractMonologHandler::handle()
+     */
+    public function handle($level, $message, array $context = [], iCanGenerateDebugWidgets $sender = null)
+    {
+        
+    }
+    
+    protected function createRealLogger()
+    {}
+
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\Core\Interfaces\WorkbenchDependantInterface::getWorkbench()
+     */
+    public function getWorkbench()
+    {
+        return $this->workbench;
+    }
+
+}
