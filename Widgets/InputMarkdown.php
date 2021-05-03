b8977ad7f03066eb37b5a6266d1b549012b9f346
Andrej Kabachnik <andrej.kabachnik@CPX-2CVQ0HY0B2G.dir.svc.accenture.com>
2021-05-03 11:48:47 +0200

    NEW Widget InputMarkdown

---
+++ Widgets/InputMarkdown.php
@@ -0,0 +1,15 @@
+<?php
+namespace exface\Core\Widgets;
+
+/**
+ * Markdown editor
+ * 
+ * How exactly the editor will look and function depends on the facade used. Facades are encouraged
+ * to offer WYSIWYG-editors here or at least those with proper syntax highlighting.
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
+class InputMarkdown extends InputText
+{
+}
