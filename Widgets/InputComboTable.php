b6969e42e209fda5f037bd858eba1cd7ecde85f2
Andrej Kabachnik <andrej.kabachnik@CPX-2CVQ0HY0B2G.dir.svc.accenture.com>
2021-05-04 16:32:14 +0200

    FIX memory leak in InputComboTable with value=text

--- Widgets/InputComboTable.php
+++ Widgets/InputComboTable.php
@@ -251,7 +251,9 @@ class InputComboTable extends InputCombo implements iCanPreloadData
         
         // Add a quick-search filter over the text-attribute to make sure quick search works correctly
         // even if the table object has no alias!
-        $table->addFilter($table->getConfiguratorWidget()->createFilterWidget($this->getTextAttributeAlias())->setIncludeInQuickSearch(true));
+        if ($this->getTextAttribute() !== $this->getValueAttribute()) {
+            $table->addFilter($table->getConfiguratorWidget()->createFilterWidget($this->getTextAttributeAlias())->setIncludeInQuickSearch(true));
+        }
         
         $this->data_table = $table;
         
