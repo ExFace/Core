02982e474785d39780ca982e66c2ea73b37f2874
Andrej Kabachnik <andrej.kabachnik@CPX-2CVQ0HY0B2G.dir.svc.accenture.com>
2021-06-11 11:12:48 +0200

    FIX action DeleteObject now checks if input data is of same object

--- Actions/DeleteObject.php
+++ Actions/DeleteObject.php
@@ -8,6 +8,9 @@ use exface\Core\Interfaces\Tasks\TaskInterface;
 use exface\Core\Interfaces\DataSources\DataTransactionInterface;
 use exface\Core\Interfaces\Tasks\ResultInterface;
 use exface\Core\Factories\ResultFactory;
+use exface\Core\Exceptions\Actions\ActionInputInvalidObjectError;
+use exface\Core\Factories\DataSheetFactory;
+use exface\Core\DataTypes\ComparatorDataType;
 
 /**
  * Deletes objects in the input data from their data sources.
@@ -37,6 +40,24 @@ class DeleteObject extends AbstractAction implements iDeleteData
     protected function perform(TaskInterface $task, DataTransactionInterface $transaction) : ResultInterface
     {
         $input_data = $this->getInputDataSheet($task);
+        
+        // IDEA delete action objects via filter if input data includes relations to them (not tested yet!)
+        /*
+        if (! $input_data->getMetaObject()->is($this->getMetaObject()) && $input_data->hasUidColumn(true)) {
+            foreach ($input_data->getColumns() as $col) {
+                if ($col->isAttribute() && $col->getAttribute()->getObject()->isExactly($this->getMetaObject())) {
+                    $relPathFromInput = $col->getAttribute()->getRelationPath();
+                    $relPathToInput = $relPathFromInput->reverse();
+                    $dataToDelete = DataSheetFactory::createFromObject($this->getMetaObject());
+                    $dataToDelete->getFilters()->addConditionFromString($relPathToInput->toString(), $col->getValues(false), ComparatorDataType::IN);
+                }
+            }
+        }*/
+        
+        // If the input data does not contain the object to delete - error
+        if (! $input_data->getMetaObject()->is($this->getMetaObject())) {
+            throw new ActionInputInvalidObjectError($this, 'Cannot delete object ' . $this->getMetaObject()->__toString() . ' using input data of object ' . $input_data->getMetaObject()->__toString() . '!');
+        }
         
         $deletedRows = $input_data->dataDelete($transaction);
         
