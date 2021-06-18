d4870b3e7db09592620b736d7fe6c40937d477d2
Andrej Kabachnik <andrej.kabachnik@CPX-2CVQ0HY0B2G.dir.svc.accenture.com>
2021-06-18 14:24:06 +0200

    FIX more issues with MS SQL migrations

--- Install/Sql/MsSQL/Migrations/1.04/20210520_0806_01_FIX_monitor_error_index.sql
+++ Install/Sql/MsSQL/Migrations/1.04/20210520_0806_01_FIX_monitor_error_index.sql
@@ -1,11 +1,9 @@
 -- UP
 
 IF NOT EXISTS (SELECT * FROM sys.indexes WHERE NAME = N'IDX_exf_monitor_error_log_id')
-CREATE INDEX [exf_idx_monitor_error_log_id] ON [dbo].[exf_monitor_error] ([log_id])
-GO
+CREATE INDEX [exf_idx_monitor_error_log_id] ON [dbo].[exf_monitor_error] ([log_id]);
 	
 -- DOWN
 
 IF EXISTS (SELECT * FROM sys.indexes WHERE NAME = N'IDX_exf_monitor_error_log_id') 
-DROP INDEX [exf_idx_monitor_error_log_id] ON [dbo].[exf_monitor_error]
-GO
+DROP INDEX [exf_idx_monitor_error_log_id] ON [dbo].[exf_monitor_error];
--- Install/Sql/MsSQL/Migrations/1.04/20210617_1300_01_NEW_default_display_widget_for_datatypes.sql
+++ Install/Sql/MsSQL/Migrations/1.04/20210617_1300_01_NEW_default_display_widget_for_datatypes.sql
@@ -3,9 +3,9 @@
 ALTER TABLE [dbo].[exf_data_type]
 	ADD [default_display_uxon] nvarchar(max) DEFAULT NULL;
 	
+IF NOT EXISTS (SELECT oid from [dbo].[exf_attribute] WHERE [oid] = 0x11eb85e9cccb550685e9025041000001)
 INSERT INTO [dbo].[exf_attribute] ([oid], [attribute_alias], [attribute_name], [object_oid], [data], [data_properties], [attribute_formatter], [data_type_oid], [default_display_order], [default_sorter_order], [default_sorter_dir], [object_label_flag], [object_uid_flag], [attribute_readable_flag], [attribute_writable_flag], [attribute_hidden_flag], [attribute_editable_flag], [attribute_copyable_flag], [attribute_required_flag], [attribute_system_flag], [attribute_sortable_flag], [attribute_filterable_flag], [attribute_aggregatable_flag], [default_value], [fixed_value], [related_object_oid], [related_object_special_key_attribute_oid], [relation_cardinality], [copy_with_related_object], [delete_with_related_object], [attribute_short_description], [default_editor_uxon], [default_display_uxon], [custom_data_type_uxon], [comments], [created_on], [modified_on], [created_by_user_oid], [modified_by_user_oid], [default_aggregate_function], [value_list_delimiter], [attribute_type]) VALUES
-(0x11eb85e9cccb550685e9025041000001, 'DEFAULT_DISPLAY_UXON', 'Default display', 0x32360000000000000000000000000000, 'default_display_uxon', NULL, '', 0x11e905162eeb6334b04c0205857feb80, NULL, NULL, '', 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, '', '', NULL, NULL, '', 0, 0, '', NULL, NULL, '{\"length_max\":4294967295}', '', '2021-06-17 12:50:15', '2021-06-17 12:52:24', 0x31000000000000000000000000000000, 0x31000000000000000000000000000000, '', ',', 'D')
-WHERE NOT EXISTS (SELECT oid from [dbo].[exf_attribute] WHERE oid = 0x11eb85e9cccb550685e9025041000001);
+(0x11eb85e9cccb550685e9025041000001, 'DEFAULT_DISPLAY_UXON', 'Default display', 0x32360000000000000000000000000000, 'default_display_uxon', NULL, '', 0x11e905162eeb6334b04c0205857feb80, NULL, NULL, '', 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, '', '', NULL, NULL, '', 0, 0, '', NULL, NULL, '{\"length_max\":4294967295}', '', '2021-06-17 12:50:15', '2021-06-17 12:52:24', 0x31000000000000000000000000000000, 0x31000000000000000000000000000000, '', ',', 'D');
 	
 -- DOWN
 
@@ -18,7 +18,7 @@ BEGIN
         ON c.default_object_id = dc.object_id
     WHERE 
         dc.parent_object_id = OBJECT_ID('exf_data_type')
-    AND c.name IN (N'request_id', N'comment', N'ticket_no')
+    AND c.name IN (N'default_display_uxon')
     IF @@ROWCOUNT = 0 BREAK
     EXEC (@sql)
 END
