23da073f795d3629064ba5af6e626b8ab706e491
Andrej Kabachnik <andrej.kabachnik@CPX-2CVQ0HY0B2G.dir.svc.accenture.com>
2021-07-12 12:24:59 +0200

    FIX improved HtmlBrowserTrait

--- Facades/AbstractAjaxFacade/Elements/HtmlBrowserTrait.php
+++ Facades/AbstractAjaxFacade/Elements/HtmlBrowserTrait.php
@@ -1,6 +1,7 @@
 <?php
 namespace exface\Core\Facades\AbstractAjaxFacade\Elements;
 use exface\Core\Widgets\Browser;
+
 /**
  * Renders an HTML IFrame for a Browser widget
  * 
@@ -19,7 +20,7 @@ trait HtmlBrowserTrait
     {
         $url = $this->getWidget()->getUrlBase() . $this->getWidget()->getUrl();
         return <<<HTML
-<iframe src="{$url}" style="{$this->buildCssElementStyle()}" seamless></iframe>
+<iframe src="{$url}" style="{$this->buildCssElementStyle()}" id="{$this->getId()}" name="{$this->getId()}" seamless></iframe>
 HTML;
     }
         
@@ -31,38 +32,5 @@ HTML;
     public function buildCssElementStyle()
     {
         return 'width: 100%; height: 100%; border: 0;';
-    }
-    
-    /**
-     * Wraps the given content in a sap.m.Page with back-button, that works with the iFrame.
-     *
-     * @param string $contentJs
-     * @param string $footerConstructor
-     * @param string $headerContentJs
-     *
-     * @return string
-     */
-    protected function buildJsPageWrapper(string $contentJs) : string
-    {
-        $caption = $this->getCaption();
-        if ($caption === '' && $this->getWidget()->hasParent() === false) {
-            $caption = $this->getWidget()->getPage()->getName();
-        }
-        
-        return <<<JS
-        
-        new sap.m.Page({
-            title: "{$caption}",
-            showNavButton: true,
-            navButtonPress: function(){window.history.go(-1);},
-            content: [
-                {$contentJs}
-            ],
-            headerContent: [
-                
-            ]
-        })
-        
-JS;
     }
 }
