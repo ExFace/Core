3a920ab2ce02f43f810ad939f616cfd62b63615a
Andrej Kabachnik <andrej.kabachnik@CPX-2CVQ0HY0B2G.dir.svc.accenture.com>
2021-11-19 17:09:42 +0100

    FIX improved IIS server installer

--- CommonLogic/AppInstallers/IISServerInstaller.php
+++ CommonLogic/AppInstallers/IISServerInstaller.php
@@ -1,7 +1,6 @@
 <?php
 namespace exface\Core\CommonLogic\AppInstallers;
 
-
 use exface\Core\DataTypes\StringDataType;
 
 /**
@@ -15,31 +14,54 @@ class IISServerInstaller extends AbstractAppInstaller
 {
     const IIS_USERNAME = 'IUSR';
     
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\Interfaces\InstallerInterface::backup()
+     */
     public function backup(string $absolute_path) : \Iterator
     {
         return new \EmptyIterator();
     }
     
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\Interfaces\InstallerInterface::uninstall()
+     */
     public function uninstall() : \Iterator
     {
         return new \EmptyIterator();
     }
 
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\Interfaces\InstallerInterface::install()
+     */
     public function install(string $source_absolute_path): \Iterator
     {
         $indent = $this->getOutputIndentation();
-        yield $indent . $this->setPermissionsForDataFolder() . PHP_EOL;
-        yield $indent . $this->setPermissionsForLogsFolder() . PHP_EOL;
-        yield $indent . $this->setPermissionsForConfigFolder() . PHP_EOL;
-        yield $indent . $this->setPermissionsForCacheFolder() . PHP_EOL;
-        yield $indent . $this->setPermissionsForBackupFolder() . PHP_EOL;
-        yield $indent . $this->setPermissionsForTranslationsFolder() . PHP_EOL;                
+        $fm = $this->getWorkbench()->filemanager();
+        $user = self::IIS_USERNAME;
+        yield $indent . $this->setPermissionsForPath($fm->getPathToBaseFolder(), $user) . PHP_EOL;
+        yield $indent . $this->setPermissionsForPath($fm->getPathToDataFolder(), $user) . PHP_EOL;
+        yield $indent . $this->setPermissionsForPath($fm->getPathToLogFolder(), $user) . PHP_EOL;
+        yield $indent . $this->setPermissionsForPath($fm->getPathToConfigFolder(), $user) . PHP_EOL;
+        yield $indent . $this->setPermissionsForPath($fm->getPathToCacheFolder(), $user) . PHP_EOL;
+        yield $indent . $this->setPermissionsForPath($fm->getPathToBackupFolder(), $user) . PHP_EOL;
+        yield $indent . $this->setPermissionsForPath($fm->getPathToTranslationsFolder(), $user) . PHP_EOL;                
     }
     
-    protected function setPermissionsForPath(string $path): string
+    /**
+     * 
+     * @param string $path
+     * @param string $user
+     * @return string
+     */
+    protected function setPermissionsForPath(string $path, string $user): string
     {
         $output = [];
-        $user = self::IIS_USERNAME;
         exec("CACLS {$path} /e /p {$user}:c", $output);
         $shortPath = StringDataType::substringAfter($path, DIRECTORY_SEPARATOR, false, false, true);
         if (empty($output)) {
@@ -47,41 +69,4 @@ class IISServerInstaller extends AbstractAppInstaller
         }
         return "Permission for the user '{$user}' and folder/file '{$shortPath}' changed!";
     }
-    
-    protected function setPermissionsForDataFolder() : string
-    {
-        $path = $this->getWorkbench()->filemanager()->getPathToDataFolder();
-        return $this->setPermissionsForPath($path);        
-    }
-    
-    protected function setPermissionsForLogsFolder() : string
-    {
-        $path = $this->getWorkbench()->filemanager()->getPathToLogFolder();
-        return $this->setPermissionsForPath($path); 
-    }
-    
-    protected function setPermissionsForConfigFolder() : string
-    {
-        $path = $this->getWorkbench()->filemanager()->getPathToConfigFolder();
-        return $this->setPermissionsForPath($path);
-    }
-    
-    protected function setPermissionsForCacheFolder() : string
-    {
-        $path = $this->getWorkbench()->filemanager()->getPathToCacheFolder();
-        return $this->setPermissionsForPath($path);
-    }
-
-    protected function setPermissionsForBackupFolder() : string
-    {
-        $path = $this->getWorkbench()->filemanager()->getPathToBackupFolder();
-        return $this->setPermissionsForPath($path);
-    }
-    
-    protected function setPermissionsForTranslationsFolder() : string
-    {
-        $path = $this->getWorkbench()->filemanager()->getPathToTranslationsFolder();
-        return $this->setPermissionsForPath($path);
-    }
-    
 }
