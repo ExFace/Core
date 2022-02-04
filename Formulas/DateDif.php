f7a48b1a968742bc7603d2e33a1647aa365a90cd
Andrej Kabachnik <andrej.kabachnik@CPX-2CVQ0HY0B2G.dir.svc.accenture.com>
2022-02-04 13:07:29 +0100

    NEW Formulas =DateDif, =Min, =Max

---
+++ Formulas/DateDif.php
@@ -0,0 +1,96 @@
+<?php
+namespace exface\Core\Formulas;
+
+use exface\Core\Factories\DataTypeFactory;
+use exface\Core\DataTypes\DateDataType;
+use exface\Core\Exceptions\DataTypes\DataTypeCastingError;
+use exface\Core\Exceptions\FormulaError;
+
+/**
+ * Calculates the number of days, months, or years between two dates.
+ * 
+ * Examples:
+ * 
+ * - `=DateDif('01.01.2014', '06.05.2016')` = 856
+ * - `=DateDif('01.01.2014', '06.05.2016', 'D')` = 856
+ * - `=DateDif('01.01.2014', '06.05.2016', 'M')` = 28
+ * - `=DateDif('01.01.2014', '06.05.2016', 'Y')` = 2
+ * 
+ */
+class DateDif extends \exface\Core\CommonLogic\Model\Formula
+{
+    const UNIT_D = 'D';
+    const UNIT_M = 'M';
+    const UNIT_Y = 'Y';
+    
+    /**
+     * 
+     * @param string $date
+     * @param string $format
+     * @return void|\DateTime
+     */
+    public function run($start_date = null, $end_date = null, $unit = self::UNIT_D)
+    {
+        if ($start_date === null || $start_date === '' || $end_date === null || $end_date === '') {
+            return null;
+        }
+        
+        $date1 = DateDataType::castToPhpDate($start_date);
+        $date2 = DateDataType::castToPhpDate($end_date);
+        $interval = $date1->diff($date2);
+        
+        switch (mb_strtoupper($unit)) {
+            case self::UNIT_D: return $interval->days;
+            case self::UNIT_M: return $interval->y * 12 + $interval->m;
+            case self::UNIT_Y: return $interval->y;
+            default:
+                throw new FormulaError('Cannot evaluate formula "' . $this->__toString() . '": invalid unit "' . $unit . '" provided!');
+        }
+    }
+
+    /**
+     * 
+     * @param mixed $date
+     * @param string $formatTo
+     * @return string
+     */
+    protected function formatDate($date, string $formatTo) : string
+    {
+        $formatTo = $this->sanitizeFormat($formatTo);
+        
+        $dataType = $this->getDataType();
+        $dataType->setFormat($formatTo);
+        
+        try {
+            return $dataType->formatDate($dataType::castToPhpDate($date));
+        } catch (DataTypeCastingError $e) {
+            return $date;
+        }
+    }
+    
+    /**
+     * 
+     * @param string $formatArgument
+     * @return string|NULL
+     */
+    protected function sanitizeFormat(string $formatArgument) : ?string
+    {
+        $format = $formatArgument;
+        if ($format === 'Y-m-d') {
+            $format = 'yyyy-MM-dd';
+        } elseif ($format === 'Y-m-d H:i:s') {
+            $format = 'yyyy-MM-dd HH:mm:ss';
+        }
+        return $format;
+    }
+    
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\Core\CommonLogic\Model\Formula::getDataType()
+     */
+    public function getDataType()
+    {
+        return DataTypeFactory::createFromPrototype($this->getWorkbench(), DateDataType::class);
+    }
+}
