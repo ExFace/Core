48d152906eba832918b1221627a0fd3c20cded55
Andrej Kabachnik <andrej.kabachnik@salt-solutions.de>
2022-07-11 10:32:20 +0200

    NEW Map layer "WebTiles"

---
+++ Widgets/Parts/Maps/WebTilesLayer.php
@@ -0,0 +1,14 @@
+<?php
+namespace exface\Core\Widgets\Parts\Maps;
+
+use exface\Core\Widgets\Parts\Maps\BaseMaps\GenericUrlTiles;
+
+/**
+ * Allows to use a tile URL (like the one used in OpenStreetMap) as a map layer instead of a base map
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
+class WebTilesLayer extends GenericUrlTiles
+{   
+}
