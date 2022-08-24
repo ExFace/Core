0d56f6c878eef37e3d1e2062482ce692820e4d72
Andrej Kabachnik <andrej.kabachnik@salt-solutions.de>
2022-08-24 12:36:00 +0200

    FIX widget-not-found errors in CallWidgetFunction actions inside dialogs

--- Actions/CallWidgetFunction.php
+++ Actions/CallWidgetFunction.php
@@ -10,6 +10,8 @@ use exface\Core\Interfaces\Tasks\ResultInterface;
 use exface\Core\Factories\ResultFactory;
 use exface\Core\Interfaces\Actions\iCallWidgetFunction;
 use exface\Core\Interfaces\Model\UiPageInterface;
+use exface\Core\CommonLogic\Model\UiPage;
+use exface\Core\DataTypes\StringDataType;
 
 /**
  * Activates a function of a select widget (see available functions in widget docs).
@@ -76,16 +78,28 @@ class CallWidgetFunction extends AbstractAction implements iCallWidgetFunction
      */
     public function getWidget(UiPageInterface $page): WidgetInterface
     {
-        return $page->getWidget($this->getWidgetId());
+        $id = $this->getWidgetId();
+        $idSpace = StringDataType::substringBefore($id, UiPage::WIDGET_ID_SPACE_SEPARATOR, '', false, true);
+        if ($idSpace === '' && $this->isDefinedInWidget()) {
+            $idSpace = $this->getWidgetDefinedIn()->getIdSpace();
+            return $page->getWidget(($idSpace ? $idSpace . UiPage::WIDGET_ID_SPACE_SEPARATOR : '') . $id);
+        }
+        return $id;
     }
 
+    /**
+     * 
+     * @return string
+     */
     protected function getWidgetId() : string
     {
         return $this->widgetId;
     }
     
     /**
-     * The ID of the target widget
+     * The ID of the target widget.
+     * 
+     * NOTE:the `widget_id` should either be in the same id space, as the trigger button, or an id space must be included!
      * 
      * @uxon-property widget_id
      * @uxon-type uxon:$..id
