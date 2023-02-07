be92ead4ee2a55b92cb90324bfb595dc16947984
Andrej Kabachnik <andrej.kabachnik@salt-solutions.de>
2023-02-07 17:29:46 +0100

    FIX next issue with SQL migration

--- Install/Sql/MsSQL/Migrations/1.07/20230207_1047_01_NEW_time_zones_for_connections.sql
+++ Install/Sql/MsSQL/Migrations/1.07/20230207_1047_01_NEW_time_zones_for_connections.sql
@@ -40,8 +40,39 @@ UPDATE dbo.[exf_data_connection]
 	WHERE filter_context_UXON IS NOT NULL 
 		AND filter_context_uxon <> '';
 	
-ALTER TABLE dbo.[exf_data_connection]
-	DROP COLUMN [filter_context_uxon];
+IF COL_LENGTH('dbo.exf_data_connection', 'filter_context_uxon') IS NOT NULL
+BEGIN
+	DECLARE @sql NVARCHAR(MAX),
+			@schema NVARCHAR(50) = 'dbo',
+			@table NVARCHAR(50) = 'exf_data_connection',
+			@column NVARCHAR(50) = 'filter_context_uxon'
+	/* DROP default constraints	*/
+	WHILE 1=1
+	BEGIN
+		SELECT TOP 1 @sql = N'ALTER TABLE '+@schema+'.'+@table+' DROP CONSTRAINT ['+dc.NAME+N']'
+			FROM sys.default_constraints dc
+				JOIN sys.columns c ON c.default_object_id = dc.object_id
+			WHERE 
+				dc.parent_object_id = OBJECT_ID(@table)
+				AND c.name = @column
+		IF @@ROWCOUNT = 0 BREAK
+		EXEC (@sql)
+	END
+	/* DROP foreign keys */
+	WHILE 1=1
+	BEGIN
+		SELECT TOP 1 @sql = N'ALTER TABLE '+@schema+'.'+@table+' DROP CONSTRAINT ['+fk.NAME+N']'
+			FROM sys.foreign_keys fk
+				JOIN sys.foreign_key_columns fk_cols ON fk_cols.constraint_object_id = fk.object_id
+			WHERE 
+				fk.parent_object_id = OBJECT_ID(@table)
+				AND COL_NAME(fk.parent_object_id, fk_cols.parent_column_id) = @column
+		IF @@ROWCOUNT = 0 BREAK
+		EXEC (@sql)
+	END
+	/* DROP column */
+	EXEC(N'ALTER TABLE ['+@schema+'].['+@table+'] DROP COLUMN ['+@column+']')
+END
 	
 -- DOWN
 
