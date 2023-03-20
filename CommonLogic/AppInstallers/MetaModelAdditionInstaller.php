ca83927fea4e64d1dd165d480109d460ea2b9fdc
Andrej Kabachnik <andrej.kabachnik@salt-solutions.de>
2023-03-20 16:56:54 +0100

    REFACTOR improved MetaModelAdditionInstaller

--- CommonLogic/AppInstallers/MetaModelAdditionInstaller.php
+++ CommonLogic/AppInstallers/MetaModelAdditionInstaller.php
@@ -5,6 +5,10 @@ use exface\Core\Interfaces\Selectors\SelectorInterface;
 use exface\Core\Interfaces\InstallerContainerInterface;
 use exface\Core\Exceptions\RuntimeException;
 use exface\Core\Interfaces\DataSheets\DataSheetInterface;
+use exface\Core\DataTypes\SortingDirectionsDataType;
+use exface\Core\DataTypes\ComparatorDataType;
+use exface\Core\Exceptions\AppNotFoundError;
+use exface\Core\Factories\DataSheetFactory;
 
 /**
  * Allows to add additional data sheets to the MetaModelInstaller.
@@ -15,6 +19,10 @@ use exface\Core\Interfaces\DataSheets\DataSheetInterface;
 class MetaModelAdditionInstaller extends AbstractAppInstaller
 {
     private $modelInstaller = null;
+    
+    private $appInstalled = null;
+    
+    private $appUid = null;
     
     public function __construct(SelectorInterface $selectorToInstall, InstallerContainerInterface $installerContainer)
     {
@@ -29,6 +37,35 @@ class MetaModelAdditionInstaller extends AbstractAppInstaller
     {
         $this->modelInstaller->addModelDataSheet($subfolder, $sheetToExport, $lastUpdateAttributeAlias);
         return $this;
+    }
+    
+    /**
+     * 
+     * @param string $subfolder
+     * @param string $objectSelector
+     * @param string $appRelationAttribute
+     * @param string $sorterAttribute
+     * @return DataSheetInterface
+     */
+    protected function createModelDataSheet(string $objectSelector, string $appRelationAttribute, string $sorterAttribute) : DataSheetInterface
+    {
+        // If the app is not installed yet, we don't know it's UID,
+        // but we also don't need it for this case - so just let it
+        // be NULL.
+        if ($this->appInstalled === null) {
+            try {
+                $this->appUid = $this->getApp()->getUid();
+                $this->appInstalled = true;
+            } catch (AppNotFoundError $e) {
+                $this->appInstalled = false;
+            }
+        }
+        
+        $additionalSheet = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), $objectSelector);
+        $additionalSheet->getFilters()->addConditionFromString($appRelationAttribute, $this->appUid, ComparatorDataType::EQUALS);
+        $additionalSheet->getSorters()->addFromString($sorterAttribute, SortingDirectionsDataType::ASC);
+        $additionalSheet->getColumns()->addFromAttributeGroup($additionalSheet->getMetaObject()->getAttributeGroup('~WRITABLE'));
+        return $additionalSheet;
     }
     
     /**
