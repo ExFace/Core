a0be46ff9ad160bf7881cb3cba23da5ef5b02847
Thomas-Ressel-92 <thomasressel92@web.de>
2023-03-31 15:41:04 +0200

    New SyncRolesWithTokenTrait for PowerUI Sync with Azure AD

--- CommonLogic/Security/Authenticators/Traits/SyncRolesWithTokenTrait.php
+++ CommonLogic/Security/Authenticators/Traits/SyncRolesWithTokenTrait.php
@@ -14,6 +14,7 @@ trait SyncRolesWithTokenTrait
     
     protected function syncUserRoles(UserInterface $user, AuthenticationTokenInterface $token)
     {
+        
         if($this->hasSyncRoles() === false){
             return;
         }
