fad7303619001dbd89a934ae43f8373cb6af7e5d
ralf.mulansky <ralf.mulansky@accenture.com>
2023-04-20 16:06:41 +0200

    FIX MSSql migration for `exf_page_permitted_per_role` view fixed

--- Install/Sql/MsSQL/Views/06_action_permitted_per_role.sql
+++ Install/Sql/MsSQL/Views/06_action_permitted_per_role.sql
@@ -3,7 +3,7 @@ IF OBJECT_ID('dbo.exf_action_permitted_per_role', 'V') IS NOT NULL
 GO;
 
 /* View of permitted pages per role */
-CREATE OR REPLACE VIEW exf_action_permitted_per_role AS
+CREATE VIEW [dbo].[exf_action_permitted_per_role] AS
 SELECT a.oid as app_oid, oa.oid as action_oid, r.oid as role_oid,
 (CASE WHEN EXISTS (SELECT AUTHORIZATION_POLICY.oid FROM exf_auth_policy AUTHORIZATION_POLICY
   WHERE (AUTHORIZATION_POLICY.target_user_role_oid = r.oid OR AUTHORIZATION_POLICY.target_user_role_oid IS NULL) AND AUTHORIZATION_POLICY.target_object_action_oid = oa.oid AND AUTHORIZATION_POLICY.disabled_flag = 0 AND AUTHORIZATION_POLICY.effect = 'P') THEN 1
