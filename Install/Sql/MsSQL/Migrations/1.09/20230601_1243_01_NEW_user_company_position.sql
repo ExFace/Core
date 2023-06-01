ec32d2676331a98062160813cb75981fac651d71
Andrej Kabachnik <andrej.kabachnik@salt-solutions.de>
2023-06-01 13:31:10 +0200

    NEW Company and position for every user

---
+++ Install/Sql/MsSQL/Migrations/1.09/20230601_1243_01_NEW_user_company_position.sql
@@ -0,0 +1,18 @@
+-- UP
+
+IF COL_LENGTH('dbo.exf_user','company') IS NULL
+ALTER TABLE exf_user
+	ADD COLUMN company NVARCHAR(200) NULL;
+	
+IF COL_LENGTH('dbo.exf_user','position') IS NULL
+ALTER TABLE exf_user
+	ADD COLUMN position NVARCHAR(200) NULL;
+
+	
+-- DOWN
+
+IF COL_LENGTH('dbo.exf_user','company') IS NOT NULL
+ALTER TABLE exf_user DROP COLUMN company;
+
+IF COL_LENGTH('dbo.exf_user','position') IS NOT NULL
+ALTER TABLE exf_user DROP COLUMN position;
