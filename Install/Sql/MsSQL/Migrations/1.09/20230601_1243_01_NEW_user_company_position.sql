2d74058cc3da88a868d97decf62423cc643a8a8f
ralf.mulansky <ralf.mulansky@accenture.com>
2023-06-01 14:23:47 +0200

    FIX added missing database scheme in last MsSQL migration

--- Install/Sql/MsSQL/Migrations/1.09/20230601_1243_01_NEW_user_company_position.sql
+++ Install/Sql/MsSQL/Migrations/1.09/20230601_1243_01_NEW_user_company_position.sql
@@ -1,18 +1,18 @@
 -- UP
 
 IF COL_LENGTH('dbo.exf_user','company') IS NULL
-ALTER TABLE exf_user
+ALTER TABLE dbo.exf_user
 	ADD [company] NVARCHAR(200) NULL;
 	
 IF COL_LENGTH('dbo.exf_user','position') IS NULL
-ALTER TABLE exf_user
+ALTER TABLE dbo.exf_user
 	ADD [position] NVARCHAR(200) NULL;
 
 	
 -- DOWN
 
 IF COL_LENGTH('dbo.exf_user','company') IS NOT NULL
-ALTER TABLE exf_user DROP COLUMN [company];
+ALTER TABLE dbo.exf_user DROP COLUMN [company];
 
 IF COL_LENGTH('dbo.exf_user','position') IS NOT NULL
-ALTER TABLE exf_user DROP COLUMN [position];
+ALTER TABLE dbo.exf_user DROP COLUMN [position];
