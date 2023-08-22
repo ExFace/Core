7b4b850359417cac6dcdeb79c6ee52e4a6ea3372
ralf.mulansky <ralf.mulansky@accenture.com>
2023-08-22 18:13:38 +0200

    FIX only do checks on data in ReadPrefill if data has uid column

--- Actions/ReadPrefill.php
+++ Actions/ReadPrefill.php
@@ -151,6 +151,21 @@ class ReadPrefill extends ReadData implements iPrefillWidget
                 $log .= '- Refresh is not required or not possible' . PHP_EOL;
             }
         }
+        // Check the prefill data again if it is still valid because actual input data
+        // for the action could have been old and user might actually not be allowed
+        // to trigger showing a dialog anymore and therefor trigger the prefill.
+        // As we might have to load additional data for checks, only do that if data sheet has
+        // UID column with values.
+        // This can occur if user did load data in a table and the another user changed a data entry
+        // after the first user did load the table. The first user can then select an entry and press a button
+        // to show a dialog, for example to change a status with ading a commentary.
+        // With up to date data that action wouldn't be allowed, but as the user still has old data shown they can
+        // trigger the action initially.
+        // Therefor we should check again after we load the actual data in the prefill if the prefill ist actually allowed
+        // by checking again against the checks of the trigger action.
+        if ($mainSheet !== null && $mainSheet->hasUidColumn()) {
+            $this->validateInputData($mainSheet);
+        }
         
         if ($mainSheet === null) {
             $log .= '- No prefill data found so far: creating an empty data sheet.' . PHP_EOL;
@@ -181,17 +196,6 @@ class ReadPrefill extends ReadData implements iPrefillWidget
                     unset ($defaults[$widget->getId()]);
                 }
             });
-            // Check fresh prefill data again if it is still valid because actual input data
-            // for the action could have been old and user might actually not be allowed
-            // to trigger showing a dialog anymore and therefor trigger the prefill
-            // This can occur if user did load data in a table and the another user changed a data entry
-            // after the first user did load the table. The first user can then select an entry and press a button
-            // to show a dialog, for example to change a status with ading a commentary.
-            // With up to date data that action wouldn't be allowed, but as the user still has old data shown they can
-            // trigger the action initially.
-            // Therefor we should check again after we load the actual data in the prefill if the prefill ist actually allowed
-            // by checking again against the checks of the trigger action.
-            $this->validateInputData($mainSheet);
             
             // Do the prefill to trigger the events
             $targetWidget->prefill($mainSheet);
