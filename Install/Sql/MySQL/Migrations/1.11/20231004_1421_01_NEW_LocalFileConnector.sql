9bfaf6d24b61840eb351b529de231fc50e1e67e6
Andrej Kabachnik <andrej.kabachnik@salt-solutions.de>
2023-10-05 10:20:06 +0200

    FIX added migration finder_depth > folder_depth

--- Install/Sql/MySQL/Migrations/1.11/20231004_1421_01_NEW_LocalFileConnector.sql
+++ Install/Sql/MySQL/Migrations/1.11/20231004_1421_01_NEW_LocalFileConnector.sql
@@ -4,6 +4,8 @@ UPDATE exf_data_connection SET data_connector = 'exface/Core/DataConnectors/Loca
 	
 UPDATE exf_data_source SET default_query_builder = 'exface/Core/QueryBuilders/FileBuilder.php' WHERE default_query_builder = 'exface/Core/QueryBuilders/FileFinderBuilder.php';
 UPDATE exf_data_source SET custom_query_builder = 'exface/Core/QueryBuilders/FileBuilder.php' WHERE custom_query_builder = 'exface/Core/QueryBuilders/FileFinderBuilder.php';
+
+UPDATE exf_object SET data_address_properties = REPLACE(data_address_properties, 'finder_depth', 'folder_depth');
 	
 -- DOWN
 
@@ -12,3 +14,4 @@ UPDATE exf_data_connection SET data_connector = 'exface/Core/DataConnectors/File
 UPDATE exf_data_source SET default_query_builder = 'exface/Core/QueryBuilders/FileFinderBuilder.php' WHERE default_query_builder = 'exface/Core/QueryBuilders/FileBuilder.php';
 UPDATE exf_data_source SET custom_query_builder = 'exface/Core/QueryBuilders/FileFinderBuilder.php' WHERE custom_query_builder = 'exface/Core/QueryBuilders/FileBuilder.php';
 
+UPDATE exf_object SET data_address_properties = REPLACE(data_address_properties, 'folder_depth', 'finder_depth');
