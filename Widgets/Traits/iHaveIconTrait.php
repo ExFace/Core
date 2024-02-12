e25ea15d249508e01ada7d35aef3e6ecdc24ded8
Andrej Kabachnik <andrej.kabachnik@salt-solutions.de>
2024-02-12 08:45:08 +0100

    DOCs added for SVG icons

--- Widgets/Traits/iHaveIconTrait.php
+++ Widgets/Traits/iHaveIconTrait.php
@@ -26,13 +26,30 @@ trait iHaveIconTrait {
      * The name of the icon to be displayed.
      * 
      * Refer to the documentation of the facade for supported icon names. Most
-     * facades will support font awesome icons and some poprietary icon set
-     * additionally (like UI5 icons in the UI5Facade).
+     * facades will support font awesome icons, SVG icons and some poprietary icon 
+     * sets additionally (like UI5 icons in the UI5Facade). 
+     * 
+     * Please set the correct `icon_set` if using icons other than the default Font Awesome.
      * 
      * You can search for icons here:
      * 
      * - [Font Awesome 4 icons](https://fontawesome.com/v4/icons/)
+     * - [SVG Material Design icons](https://pictogrammers.com/library/mdi/)
      * - [UI5 icon explorer (only for UI5!)](https://sapui5.hana.ondemand.com/sdk/test-resources/sap/m/demokit/iconExplorer/webapp/index.html#/overview/SAP-icons/?tab=grid)
+     * 
+     * The ability to use SVG icons offers a lot of flexibility as most icon sets include
+     * SVGs. Make sure to use icons from a single icon set or at least similarly looking
+     * sets to ensure a consistent look of your app.
+     * 
+     * Here is an example for an SVG icon:
+     * 
+     * ```
+     *  {
+     *      "icon_set": "svg",
+     *      "icon": "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\"><title>file-tree</title><path d=\"M3,3H9V7H3V3M15,10H21V14H15V10M15,17H21V21H15V17M13,13H7V18H13V20H7L5,20V9H7V11H13V13Z\" /></svg>"
+     *  }
+     *  
+     * ```
      * 
      * @uxon-property icon
      * @uxon-type icon|string
