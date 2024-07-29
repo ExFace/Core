c7a7e0f4bb9b3c4ecb47f98aa2e54765466488ab
Andrej Kabachnik <kabachello@users.noreply.github.com>
2024-07-29 16:15:52 +0200

    FIX restored backwards compatibilty for legacy config of DiffText widget

--- Widgets/DiffText.php
+++ Widgets/DiffText.php
@@ -15,6 +15,25 @@ use exface\Core\CommonLogic\Model\Expression;
  * and the text to compare to (the "new" text) is set by `attribute_alias_to_compare` or `value_to_compare`
  * respectively.
  * 
+ * ## Examples
+ * 
+ * ### Compare the current value of a text attribute with a backup
+ * 
+ * The following widget will compare the current value of the `DESCRIPTION` attribute
+ * of some object with the latest backup of it assuming the backup object is accessible
+ * via relation `BACKUP`.
+ * 
+ * Since the base value is the backup, any changes since the last backup will be highlighted
+ * as "new".
+ * 
+ * ```
+ *  {
+ *      "widget_type": "DiffText",
+ *      "attribute_alias": "BACKUP__DESCRIPTION:MAX_OF(CREATED_ON)",
+ *      "attribute_alias_to_compare": "DESCRIPTION"
+ *  }
+ * ```
+ * 
  * @author Andrej Kabachnik
  *        
  */
@@ -171,37 +190,44 @@ class DiffText extends Value
         
         return $data_sheet;
     }
-    /*
-    public function getLeftValue()
+    
+    /**
+     * @deprecated use setValue() instead
+     * @param string $value
+     * @return DiffText
+     */
+    protected function setLeftValue($value) : DiffText
     {
-        return $this->left_value;
+        return $this->setValue($value);
     }
     
-    public function setLeftValue($value)
+    /**
+     * @deprecated use setValueToCompare() instead
+     * @param string $value
+     * @return DiffText
+     */
+    protected function setRightValue($value) : DiffText
     {
-        $this->left_value = $value;
-        return $this;
+        return $this->setValueToCompare($value);
     }
     
-    public function getRightValue()
-    {
-        return $this->right_value;
-    }
-    
-    public function setRightValue($value)
-    {
-        $this->right_value = $value;
-        return $this;
-    }
-    
-    protected function setLeftAttributeAlias($value)
+    /**
+     * @deprecated use setAttributeAlias() instead
+     * @param string $value
+     * @return DiffText
+     */
+    protected function setLeftAttributeAlias($value) : DiffText
     {
         return $this->setAttributeAlias($value);
     }
     
-    protected function setRightAttributeAlias($value)
+    /**
+     * @deprecated use setAttributeAliasToCompare() instead
+     * @param string $value
+     * @return DiffText
+     */
+    protected function setRightAttributeAlias($value) : DiffText
     {
         return $this->setAttributeAliasToCompare($value);
     }
-    */
 }
