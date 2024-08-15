53244f55565c86a578baa38cbb75ccf9ebd51374
Andrej Kabachnik <kabachello@users.noreply.github.com>
2024-08-15 17:55:39 +0200

    DEV property text for ColorIndicator Widget

--- Widgets/ColorIndicator.php
+++ Widgets/ColorIndicator.php
@@ -3,6 +3,10 @@ namespace exface\Core\Widgets;
 
 use exface\Core\Interfaces\Widgets\iHaveColor;
 use exface\Core\DataTypes\BooleanDataType;
+use exface\Core\Interfaces\DataSheets\DataSheetInterface;
+use exface\Core\Widgets\Parts\WidgetPropertyBinding;
+use exface\Core\Interfaces\Widgets\WidgetPropertyBindingInterface;
+use exface\Core\CommonLogic\UxonObject;
 
 /**
  * A ColorIndicator will change it's color depending the value of it's attribute.
@@ -30,11 +34,23 @@ use exface\Core\DataTypes\BooleanDataType;
  */
 class ColorIndicator extends Display implements iHaveColor
 {
+    const BINDING_PROPERTY_TEXT = 'text';
+    
     private $fixedColor = null;
     
     private $fill = true;
     
     private $colorOnly = null;
+    
+    private $textBindingUxon = null;
+    
+    private $textBinding = null;
+    
+    protected function init()
+    {
+        parent::init();
+        $this->textBindingUxon = new UxonObject();
+    }
     
     /**
      * 
@@ -109,5 +125,83 @@ class ColorIndicator extends Display implements iHaveColor
     {
         $this->colorOnly = $value;
         return $this;
-    }    
+    }
+    
+    /**
+     *
+     * @return string
+     */
+    public function getTextAttributeAlias() : string
+    {
+        return $this->textAttributeAlias;
+    }
+    
+    /**
+     * Makes the progressbar show the value of a different attribute than the one used for the progress.
+     *
+     * @uxon-property text_attribute_alias
+     * @uxon-type metamodel:attribute
+     *
+     * @param string $value
+     * @return ColorIndicator
+     */
+    public function setTextAttributeAlias(string $value) : ColorIndicator
+    {
+        $this->textBindingUxon->setProperty('attribute_alias', $value);
+        $this->textBinding = null;
+        return $this;
+    }
+    
+    /**
+     * A static text value
+     * 
+     * @uxon-property text
+     * @uxon-type string
+     * 
+     * @param string $value
+     * @return ColorIndicator
+     */
+    protected function setText(string $value) : ColorIndicator
+    {
+        $this->textBindingUxon->setProperty('value', $value);
+        $this->textBinding = null;
+        return $this;
+    }
+    
+    /**
+     * 
+     * @return WidgetPropertyBindingInterface
+     *//*
+    public function getTextBinding() : WidgetPropertyBindingInterface
+    {
+        // Create emtpy binding if none was set explicitly
+        if ($this->textBinding === null) {
+            $this->textBinding = new WidgetPropertyBinding($this, self::BINDING_PROPERTY_TEXT, $this->textBindingUxon);
+        }
+        return $this->textBinding;
+    }*/
+    
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\Core\Widgets\Value::prepareDataSheetToRead()
+     */
+    public function prepareDataSheetToRead(DataSheetInterface $data_sheet = null)
+    {
+        $data_sheet = parent::prepareDataSheetToRead($data_sheet);
+        //$data_sheet = $this->getTextBinding()->prepareDataSheetToRead($data_sheet);
+        return $data_sheet;
+    }
+    
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\Core\Widgets\Value::doPrefill()
+     */
+    protected function doPrefill(DataSheetInterface $data_sheet)
+    {
+        parent::doPrefill($data_sheet);
+        //$this->getTextBinding()->prefill($data_sheet);
+        return;
+    }
 }
