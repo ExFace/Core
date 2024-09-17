904cf4b7af30445897d1654c43f019847154686b
Andrej Kabachnik <kabachello@users.noreply.github.com>
2024-09-17 15:49:36 +0200

    FIX improved SQL migration for AI agents

--- Install/Sql/MySQL/Migrations/1.20/20240917_1524_1_NEW_AI_agents.sql
+++ Install/Sql/MySQL/Migrations/1.20/20240917_1524_1_NEW_AI_agents.sql
@@ -14,7 +14,11 @@ CREATE TABLE `exf_ai_agent` (
   `config_uxon` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
   `data_connection_default_oid` binary(16) DEFAULT NULL,
   PRIMARY KEY (`oid`) USING BTREE,
-  UNIQUE KEY `Key unique` (`alias`) USING BTREE
+  UNIQUE KEY `alias_app_oid` (`alias`,`app_oid`),
+  KEY `exf_ai_agent_app` (`app_oid`),
+  KEY `exf_ai_agent_data_connection_default` (`data_connection_default_oid`),
+  CONSTRAINT `exf_ai_agent_app` FOREIGN KEY (`app_oid`) REFERENCES `exf_app` (`oid`),
+  CONSTRAINT `exf_ai_agent_data_connection_default` FOREIGN KEY (`data_connection_default_oid`) REFERENCES `exf_data_connection` (`oid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
 
 
@@ -29,7 +33,11 @@ CREATE TABLE `exf_ai_conversation` (
   `title` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
   `meta_object_oid` binary(16) DEFAULT NULL,
   `context_data_uxon` text,
-  PRIMARY KEY (`oid`) USING BTREE
+  PRIMARY KEY (`oid`) USING BTREE,
+  KEY `exf_ai_conversation_agent` (`ai_agent_oid`),
+  KEY `exf_ai_conversation_user` (`user_oid`),
+  CONSTRAINT `exf_ai_conversation_agent` FOREIGN KEY (`ai_agent_oid`) REFERENCES `exf_ai_agent` (`oid`),
+  CONSTRAINT `exf_ai_conversation_user` FOREIGN KEY (`user_oid`) REFERENCES `exf_user` (`oid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
 
 
@@ -45,7 +53,11 @@ CREATE TABLE `exf_ai_message` (
   `data` text,
   `tokens_prompt` int DEFAULT NULL,
   `tokens_completion` int DEFAULT NULL,
-  PRIMARY KEY (`oid`) USING BTREE
+  PRIMARY KEY (`oid`) USING BTREE,
+  KEY `exf_ai_message_conversation` (`ai_conversation_oid`),
+  CONSTRAINT `exf_ai_message_conversation` FOREIGN KEY (`ai_conversation_oid`) REFERENCES `exf_ai_conversation` (`oid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
 
 -- DOWN
+
+-- Do not delete tables!
