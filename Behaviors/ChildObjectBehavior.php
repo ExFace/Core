fa21088fbbb9dac5d5e2832a19dde7df92be4360
hail-cookies <81558994+hail-cookies@users.noreply.github.com>
2024-11-25 17:46:33 +0100

    DEV ChildObjectBehavior expanded documentation

--- Behaviors/ChildObjectBehavior.php
+++ Behaviors/ChildObjectBehavior.php
@@ -47,7 +47,18 @@ use exface\Core\Interfaces\Model\IAffectMetaObjectsInterface;
  * 
  * ## Underlying rules for loading data
  * 
- * For this behavio
+ * For this behavior to work, the input sheet needs to have a UID column. If the input-UID is missing, the event
+ * cannot fetch the target data and will fail to relay the event. If this happens, a corresponding entry will be added to
+ * the tracer log.
+ * 
+ * If an input-UID is present, the behavior works as follows:
+ * - Any pre-transaction events try to fetch the target data first from the input sheet and if that fails from the data source. 
+ * Fetching from the source is slow, so try to include all relation columns in the input sheet if possible.
+ * - Any target data loaded for pre-transaction events this behavior will be cached for use by the corresponding post-transaction event-
+ * - Any post-transaction events will try to fetch their target data from the cache.
+ * - For `OnCreateData` and `OnDeleteData` this is always sufficient.
+ * - For `OnUpdateData` and `OnReplaceData` the target objects may have been altered by the transaction itself, which this behavior can detect
+ * and resolve by loading additional data from source.
  * 
  * ## Examples
  * 
