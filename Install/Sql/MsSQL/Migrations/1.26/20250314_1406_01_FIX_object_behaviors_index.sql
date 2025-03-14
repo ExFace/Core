de84ac04c0c914d2018e0def44bd933fc5402588
Andrej Kabachnik <andrej.kabachnik@accenture.com>
2025-03-14 15:09:41 +0100

    FIX MS SQL migration

--- Install/Sql/MsSQL/Migrations/1.26/20250314_1406_01_FIX_object_behaviors_index.sql
+++ Install/Sql/MsSQL/Migrations/1.26/20250314_1406_01_FIX_object_behaviors_index.sql
@@ -1,6 +1,6 @@
 -- UP
 
-CREATE INDEX IX_exf_object_behaviors_object (object_oid) ON exf_object_behaviors;
+CREATE INDEX IX_exf_object_behaviors_object ON exf_object_behaviors (object_oid);
 
 -- DOWN
 
