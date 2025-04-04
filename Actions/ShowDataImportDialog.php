16248a070a58568b47d10d94072cd99a8959ca27
Andrej Kabachnik <andrej.kabachnik@accenture.com>
2025-04-04 12:33:00 +0200

    FIX Improved ShowDataImporterDialog, added exclude_attributes

--- Actions/ShowDataImportDialog.php
+++ Actions/ShowDataImportDialog.php
@@ -2,16 +2,21 @@
 namespace exface\Core\Actions;
 
 use exface\Core\CommonLogic\Constants\Icons;
+use exface\Core\DataTypes\MessageTypeDataType;
 use exface\Core\DataTypes\WidgetVisibilityDataType;
 use exface\Core\Interfaces\Model\UiPageInterface;
 use exface\Core\Interfaces\Tasks\TaskInterface;
 use exface\Core\Interfaces\WidgetInterface;
+use exface\Core\Interfaces\Widgets\iShowData;
+use exface\Core\Interfaces\Widgets\iUseData;
 use exface\Core\Interfaces\Widgets\iUseInputWidget;
 use exface\Core\Factories\WidgetFactory;
 use exface\Core\CommonLogic\UxonObject;
+use exface\Core\Widgets\Dialog;
+use exface\Core\Widgets\Message;
 
 /**
- * Shows a dalog with a data importer to quickly fill the input widget of the button from Excel
+ * Shows a dalog with a data importer to quickly fill the input widget of the button from Excel.
  *
  * @author Andrej Kabachnik
  *
@@ -19,8 +24,8 @@ use exface\Core\CommonLogic\UxonObject;
 class ShowDataImportDialog extends ShowDialog
 {
     private $targetWidgetId = null;
-
     private $targetWidget = null;
+    private $excludedAliases = [];
     
     /**
      * 
@@ -36,6 +41,12 @@ class ShowDataImportDialog extends ShowDialog
     protected function createDialogWidget(UiPageInterface $page, WidgetInterface $contained_widget = NULL)
     {
         $dialog = parent::createDialogWidget($page);
+        
+        $actionObj = $this->getMetaObject();
+        if (! $actionObj->hasDataSource() || ! $actionObj->isWritable()) {
+            $this->addMessage($dialog, $this->getWorkbench()->getCoreApp()->getTranslator()->translate('ACTION.SHOWOBJECTEDITDIALOG.DATA_SOURCE_NOT_WRITABLE'));
+            return $dialog;
+        }
 
         if ($dialog->isEmpty()) {
             $colAttrs = [];
@@ -43,8 +54,12 @@ class ShowDataImportDialog extends ShowDialog
             $uxon = new UxonObject([
                 'columns' => []
             ]);
+            $excludeAliases = $this->getExcludedAttributeAliases();
             foreach($dataWidget->getColumns() as $colWidget) {
                 if (! $colWidget->isBoundToAttribute()) {
+                    continue;
+                }
+                if ($colWidget->hasAggregator()) {
                     continue;
                 }
                 $attr = $colWidget->getAttribute();
@@ -66,50 +81,71 @@ class ShowDataImportDialog extends ShowDialog
                     continue;
                 }
                 $colAttrs[$attr->getAlias()] = $attr;
-                $uxon->appendToProperty('columns', new UxonObject([
-                    'attribute_alias' => $colWidget->getAttributeAlias(),
-                    'editable' => true
-                ]));
             }
-            /** @var \exface\Core\Widgets\DataImporter $importer*/
-            $importer = WidgetFactory::createFromUxonInParent($dialog, $uxon, 'DataImporter');
-            foreach ($dialog->getMetaObject()->getAttributes()->getRequired()->getEditable() as $attr) {
-                if (! array_key_exists($attr->getAlias(), $colAttrs)) {
-                    $colAttrs[$attr->getAlias()] = $attr;
-                    $col = $importer->createColumnFromAttribute($attr);
-                    $col->setEditable(true);
-                    $importer->addColumn($col);
-                }
+        } else {
+            foreach ($this->getMetaObject()->getAttributes()->getEditable() as $attr) {
+                $colAttrs[$attr->getAlias()] = $attr;
             }
-            $dialog->addWidget($importer);
-            $widthCalc = floor(count($colAttrs) / 3);
-            $width = max(min($widthCalc, 4), 1);
-            $importer->setWidth($width);
-            $importer->setHeight('100%');
-            $dialog->setWidth($width);
-
-            $actionUxon = new UxonObject([
-                'alias' => 'exface.Core.MergeData'
-            ]);
-            if (false) {
-                // TODO
-            } elseif ($this->getMetaObject()->hasLabelAttribute()) {
-                $actionUxon->setProperty('update_if_matching_attributes', new UxonObject([
-                    $this->getMetaObject()->getLabelAttribute()->getAlias()
-                ]));
-            }
-
-            if ($dialog->getHeight()->isUndefined()) {
-                $dialog->setHeight('80%');
-            }
-
-            $dialog->addButton($dialog->createButton(new UxonObject([
-                'caption' => $this->isDefinedInWidget() ? $this->getWidgetDefinedIn()->getCaption() : $this->getName(),
-                'action' => $actionUxon,
-                'visibility' => WidgetVisibilityDataType::PROMOTED,
-                'align' => EXF_ALIGN_OPPOSITE
-            ])));
         }
+
+        if (empty($colAttrs)) {
+            $this->addMessage($dialog, $this->getWorkbench()->getCoreApp()->getTranslator()->translate('ACTION.SHOWDATAIMPORTDIALOG.MESSAGE_EMPTY'));
+            return $dialog;
+        }
+
+        foreach ($colAttrs as $i => $attr) {
+            if (in_array($attr->getAliasWithRelationPath(), $excludeAliases)) {
+                unset($colAttrs[$i]);
+                continue;
+            } 
+            $uxon->appendToProperty('columns', new UxonObject([
+                'attribute_alias' => $attr->getAliasWithRelationPath(),
+                'editable' => true
+            ]));
+        }
+        
+        /** @var \exface\Core\Widgets\DataImporter $importer*/
+        $importer = WidgetFactory::createFromUxonInParent($dialog, $uxon, 'DataImporter');
+        foreach ($dialog->getMetaObject()->getAttributes()->getRequired()->getEditable() as $attr) {
+            if (! array_key_exists($attr->getAlias(), $colAttrs)) {
+                $colAttrs[$attr->getAlias()] = $attr;
+                $col = $importer->createColumnFromAttribute($attr);
+                $col->setEditable(true);
+                $importer->addColumn($col);
+            }
+        }
+        $dialog->addWidget($importer);
+        if (count($colAttrs) === 1) {
+            $width = 1;
+        } else {
+            $widthCalc = floor(count($colAttrs) / 2);
+            $width = max(min($widthCalc, 4), 2);
+        }
+        $importer->setWidth($width);
+        $importer->setHeight('100%');
+        $dialog->setWidth($width);
+
+        $actionUxon = new UxonObject([
+            'alias' => 'exface.Core.MergeData'
+        ]);
+        if (false) {
+            // TODO
+        } elseif ($this->getMetaObject()->hasLabelAttribute()) {
+            $actionUxon->setProperty('update_if_matching_attributes', new UxonObject([
+                $this->getMetaObject()->getLabelAttribute()->getAlias()
+            ]));
+        }
+
+        if ($dialog->getHeight()->isUndefined()) {
+            $dialog->setHeight('80%');
+        }
+
+        $dialog->addButton($dialog->createButton(new UxonObject([
+            'caption' => $this->isDefinedInWidget() ? $this->getWidgetDefinedIn()->getCaption() : $this->getName(),
+            'action' => $actionUxon,
+            'visibility' => WidgetVisibilityDataType::PROMOTED,
+            'align' => EXF_ALIGN_OPPOSITE
+        ])));
         
         return $dialog;
     }
@@ -133,7 +169,50 @@ class ShowDataImportDialog extends ShowDialog
                 $widget = $trigger;
             }
         }
+
+        switch (true) {
+            case $widget instanceof iShowData:
+                return $widget;
+            case $widget instanceof iUseData:
+                return $widget->getData();
+        }
         
-        return $widget;
+        return null;
+    }
+
+    /**
+     * List of attribute aliases to be excluded from the import although they might be shown in the input widget
+     * 
+     * @uxon-property exclude_attributes
+     * @uxon-type metamodel:attribute[]
+     * @uxon-template [""]
+     * 
+     * @param \exface\Core\CommonLogic\UxonObject $arrayOfExpressions
+     * @return ShowDataImportDialog
+     */
+    protected function setExcludeAttributes(UxonObject $arrayOfExpressions) : ShowDataImportDialog
+    {
+        $this->excludedAliases = $arrayOfExpressions->toArray();
+        return $this;
+    }
+
+    /**
+     * 
+     * @return string[]
+     */
+    protected function getExcludedAttributeAliases() : array
+    {
+        return $this->excludedAliases;
+    }
+    protected function addMessage(Dialog $dialog, string $message, string $type = MessageTypeDataType::WARNING) : Message
+    {
+        $message = WidgetFactory::createFromUxonInParent($dialog, new UxonObject([
+            'widget_type' => 'Message',
+            'type' => $type,
+            'width' => '100%',
+            'text' => $message
+        ]));
+        $dialog->addWidget($message);
+        return $message;
     }
 }
