84546db27fb02177ef8c0f9dff34ebd611f1aab9
Andrej Kabachnik <andrej.kabachnik@accenture.com>
2025-04-17 17:27:21 +0200

    REF added MS SQL index proposed by Azure

---
+++ Install/Sql/MsSQL/Migrations/1.29/20250417_1717_1_REF_notification_indices.sql
@@ -0,0 +1,24 @@
+-- UP
+
+-- Recommended by Azure Insights
+DROP INDEX IF EXISTS [IX_exf_notification_hide_from_inbox_user_oid] ON [dbo].[exf_notification];
+GO;
+CREATE NONCLUSTERED INDEX [IX_exf_notification_hide_from_inbox_user_oid] ON [dbo].[exf_notification] ( 
+    [user_oid],
+    [hide_from_inbox]
+) INCLUDE (
+    [icon], 
+    [modified_on], 
+    [read_on], 
+    [reference], 
+    [sent_by], 
+    [sent_on], 
+    [title], 
+    [widget_uxon]
+) WITH (ONLINE = ON);
+
+-- DOWN
+
+DROP INDEX IF EXISTS [IX_exf_notification_hide_from_inbox_user_oid] ON [dbo].[exf_notification];
+
+
