d5528e15ed8fc8eabeabcfd3e4b038e3bc797135
andrejkabachnik <andrej.kabachnik@accenture.com>
2025-06-12 11:12:06 +0200

    NEW UI page mutations can now change page names

--- Mutations/Prototypes/UiPageMutation.php
+++ Mutations/Prototypes/UiPageMutation.php
@@ -20,16 +20,21 @@ class UiPageMutation extends AbstractMutation
 {
     private UxonObject|null $widgetMutationUxon = null;
     private GenericUxonMutation|null $widgetMutation = null;
+    private string|null $changedName = null;
 
     /**
      * @see MutationInterface::apply()
      */
     public function apply($subject): AppliedMutationInterface
     {
+        /* @var $subject \exface\Core\CommonLogic\Model\UiPage */
         if (! $this->supports($subject)) {
             throw new InvalidArgumentException('Cannot apply page mutation to ' . get_class($subject) . ' - only instances of pages supported!');
         }
+
         $stateBefore = null;
+
+        // Apply widget mutations
         if ((null !== $mutation = $this->getWidgetMutation()) && $subject instanceof UiPageInterface) {
             $stateBefore = $stateBefore ?? $subject->exportUxonObject()->toJson(true);
             $uxon = $subject->getContentsUxon();
@@ -37,7 +42,11 @@ class UiPageMutation extends AbstractMutation
             $subject->setContents($uxon);
         }
 
-        // TODO implement a mutation to change properties of the page: e.g. name, description, etc.
+        // Apply changes to regular properties of the page
+        if (null !== $val = $this->getChangedName()) {
+            $subject->setName($val);
+        }
+        // TODO implement a mutation to change all properties of the page: e.g. name, description, etc.
         // A GenericUxonPrototypeMutation would be cool. We could use the on attributes, objects, etc.
 
         if ($stateBefore !== null) {
@@ -68,17 +77,51 @@ class UiPageMutation extends AbstractMutation
     /**
      * Modifies the widget of the page by applying UXON mutation rules
      *
-     * @uxon-property widget
+     * @uxon-property change_widget
      * @uxon-type \exface\Core\Mutations\Prototypes\GenericUxonMutation
      * @uxon-template {"": ""}
      *
      * @param UxonObject $uxonMutation
      * @return $this
      */
-    protected function setWidget(UxonObject $uxonMutation) : UiPageMutation
+    protected function setChangeWidget(UxonObject $uxonMutation) : UiPageMutation
     {
         $this->widgetMutationUxon = $uxonMutation;
         $this->widgetMutation = null;
+        return $this;
+    }
+
+    /**
+     * @deprecated use change_widget instead
+     *
+     * @param UxonObject $uxonMutation
+     * @return $this
+     */
+    protected function setWidget(UxonObject $uxonMutation) : UiPageMutation
+    {
+        return $this->setChangeWidget($uxonMutation);
+    }
+
+    /**
+     * @return string|null
+     */
+    protected function getChangedName() : ?string
+    {
+        return $this->changedName;
+    }
+
+    /**
+     * Changes the name of the page
+     *
+     * @uxon-proeprty change_name
+     * @uxon-type string|metamodel:formula
+     *
+     * @param string $changedName
+     * @return $this
+     */
+    protected function setChangeName(string $changedName) : UiPageMutation
+    {
+        $this->changedName = $changedName;
         return $this;
     }
 }
