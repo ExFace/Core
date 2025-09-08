8afa50f4a7a11b1fdf6e35f5b43db3935b0097b5
Andrej Kabachnik <andrej.kabachnik@accenture.com>
2025-09-08 16:46:54 +0200

    FIX UXON autosuggest becase case sensitive with JsonEditor 10.4.1

--- Facades/AbstractAjaxFacade/Elements/JsonEditorTrait.php
+++ Facades/AbstractAjaxFacade/Elements/JsonEditorTrait.php
@@ -565,20 +565,21 @@ JS;
 
 {
     applyTo: ['value'],
+    caseSensitive: false,
     filter: function (token, match, config) {
         // remove leading space in token if not the only character
         if (  token.length > 1 && ( token.search(/^\s[^\s]/i) > -1 ) ) {
             token = token.substr(1, token.length - 1);
         }
         
-        //remove spaces in token if preceeded by double underscores
-        if (  token.length > 3  && token.search(/\_\_\s/i) ) {
-            token = token.substr(0, token.length - 1);
-        } else if (!token.replace(/\s/g, '').length) {
+        // remove spaces in token if preceeded by double underscores
+        if (token.length > 3) {
+            token = token.replace(/__\s+$/i, '__');
+        } else if (! token.replace(/\s/g, '').length) {
             // return true if token consists of whitespace characters only
             return true;
         }
-       return match.indexOf(token) > -1;
+       return match.toLowerCase().indexOf(token.toLowerCase()) > -1;
     },
     getOptions:  function (text, path, input, editor) {
         return new Promise(function (resolve, reject) {
