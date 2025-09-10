ff210df53309cb508e64c0dae3678c4db8381e63
Seitz, Miriam <miriam.seitz@accenture.com>
2025-09-10 13:26:10 +0200

    NEW Formula Distinct
    Returns a distinct string from given sequence as string

---
+++ Formulas/Distinct.php
@@ -0,0 +1,54 @@
+<?php
+namespace exface\Core\Formulas;
+
+use exface\Core\Exceptions\InvalidArgumentException;
+
+/**
+ * Returns a distinct string from a given sequence as string.
+ * 
+ * If only one value is within the string, the value will be returned without any changes.
+ * If multiple values are within the string, the string will be concatenated to contain only distinct values.
+ * 
+ * ### Examples:
+ *
+ * - `=Distinct('12')` -> returns '12'
+ * - `=Distinct('12,12,12')` -> returns '12'
+ * - `=Distinct('12, 12, 12')` -> returns '12'
+ * - `=Distinct('12; 12; 12')` -> returns '12'
+ * - `=Distinct('12, 12, 3, 3')` -> returns '12, 3'
+ *
+ * @author Miriam Seitz
+ *        
+ */
+class Distinct extends \exface\Core\CommonLogic\Model\Formula
+{
+
+    /**
+     * 
+     * @param string|null $list - The list that should be reduced to only distinct values
+     * @return string
+     */
+    function run(string $list = null) : string
+    {
+        if ($list === null) {
+            throw new InvalidArgumentException('No string to process provided.');
+        }
+
+        switch (true) {
+         case str_contains($list, ','):
+             $separator = ',';
+             $list_as_array = explode($separator, $list);
+             break;
+         case str_contains($list, ';'):
+             $separator = ';';
+             $list_as_array = explode($separator, $list);
+             break;
+             default:
+                 throw new InvalidArgumentException('Provided string \'' . $list . '\' has no recognizable separator.');
+        }
+
+        $distinct_array = array_unique($list_as_array);
+
+        return implode($separator, $distinct_array);
+    }
+}
