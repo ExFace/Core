9d256dac3dae8a6f1193fff7cf7dbd67b773d14e
Andrej Kabachnik <andrej.kabachnik@accenture.com>
2025-10-02 10:28:33 +0200

    NEW formula =ObjectProperty() can now read any attribute of the metaobject

--- Formulas/ObjectProperty.php
+++ Formulas/ObjectProperty.php
@@ -3,7 +3,9 @@
 namespace exface\Core\Formulas;
 
 use exface\Core\CommonLogic\Model\Formula;
+use exface\Core\DataTypes\ComparatorDataType;
 use exface\Core\Exceptions\InvalidArgumentException;
+use exface\Core\Factories\DataSheetFactory;
 use exface\Core\Factories\MetaObjectFactory;
 
 /**
@@ -13,6 +15,7 @@ use exface\Core\Factories\MetaObjectFactory;
  *
  * - `=ObjectProperty('UID')` will return the UID of the data object.
  * - `=ObjectProperty('ALIAS_WITH_NS')` will return the namespaced alias of the data object.
+ * - `=ObjectProperty('APP__ALIAS')` will return the namespace of data object (= alias of the app).
  * - `=ObjectProperty('UID', 'my.App.ObjectAlias')` will return the UID of the specified object.
  * 
  */
@@ -35,15 +38,24 @@ class ObjectProperty extends Formula
             $obj = MetaObjectFactory::createFromString($this->getWorkbench(), $objectAlias);
         }
 
-        $property = $property ?? 'name';
+        $property = $property ?? 'NAME';
         switch (mb_strtoupper($property)) {
             case 'NAME': return $obj->getName();
             case 'ALIAS': return $obj->getAlias();
             case 'ALIAS_WITH_NS': return $obj->getAliasWithNamespace();
+            case 'NAMESPACE':
+            case 'APP__ALIAS': return $obj->getNamespace();
             case 'DATA_ADDRESS': return $obj->getDataAddress();
         }
         
-        // TODO maybe load other data via DataSheet - similarly to the =User() formula?
+        $obj = MetaObjectFactory::createFromString($this->getWorkbench(), 'exface.Core.OBJECT');
+        if ($obj->hasAttribute($property)) {
+            $lookupSheet = DataSheetFactory::createFromObject($obj);
+            $lookupSheet->getFilters()->addConditionFromString('ALIAS_WITH_NS', $objectAlias, ComparatorDataType::EQUALS);
+            $col = $lookupSheet->getColumns()->addFromExpression($property);
+            $lookupSheet->dataRead();
+            return $col->getValue(0);
+        }
         
         throw new InvalidArgumentException('Invalid property "' . $property . '" requested for =ObjectProperty() formula');
     }
