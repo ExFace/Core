b50b54139307c6b9756e60a7793d2b89d19548f7
gizem-bicer <gizem.bicer@accenture.com>
2025-11-25 14:00:29 +0100

    FIX PostgreSqlLoader and PostgreSqlBuilder updates for ids described as UUID (#619)

--- ModelLoaders/PostgreSqlModelLoader.php
+++ ModelLoaders/PostgreSqlModelLoader.php
@@ -25,7 +25,7 @@ class PostgreSqlModelLoader extends SqlModelLoader
     protected function buildSqlUuidSelector($field_name) : string
     {
         // In PostgreSQL casting to `::text` should normalize all letters to lowercase
-        return "REPLACE({$field_name}::text, '\\x', '0x')";
+        return "('0x' || REPLACE({$field_name}::text, '-', ''))";
     }
 
     /**
@@ -35,9 +35,9 @@ class PostgreSqlModelLoader extends SqlModelLoader
     protected function buildSqlEscapedUid(string $uid) : string
     {
         $uid = parent::buildSqlEscapedUid($uid);
-        // Convert 0x6876846 -> \x6876846
+        // Convert 0x6876846 -> 6876846-547547-
         $hex = substr($uid, 2);
-        return "E'\\\\x{$hex}'::bytea";
+        return "'{$hex}'";
     }
     
     /**
