52aab7c169e850de1ada033836a2447218432283
Andrej Kabachnik <andrej.kabachnik@accenture.com>
2026-01-26 12:19:50 +0100

    FIX missing ActionAppNotFoundError class

---
+++ Exceptions/Actions/ActionAppNotFoundError.php
@@ -0,0 +1,12 @@
+<?php
+namespace exface\Core\Exceptions\Actions;
+
+/**
+ * Exception thrown if an action of an app is requested, that is not installed
+ *
+ * @author Andrej Kabachnik
+ *        
+ */
+class ActionAppNotFoundError extends ActionNotFoundError
+{
+}
