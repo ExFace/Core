d897cfd154e610c87f2e9a94ad3c19ae8a1dec72
Andrej Kabachnik <andrej.kabachnik@accenture.com>
2026-03-12 19:59:04 +0100

    FIX bad migration for PostgreSQL

--- Install/Sql/PostgreSQL/Migrations/1.35/20260210_1130_1_DEV_MONITOR_task_metadata.sql
+++ Install/Sql/PostgreSQL/Migrations/1.35/20260210_1130_1_DEV_MONITOR_task_metadata.sql
@@ -1,8 +1,9 @@
 -- UP
-ALTER TABLE `exf_monitor_action` 
-	ADD `task_class` varchar(100) COLLATE 'utf8mb3_general_ci' NULL,
-	ADD `ui_flag` tinyint NULL,
-	ADD `request_size` int NULL;
+
+ALTER TABLE exf_monitor_action
+    ADD COLUMN task_class VARCHAR(100),
+    ADD COLUMN ui_flag SMALLINT,
+    ADD COLUMN request_size INTEGER;
 
 -- DOWN
 -- Do not delete columns to avoid losing data!
