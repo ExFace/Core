2ed2212ab7b4ca6fe4aaeed686bce569a5178c4f
sergejriel <133098990+sergejriel@users.noreply.github.com>
2026-03-17 07:56:18 +0100

    NEW JWT authentication tokens + HTTP facade authentication option bearer_token_as_jwt (#711)
    * NEW: added authentication with JWTAuthToken
    
    * REF Moved jwt token specific logic to JWTAuthToken, removed setters.
    
    * NEW Error check
    
    * NEW added comments
    
    * REF code cleanup

--- CommonLogic/Security/AuthenticationToken/JWTAuthToken.php
+++ CommonLogic/Security/AuthenticationToken/JWTAuthToken.php
@@ -4,6 +4,8 @@ namespace exface\Core\CommonLogic\Security\AuthenticationToken;
 use exface\Core\Interfaces\Facades\FacadeInterface;
 use exface\Core\Interfaces\Facades\HttpFacadeInterface;
 use exface\Core\Interfaces\Security\JWTAuthenticationTokenInterface;
+use JsonException;
+use RuntimeException;
 
 /**
  * Common authentication token implementation for JSON Web Tokens (JWT).
@@ -14,15 +16,14 @@ use exface\Core\Interfaces\Security\JWTAuthenticationTokenInterface;
  */
 class JWTAuthToken implements JWTAuthenticationTokenInterface
 {
-    private const JWT_ERROR_PREFIX = 'Azure App Registration Authenticator Error: ';
-    
+    private const JWT_ERROR_PREFIX = 'JWTAuthToken Error: ';
+    private const ALLOWED_ALGORITHMS = ['RS256', 'RS384', 'RS512', 'ES384','ES256', 'HS256', 'HS384', 'HS512'];
     private string $token;
-    private ?string $username = null;
-    private ?HttpFacadeInterface $facade = null;
+    private ?string $username;
+    private ?HttpFacadeInterface $facade;
     private ?array $payload = null;
+    
     private ?array $header = null;
-
-    private string $expectedAudience = '';
 
     /**
      * @param string $token
@@ -34,15 +35,12 @@ class JWTAuthToken implements JWTAuthenticationTokenInterface
         string $token, 
         string $username, 
         ?HttpFacadeInterface $facade = null,
-        ?array $header = null,
-        ?array $payload = null
-    )
+        ?array $payload = null)
     {
         $this->token = $token;
         $this->facade = $facade;
         $this->username = $username;
         $this->payload = $payload;
-        $this->header = $header;
     }
 
     /**
@@ -68,27 +66,50 @@ class JWTAuthToken implements JWTAuthenticationTokenInterface
     {
         return $this->token;
     }
-    
+
     /**
-     * Returns the `aud` claim
+     * Gets the JWT header as an associative array. 
+     * The header is the first part of the JWT token, which is a base64-encoded JSON string.
      * 
-     * The "aud" (audience) claim identifies the recipients that the JWT is
-     * intended for.  Each principal intended to process the JWT MUST
-     * identify itself with a value in the audience claim.  If the principal
-     * processing the claim does not identify itself with a value in the
-     * "aud" claim when this claim is present, then the JWT MUST be
-     * rejected.  In the general case, the "aud" value is an array of case-
-     * sensitive strings, each containing a StringOrURI value.  In the
-     * special case when the JWT has one audience, the "aud" value MAY be a
-     * single case-sensitive string containing a StringOrURI value.  The
-     * interpretation of audience values is generally application specific.
-     * Use of this claim is OPTIONAL.
+     * @return array
+     */
+    public function getHeader() : array
+    {
+        if ($this->header === null) {
+            $parts = explode('.', $this->token);
+
+            if (count($parts) !== 3) {
+                throw new RuntimeException(self::JWT_ERROR_PREFIX . 'Invalid JWT format (header.payload.signature)');
+            }
+            
+            try {
+                $this->header = json_decode(base64_decode($parts[0]), true, flags: JSON_THROW_ON_ERROR);
+            } catch (JsonException $e) {
+                throw new RuntimeException(self::JWT_ERROR_PREFIX . 'Invalid JWT header: ' . $e->getMessage(), $e->getCode(), $e);
+            }
+        }
+        return $this->header;
+    }
+
+    /**
+     * Gets the encryption algorithm (alg) from the JWT header and checks if it is allowed. 
+     * It is used for verifying the token signature.
+     * 
+     * You can not just trust the alg in the header, because the token is probably not verified at this point.
+     * The public keys from the JWKS also contain the alg, but not in all cases (not in v1 tokens).
+     * That is why we check the alg against a whitelist of allowed algorithms before using it.
      * 
      * @return string
      */
-    public function getClaimAudience() : string
+    public function getHeaderAlgorithm() : string
     {
-        return $this->expectedAudience;
+        $headerAlg = $this->getHeader()['alg'] ?? null;
+        
+        if (!is_string($headerAlg) || !in_array($headerAlg, self::ALLOWED_ALGORITHMS, true)) {
+            throw new RuntimeException(self::JWT_ERROR_PREFIX . "Unexpected encryption algorithm (alg): " . (string)$headerAlg);
+        }
+        
+        return $headerAlg;
     }
 
     /**
@@ -99,40 +120,12 @@ class JWTAuthToken implements JWTAuthenticationTokenInterface
         return $this->isValidated() === false;
     }
 
+    /**
+     * @return bool
+     */
     protected function isValidated() : bool
     {
-        // The JWT token will be handled by AzureAppRegistrationAuthenticator. That authenticator will decode the token using public
-        // keys from Azure tenant. If decoded, the authenticator must create a new instance of this class, which will
-        // represent the authenticated token.
+        // The token is considered validated if the payload has been successfully decoded in corresponding Authenticator and set here.
         return ! empty($this->payload);
-    }
-    
-    public function getPayload() : ?array
-    {
-        return $this->payload;
-    }
-    
-    public function getHeader() : ?array
-    {
-        // Reading the kid from the header.
-        $parts = explode('.', $this->token);
-        if (count($parts) !== 3) {
-            throw new RuntimeException(self::JWT_ERROR_PREFIX . 'Invalid JWT format (header.payload.signature)');
-        }
-        [$headerB64] = $parts;
-
-        try {
-            $header = json_decode($this->base64UrlDecode($headerB64), true, flags: JSON_THROW_ON_ERROR);
-        } catch (JsonException $e) {
-            throw new RuntimeException(self::JWT_ERROR_PREFIX . 'Invalid JWT header: ' . $e->getMessage(), $e->getCode(), $e);
-        }
-        // TODO cache header in private var
-        return $header;
-    }
-    
-    public function getAlgorithm() : ?string
-    {
-        $header = $this->getHeader();
-        return $header['alg'] ?? null;
     }
 }
