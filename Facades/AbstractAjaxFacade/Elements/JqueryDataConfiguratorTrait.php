0c1ba862aa163cee1e7f15117d88a4afd5e4a350
Saskia Hustinx <91604850+sHustinx@users.noreply.github.com>
2026-05-07 20:49:46 +0200

    NEW: WidgetSetups can now save/load header filters (#772)
    * NEW exfTools: added JSON Object comparator function
    
    * NEW DataGetter: add property hidden to filters
    
    * REF DataGetter: moved hidden flag out of getter into separate method

--- Facades/AbstractAjaxFacade/Elements/JqueryDataConfiguratorTrait.php
+++ Facades/AbstractAjaxFacade/Elements/JqueryDataConfiguratorTrait.php
@@ -41,6 +41,43 @@ trait JqueryDataConfiguratorTrait
         }
         return;
     }
+
+    /**
+     * Returns the filters of a data object, and adds a hidden flag, indicating whether the filter is hidden or not. 
+     * This only works for rendered filters, as it gets the current JS values of the filters.
+     *
+     * @return string 
+     */
+    public function buildJsFilterGetter() : string
+    {
+        $widget = $this->getWidget();
+        $filters = [];
+        $nestedGroups = [];
+
+        foreach ($widget->getFilters() as $filter) {
+            $filterElement = $this->getFacade()->getElement($filter);
+            if ($filter->hasCustomConditionGroup() === true) {
+                $nestedGroups[] = preg_replace(
+                    '/\}\s*$/',
+                    ', "hidden" : ' . $this->escapeBool($filter->isHidden() === true) . '}',
+                    preg_replace('/,\s*\}\s*$/', '}', $filterElement->buildJsCustomConditionGroup())
+                );
+            } else {
+                $filters[] = preg_replace(
+                    '/\}\s*$/',
+                    ', "hidden" : ' . $this->escapeBool($filter->isHidden() === true) . '}',
+                    preg_replace('/,\s*\}\s*$/', '}', $filterElement->buildJsConditionGetter(null, $widget->getMetaObject()))
+                );
+            }
+        }
+
+        $filters = array_filter($filters);
+        if (empty($filters) === false || empty($nestedGroups) === false) {
+            return '{operator: "AND", ignore_empty_values: true, conditions: [' . implode(",\n", $filters) . '], nested_groups: [' . implode(",\n", $nestedGroups) . ']}';
+        }
+
+        return '';
+    }
     
     /**
      * The data JS-object of a configurator widget contains filters, sorters, etc., that are
