1f9f4ca136ae2416c98ae129b5fe701d71ecebfa
Andrej Kabachnik <andrej.kabachnik@accenture.com>
2026-05-13 14:02:36 +0200

    FIX PostgreSQL migration for app descriptions

--- Install/Sql/PostgreSQL/Migrations/1.39/20260429_1318_1_NEW_app_description_and_intro.sql
+++ Install/Sql/PostgreSQL/Migrations/1.39/20260429_1318_1_NEW_app_description_and_intro.sql
@@ -1,10 +1,10 @@
 -- UP
 
 ALTER TABLE exf_app
-    ADD description NVARCHAR(500) NULL;
+    ADD description VARCHAR(500) NULL;
 
 ALTER TABLE exf_app
-    ADD docs_intro_path NVARCHAR(200) NULL;
+    ADD docs_intro_path VARCHAR(200) NULL;
 
 -- DOWN
 -- Do not delete columns to avoid losing data!
