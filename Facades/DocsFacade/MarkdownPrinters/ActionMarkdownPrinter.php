d514668deab3242fe6cc502304a5d54f5ff926dd
Andrej Kabachnik <andrej.kabachnik@accenture.com>
2026-06-12 13:46:30 +0200

    NEW Model component registry

--- Facades/DocsFacade/MarkdownPrinters/ActionMarkdownPrinter.php
+++ Facades/DocsFacade/MarkdownPrinters/ActionMarkdownPrinter.php
@@ -1,57 +1,48 @@
 <?php
-
 namespace exface\Core\Facades\DocsFacade\MarkdownPrinters;
 
-
-use exface\Core\CommonLogic\UxonObject;
 use exface\Core\DataTypes\MarkdownDataType;
-use exface\Core\DataTypes\PhpClassDataType;
-use exface\Core\DataTypes\RelationTypeDataType;
-use exface\Core\Exceptions\Model\MetaRelationBrokenError;
+use exface\Core\Exceptions\InvalidArgumentException;
 use exface\Core\Facades\DocsFacade;
-use exface\Core\Factories\MetaObjectFactory;
-use exface\Core\Factories\QueryBuilderFactory;
 use exface\Core\Interfaces\Actions\ActionInterface;
-use exface\Core\Interfaces\Model\BehaviorInterface;
-use exface\Core\Interfaces\Model\MetaAttributeInterface;
-use exface\Core\Interfaces\Model\MetaAttributeListInterface;
+use exface\Core\Interfaces\Facades\MarkdownInstancePrinterInterface;
+use exface\Core\Interfaces\Facades\MarkdownPrinterInterface;
 use exface\Core\Interfaces\Model\MetaObjectInterface;
-use exface\Core\Interfaces\Model\MetaRelationInterface;
 use exface\Core\Interfaces\WorkbenchInterface;
 
 /**
- * Builds a Markdown documentation view for a meta object and its related objects.
- *
- * The printer renders a table of attributes for the given meta object and can
- * optionally walk through relation attributes to print child objects up to a
- * configurable depth.
+ * Builds a Markdown documentation view for an action
+ * 
+ * @author Brooklyn Fränzschky, Andrej Kabachnik
  */
-class ActionMarkdownPrinter extends AbstractMarkdownPrinter //implements MarkdownPrinterInterface
+class ActionMarkdownPrinter extends AbstractMarkdownPrinter implements MarkdownInstancePrinterInterface
 {
     protected WorkbenchInterface $workbench;
     
     private ActionInterface $action;
-    private int $headingLevel = 1;
-
-    /**
-     * Maximum depth of recursive relation traversal.
-     *
-     * Depth 0 would print only the root object, higher values include related objects.
-     */
-    private int $relationDepth = 0;
-    private ?string $relationType = RelationTypeDataType::REGULAR;
-    
+    private int $headingLevel = 1;    
 
 
     /**
      * Creates a new object markdown printer for the given action.
      *
      */
-    public function __construct(WorkbenchInterface $workbench, ActionInterface $action, int $headingLevel = 1)
+    public function __construct(ActionInterface $action, int $headingLevel = 1)
     {
-        $this->workbench = $workbench;
+        $this->workbench = $action->getWorkbench();
         $this->action = $action;
         $this->headingLevel = $headingLevel;
+    }
+
+
+
+    /**
+     * {@inheritDoc}
+     * @see MarkdownInstancePrinterInterface::constructForInstance()
+     */
+    public static function constructForInstance(object $instance) : MarkdownPrinterInterface
+    {
+        return new self($instance);
     }
 
     /**
@@ -60,9 +51,11 @@ class ActionMarkdownPrinter extends AbstractMarkdownPrinter //implements Markdow
     public function getMarkdown(): string
     {
         $action = $this->action;
-        $heading = MarkdownDataType::buildMarkdownHeader('Action "' . $action->getName() . '"' . ($action->hasMetaObject() ? ' of object "' . $action->getMetaObject()->getName() . '"' : ''), $this->headingLevel);
+        $heading = MarkdownDataType::buildMarkdownHeader('Action "' . $action->getName() . '"' . ($action->hasMetaObject() ? ' on object "' . $action->getMetaObject()->getName() . '"' : ''), $this->headingLevel);
         $prototypeClass = '\\' . get_class($action);
         $prototypeLink = DocsFacade::buildUrlToDocsForUxonPrototype($action);
+        $uxonBlock = MarkdownDataType::escapeCodeBlock($action->exportUxonObject()->toJson(true), 'json');
+        
         return <<<MD
 {$heading}
 
@@ -71,9 +64,7 @@ class ActionMarkdownPrinter extends AbstractMarkdownPrinter //implements Markdow
 - Alias: {$action->getAliasWithNamespace()}
 - Prototype: [$prototypeClass]($prototypeLink)
 
-```
-{$action->exportUxonObject()->toJson(true)}
-```
+{$uxonBlock}
 
 MD;
     }
